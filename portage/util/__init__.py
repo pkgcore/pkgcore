f28b261a8bcf7190d76a8dc30625f053abeb9675
Brian Harring <ferringb@gmail.com>
2005-08-26 00:20:02 +0100

    [savior @ 1933] remove commented cruft in __init__, throwing out the stack_* funcs from <=2.1 since it's easy to do it via map + update.

--- portage/util/__init__.py
+++ portage/util/__init__.py
@@ -1,6 +1,5 @@
 # Copyright: 2005 Gentoo Foundation
 # Author(s): Brian Harring (ferringb@gentoo.org)
 # License: GPL2
-# $Id: __init__.py 1932 2005-08-26 01:17:16Z ferringb $
+# $Id: __init__.py 1933 2005-08-26 01:20:02Z ferringb $
 
-#import modules, fs, misc

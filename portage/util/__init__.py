15072a2303b7f535fb2195ca3b69e91a9a9a75d5
Brian Harring <ferringb@gmail.com>
2005-08-26 00:17:16 +0100

    [savior @ 1932] header commit.

--- portage/util/__init__.py
+++ portage/util/__init__.py
@@ -1,1 +1,6 @@
+# Copyright: 2005 Gentoo Foundation
+# Author(s): Brian Harring (ferringb@gentoo.org)
+# License: GPL2
+# $Id: __init__.py 1932 2005-08-26 01:17:16Z ferringb $
+
 #import modules, fs, misc

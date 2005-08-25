7edf78e8e4095c0b9b70cd3ddfa0d4a7970dc3b5
Brian Harring <ferringb@gmail.com>
2005-08-25 01:37:02 +0100

    [savior @ 1897] not used by savior, although chunks of it will be pulled forward.

--- pym/transports/__init__.py
+++
@@ -1,9 +0,0 @@
-# transports/__init__.py; worthless file to keep python happy.
-# Copyright 2004 Gentoo Foundation
-# Distributed under the terms of the GNU General Public License v2
-#$Header$
-
-# worthless comment so diff creates this file.
-# yay.
-# this will ultimately hold the 'preferred fetcher' in the transport namespace, 
-# along with default options for fetchers (chunk size fex)

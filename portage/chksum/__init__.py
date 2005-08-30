89b8b0604edcddb43f962d6c7d1d094ad34c01a3
Brian Harring <ferringb@gmail.com>
2005-08-30 03:29:23 +0100

    [savior @ 1948] addition of size verifier, and cleanup (fixing) the chksum code since it decided to stop working. whanker.

--- portage/chksum/__init__.py
+++ portage/chksum/__init__.py
@@ -1,9 +1,10 @@
 # Copyright: 2005 Gentoo Foundation
 # Author(s): Brian Harring (ferringb@gentoo.org)
 # License: GPL2
-# $Id: __init__.py 1911 2005-08-25 03:44:21Z ferringb $
+# $Id: __init__.py 1948 2005-08-30 04:29:23Z ferringb $
 
-import os
+import os, sys
+from portage.util.modules import load_module
 
 chksum_types = {}
 __inited__ = False
@@ -26,8 +27,9 @@ def init(additional_handlers={}):
 			i = f.find(".")
 			if i != -1:	f = f[:i]
 			del i
-			m = __import__(f)
-		except ImportError:
+			m = load_module(__name__+"."+f)
+		except ImportError, ie:
+			print "ie",ie
 			continue
 		try:
 			types = getattr(m, "chksum_types")
@@ -45,3 +47,15 @@ def init(additional_handlers={}):
 	chksum_types.update(additional_handlers)	
 
 	__inited__ = True
+
+def size(file, required, cmp_syntax=False):
+	try:
+		size = os.stat(file).st_size
+	except OSError:
+		if cmp_syntax:
+			return -1
+		return False
+
+	if cmp_syntax:
+		return cmp(size, required)
+	return size == required

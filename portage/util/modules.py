858d0dda5d5ae6fae5b41a49b89d36e3351f2007
Brian Harring <ferringb@gmail.com>
2005-09-20 22:40:33 +0100

    [savior @ 2016] safe import; threading.Lock used to insure that portage code/plugins don't get crap imports that get left in sys.modules from a previous failed import (go python :/)

--- portage/util/modules.py
+++ portage/util/modules.py
@@ -1,26 +1,41 @@
 # Copyright: 2005 Gentoo Foundation
 # Author(s): Brian Harring (ferringb@gentoo.org)
 # License: GPL2
-# $Id: modules.py 1911 2005-08-25 03:44:21Z ferringb $
+# $Id: modules.py 2016 2005-09-20 23:40:33Z ferringb $
+
+import sys, threading
 
 class FailedImport(ImportError):
 	def __init__(self, trg, e):	self.trg, self.e = trg, e
 	def __str__(self):	return "Failed importing target '%s': '%s'" % (self.trg, self.e)
 
+__import_lock = threading.Lock()
+
 def load_module(name):
 	"""load a module, throwing a FailedImport if __import__ fails"""
+	__import_lock.acquire()
+	if name in sys.modules:
+		return sys.modules[name]
 	try:
-		m = __import__(name)
-		nl = name.split('.')
-		# __import__ returns nl[0]... so.
-		nl.pop(0)
-		while len(nl):
-			m = getattr(m, nl[0])
+		try:
+			m = __import__(name)
+			nl = name.split('.')
+			# __import__ returns nl[0]... so.
 			nl.pop(0)
-		return m	
-	except (AttributeError, ImportError), e:
-		raise FailedImport(name, e)
-	
+			while len(nl):
+				m = getattr(m, nl[0])
+				nl.pop(0)
+			return m	
+		except AttributeError, e:
+			raise FailedImport(name, e)
+		except ImportError, e:
+			try:
+				del sys.modules[name]
+			except KeyError:
+				pass
+			raise FailedImport(name, e)
+	finally:
+		__import_lock.release()
 
 def load_attribute(name):
 	"""load a specific attribute, rather then a module"""

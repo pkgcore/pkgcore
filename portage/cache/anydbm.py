05a4c4aaf134fb304c38c8232e3682e9bed392b8
Brian Harring <ferringb@gmail.com>
2005-10-11 03:29:13 +0100

    [savior @ 2120] typo on the fallback pickle import for when cPickle can't be imported, corrected it

--- portage/cache/anydbm.py
+++ portage/cache/anydbm.py
@@ -1,13 +1,13 @@
 # Copyright: 2005 Gentoo Foundation
 # Author(s): Brian Harring (ferringb@gentoo.org)
 # License: GPL2
-# $Id: anydbm.py 1911 2005-08-25 03:44:21Z ferringb $
+# $Id: anydbm.py 2120 2005-10-11 04:29:13Z ferringb $
 
 anydbm_module = __import__("anydbm")
 try:
 	import cPickle as pickle
 except ImportError:
-	import pickl
+	import pickle
 import os
 import fs_template
 import cache_errors

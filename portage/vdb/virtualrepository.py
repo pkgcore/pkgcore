9fad737198f18b25838e0128ec02d85f578f40c7
Brian Harring <ferringb@gmail.com>
2005-10-25 17:52:34 +0100

    [savior @ 2179] get_categories shouldn't throw KeyErrors under new ixseq usage

--- portage/vdb/virtualrepository.py
+++ portage/vdb/virtualrepository.py
@@ -48,8 +48,8 @@ class tree(prototype.tree):
 	def _get_categories(self, *optionalCategory):
 		# return if optionalCategory is passed... cause it's not yet supported
 		if len(optionalCategory):
-			raise KeyError("no '%s' category" % optionalCategory[0])
-		return ["virtual"]
+			return ()
+		return ("virtual",)
 
 	def _get_packages(self, category):
 		if self._virtuals == None:

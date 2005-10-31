7fddc97095b500f1e5096ae6acf2dd150e984ff7
Brian Harring <ferringb@gmail.com>
2005-10-31 08:59:29 +0100

    [savior @ 2212] stop using __, use _ (we're all consenting adults after all), and addition of __cmp__, so that max(package_list) works as expected for wrapped packages nifty trick. max(repo.match(atom)) ==> maximum version, going by package __cmp__ rules.

--- portage/package/conditionals.py
+++ portage/package/conditionals.py
@@ -1,7 +1,7 @@
 # Copyright: 2005 Gentoo Foundation
 # Author(s): Brian Harring (ferringb@gentoo.org)
 # License: GPL2
-# $Id: conditionals.py 2178 2005-10-25 15:52:57Z ferringb $
+# $Id: conditionals.py 2212 2005-10-31 08:59:29Z ferringb $
 
 from portage.util.mappings import LimitedChangeSet, Unchangable
 from portage.util.lists import unique, flatten
@@ -24,54 +24,54 @@ class PackageWrapper(object):
 			initial_settings = []
 		if unchangable_settings is None:
 			unchangable_settings = []
-		self.__wrapped_pkg = pkg_instance
+		self._wrapped_pkg = pkg_instance
 		if attributes_to_wrap is None:
 			attributes_to_wrap = {}
-		self.__wrapped_attr = attributes_to_wrap
+		self._wrapped_attr = attributes_to_wrap
 		if configurable_attribute_name.find(".") != -1:
 			raise ValueError("can only wrap first level attributes, 'obj.dar' fex, not '%s'" % (configurable_attribute_name))
 		setattr(self, configurable_attribute_name, LimitedChangeSet(initial_settings, unchangable_settings))
-		self.__unchangable = unchangable_settings
-		self.__configurable = getattr(self, configurable_attribute_name)
-		self.__configurable_name = configurable_attribute_name
-		self.__reuse_pt = 0
-		self.__cached_wrapped = {}		
-		self.__buildable = build_callback
+		self._unchangable = unchangable_settings
+		self._configurable = getattr(self, configurable_attribute_name)
+		self._configurable_name = configurable_attribute_name
+		self._reuse_pt = 0
+		self._cached_wrapped = {}		
+		self._buildable = build_callback
 
 	def __copy__(self):
-		return self.__class__(self.__wrapped_pkg, self.__configurable_name, initial_settings=set(self.__configurable), 
-			unchangable_settings=self.__unchangable, attributes_to_wrap=self.__wrapped_attr)
+		return self.__class__(self._wrapped_pkg, self._configurable_name, initial_settings=set(self._configurable), 
+			unchangable_settings=self._unchangable, attributes_to_wrap=self._wrapped_attr)
 
 	def rollback(self, point=0):
-		self.__configurable.rollback(point)
+		self._configurable.rollback(point)
 		# yes, nuking objs isn't necessarily required.  easier this way though.
 		# XXX: optimization point
-		self.__reuse_pt += 1 
+		self._reuse_pt += 1 
 	
 	def commit(self):
-		self.__configurable.commit()
-		self.__reuse_pt = 0
+		self._configurable.commit()
+		self._reuse_pt = 0
 		
 	def changes_count(self):
-		return self.__configurable.changes_count()
+		return self._configurable.changes_count()
 	
 	def request_enable(self, attr, *vals):
-		if attr not in self.__wrapped_attr:
-			if attr == self.__configurable_name:
+		if attr not in self._wrapped_attr:
+			if attr == self._configurable_name:
 				entry_point = self.changes_count()
 				try:
-					map(self.__configurable.add, vals)
-					self.__reuse_pt += 1
+					map(self._configurable.add, vals)
+					self._reuse_pt += 1
 					return True
 				except Unchangable:
 					self.rollback(entry_point)
 			return False
 		entry_point = self.changes_count()
-		a = getattr(self.__wrapped_pkg, attr)
+		a = getattr(self._wrapped_pkg, attr)
 		try:
 			for x in vals:
 				if x in a.node_conds:
-					map(self.__configurable.add, a.node_conds[x])
+					map(self._configurable.add, a.node_conds[x])
 				else:
 					if x not in a:
 						self.rollback(entry_point)
@@ -79,25 +79,25 @@ class PackageWrapper(object):
 		except Unchangable:
 			self.rollback(entry_point)
 			return False
-		self.__reuse_pt += 1
+		self._reuse_pt += 1
 		return True
 
 	def request_disable(self, attr, *vals):
-		if attr not in self.__wrapped_attr:
-			if attr == self.__configurable_name:
+		if attr not in self._wrapped_attr:
+			if attr == self._configurable_name:
 				entry_point = self.changes_count()
 				try:
-					map(self.__configurable.remove, vals)
+					map(self._configurable.remove, vals)
 					return True
 				except Unchangable:
 					self.rollback(entry_point)
 			return False
 		entry_point = self.changes_count()
-		a = getattr(self.__wrapped_pkg, attr)
+		a = getattr(self._wrapped_pkg, attr)
 		try:
 			for x in vals:
 				if x in a.node_conds:
-						map(self.__configurable.remove, a.node_conds[x])
+						map(self._configurable.remove, a.node_conds[x])
 				else:
 					if x in a:
 						self.rollback(entry_point)
@@ -105,23 +105,23 @@ class PackageWrapper(object):
 		except Unchangable:
 			self.rollback(entry_point)
 			return False
-		self.__reuse_pt += 1
+		self._reuse_pt += 1
 		return True
 
 	def __getattr__(self, attr):
-		if attr in self.__wrapped_attr:
-			if attr in self.__cached_wrapped:
-				if self.__cached_wrapped[attr][0] == self.__reuse_pt:
-					return self.__cached_wrapped[attr][1]
-				del self.__cached_wrapped[attr]
-			o = self.__wrapped_attr[attr](getattr(self.__wrapped_pkg, attr), self.__configurable)
-			self.__cached_wrapped[attr] = (self.__reuse_pt, o)
+		if attr in self._wrapped_attr:
+			if attr in self._cached_wrapped:
+				if self._cached_wrapped[attr][0] == self._reuse_pt:
+					return self._cached_wrapped[attr][1]
+				del self._cached_wrapped[attr]
+			o = self._wrapped_attr[attr](getattr(self._wrapped_pkg, attr), self._configurable)
+			self._cached_wrapped[attr] = (self._reuse_pt, o)
 			return o
 		else:
-			return getattr(self.__wrapped_pkg, attr)
+			return getattr(self._wrapped_pkg, attr)
 
 	def __str__(self):
-		return "config wrapper: %s, configurable('%s'):%s" % (self.__wrapped_pkg, self.__configurable_name, self.__configurable)
+		return "config wrapper: %s, configurable('%s'):%s" % (self._wrapped_pkg, self._configurable_name, self._configurable)
 
 	def freeze(self):
 		o = copy.copy(self)
@@ -130,10 +130,18 @@ class PackageWrapper(object):
 
 	def lock(self):
 		self.commit()
-		self.__configurable = list(self.__configurable)		
-
+		self._configurable = list(self._configurable)		
 		
 	def build(self):
-		if self.__buildable:
-			return self.__buildable(self)
+		if self._buildable:
+			return self._buildable(self)
 		return None
+
+	def __cmp__(self, other):
+		if isinstance(self, PackageWrapper) and isinstance(other, PackageWrapper):
+			c = cmp(self._wrapped_pkg, other._wrapped_pkg)
+			if c == 0:
+				return cmp(self._configurable, other._configurable)
+			return c
+		raise TypeError
+

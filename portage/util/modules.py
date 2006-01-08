697abfa9752e1eedaa8419c51810b45084bde225
Brian Harring <ferringb@gmail.com>
2006-01-08 04:08:39 -0800

    import from marienz's tree

--- portage/util/modules.py
+++ portage/util/modules.py
@@ -2,39 +2,30 @@
 # License: GPL2
 # $Id: modules.py 2327 2005-12-02 00:13:09Z marienz $
 
-import sys, threading
+import sys
 
 class FailedImport(ImportError):
 	def __init__(self, trg, e):	self.trg, self.e = trg, e
 	def __str__(self):	return "Failed importing target '%s': '%s'" % (self.trg, self.e)
 
-__import_lock = threading.Lock()
 
 def load_module(name):
 	"""load a module, throwing a FailedImport if __import__ fails"""
-	__import_lock.acquire()
+	if name in sys.modules:
+	    return sys.modules[name]
 	try:
-		if name in sys.modules:
-			return sys.modules[name]
-		try:
-			m = __import__(name)
-			nl = name.split('.')
-			# __import__ returns nl[0]... so.
+		m = __import__(name)
+		nl = name.split('.')
+		# __import__ returns nl[0]... so.
+		nl.pop(0)
+		while len(nl):
+			m = getattr(m, nl[0])
 			nl.pop(0)
-			while len(nl):
-				m = getattr(m, nl[0])
-				nl.pop(0)
-			return m
-		except (KeyboardInterrupt, SystemExit):
-			raise
-		except Exception, e:
-			try:
-				del sys.modules[name]
-			except KeyError:
-				pass
-			raise FailedImport(name, e)
-	finally:
-		__import_lock.release()
+		return m
+	except (KeyboardInterrupt, SystemExit):
+		raise
+	except Exception, e:
+		raise FailedImport(name, e)
 
 def load_attribute(name):
 	"""load a specific attribute, rather then a module"""

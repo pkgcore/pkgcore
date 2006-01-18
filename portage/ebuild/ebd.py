8ed1ba0b1b182ad8e1237ec7261f24c4e0736f4b
Brian Harring <ferringb@gmail.com>
2006-01-18 06:08:57 -0800

    can't go the PORTAGE_ACTUAL_DISTDIR route in savior, breaks too many abstractions.

--- portage/ebuild/ebd.py
+++ portage/ebuild/ebd.py
@@ -239,7 +239,8 @@ class buildable(ebd, build.base):
 
 	def setup_distfiles(self):
 		if len(self.files):
-			self.env["PORTAGE_ACTUAL_DISTDIR"] = self.env["DISTDIR"]
+			# cvs/svn ebuilds need to die.
+			#self.env["PORTAGE_ACTUAL_DISTDIR"] = self.env["DISTDIR"]
 			self.env["DISTDIR"] = normpath(os.path.join(self.builddir, "distdir"))+"/"
 
 			try:

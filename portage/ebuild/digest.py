22fb818609fb29ff3523138711f5a3515a44ec15
Brian Harring <ferringb@gmail.com>
2006-01-22 16:57:54 -0800

    size must be a long whenever kicked back from a digest subsystem.

--- portage/ebuild/digest.py
+++ portage/ebuild/digest.py
@@ -15,8 +15,8 @@ def parse_digest(path, throw_errors=True):
 				if throw_errors:
 					raise ParseChksumError(path, "line count was not 4, was %i: '%s'" % (len(l), line))
 			#MD5 c08f3a71a51fff523d2cfa00f14fa939 diffball-0.6.2.tar.bz2 305567
-			d[l[2]] = {l[0].lower():l[1], "size":l[3]}
+			d[l[2]] = {l[0].lower():l[1], "size":long(l[3])}
 		f.close()
-	except (OSError, IOError), e:
+	except (OSError, IOError, TypeError), e:
 		raise ParseChksumError("failed parsing " + path, e)
 	return d

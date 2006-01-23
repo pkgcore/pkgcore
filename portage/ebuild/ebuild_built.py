a744275d782e64ef50b8156db23bd868d3ca2430
Brian Harring <ferringb@gmail.com>
2006-01-23 00:04:25 -0800

    install support for the built ebuild format, and whitespace cleanup.

--- portage/ebuild/ebuild_built.py
+++ portage/ebuild/ebuild_built.py
@@ -5,16 +5,29 @@
 from portage.ebuild import ebuild_src
 from portage.util.mappings import ImmutableDict
 from portage.package import metadata, base
+import ebd
 
 class built(base.base):
-	
+	# these are what must override, be stored.
+	tracked_attributes = ["depends", "rdepends", "license", "slot", "use", "eapi", "keywords", 
+		"contents", "environment", "provides"]
+
 	def __init__(self, pkg, contents, environment):
 		import warnings
 		warnings.warn("%s is going to go away soon, please contact harring if you're using this" % str(self.__class__))
-		for x in ("fetchables", "depends", "rdepends", "description", "license", "use", "slot", "package", "version", "category"):
-			setattr(self, x, getattr(pkg, x))
 		self.contents = contents
-		self.bundled_data = ImmutableDict({"environment":environment})
+		self.environment = environment
+		self.__pkg = pkg
+		for x in self.tracked_attributes:
+			if not hasattr(self, x):
+				setattr(self, x, getattr(pkg, x))
+
+	def __getattr__(self, attr):
+		return getattr(self.__pkg, attr)
+
+	def _repo_install(self, *a, **kw):
+		env = {"PORT_ENV_FILE":self.environment.get_path()}
+		return ebd.install_op(self, env=env, *a, **kw)
 
 def passthrough(inst, attr):
 	return inst.data[attr.upper()]
@@ -32,7 +45,7 @@ class package(ebuild_src.package):
 	_subbed_attrs["fetchables"] = lambda *a: []
 	_subbed_attrs["use"] = lambda *a: passthrough(*a).split()
 	_subbed_attrs["contents"] = passthrough
-
+	
 	allow_regen = False
 
 	def __getattr__(self, attr):
@@ -47,6 +60,9 @@ class package(ebuild_src.package):
 			self.__dict__[attr] = obj
 		return obj
 
+	def _repo_install_op(self, features=None):
+		return ebd.install_op(self, pkg, self.environment, features=features)
+		
 
 class package_factory(metadata.factory):
 	child_class = package
@@ -56,6 +72,7 @@ class package_factory(metadata.factory):
 
 	def _get_new_child_data(self, cpv):
 		return ([self._parent_repo._get_ebuild_path], {})
+
 
 def generate_new_factory(*a, **kw):
 	return package_factory(*a, **kw).new_package

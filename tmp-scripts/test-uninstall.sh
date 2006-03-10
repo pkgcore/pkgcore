fd17240fcb3b7647be0b0f454a72a4b6e8417f11
Brian Harring <ferringb@gmail.com>
2006-03-10 00:21:59 -0800

    make the uninstall script a wee bit smarter about finding the install script when it needs it.

--- tmp-scripts/test-uninstall.sh
+++ tmp-scripts/test-uninstall.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-[ ! -d  /home/bharring/vdb-test/dev-util/bsdiff-4.3 ] && ./test-install.sh
+[ ! -d  /home/bharring/vdb-test/dev-util/bsdiff-4.3 ] && $(dirname "$0")/test-install.sh
 python -c'from portage.vdb import repository;
 v=repository("/home/bharring/vdb-test").trees[0];
 v.frozen=False;

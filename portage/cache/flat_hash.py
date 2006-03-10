e239d4b8d01c6f8eb87c214db72255e9905b5f41
Brian Harring <ferringb@gmail.com>
2006-03-10 00:02:26 -0800

    pdating copyright statements; intention was to hand copyright over to foundation as soon as they had their crap set up, but since it hasn't occured, reverting it back to the actual authors.

--- portage/cache/flat_hash.py
+++ portage/cache/flat_hash.py
@@ -1,4 +1,4 @@
-# Copyright: 2005 Gentoo Foundation
+# Copyright: 2005 Brian Harring <ferringb@gmail.com>
 # License: GPL2
 # $Id: flat_list.py 1911 2005-08-25 03:44:21Z ferringb $
 
--- portage/cache/metadata.py
+++ portage/cache/metadata.py
@@ -1,4 +1,4 @@
-# Copyright: 2005 Gentoo Foundation
+# Copyright: 2005 Brian Harring <ferringb@gmail.com>
 # License: GPL2
 # $Id: metadata.py 2270 2005-11-10 00:17:33Z ferringb $
 

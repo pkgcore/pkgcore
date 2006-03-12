8776c066429ff02f2da14ab62b01ecb500c2e6b6
Brian Harring <ferringb@gmail.com>
2006-03-12 02:29:06 -0800

    guideline cleanups- whitespace try/except blocks absolute import
    additionally, removed daft import (

--- portage/cache/sql_template.py
+++ portage/cache/sql_template.py
@@ -1,8 +1,7 @@
 # Copyright: 2005 Brian Harring <ferringb@gmail.com>
 # License: GPL2
 
-import template, cache_errors
-from template import reconstruct_eclasses
+from portage.cache import template, cache_errors
 
 class SQLDatabase(template.database):
 	"""template class for RDBM based caches
@@ -43,6 +42,7 @@ class SQLDatabase(template.database):
 		"""initialize the instance.
 		derived classes shouldn't need to override this"""
 
+		self.db = None
 		super(SQLDatabase, self).__init__(location, label, auxdbkeys, *args, **config)
 
 		config.setdefault("host","127.0.0.1")
@@ -51,50 +51,49 @@ class SQLDatabase(template.database):
 
 		self.label = self._sfilter(self.label)
 
-
 	def _dbconnect(self, config):
 		"""should be overridden if the derived class needs special parameters for initializing
 		the db connection, or cursor"""
 		self.db = self._dbClass(**config)
 		self.con = self.db.cursor()
 
-
 	def _initdb_con(self,config):
 		"""ensure needed tables are in place.
 		If the derived class needs a different set of table creation commands, overload the approriate
-		SCHEMA_ attributes.  If it needs additional execution beyond, override"""
+		SCHEMA_ attributes.  If it needs additional execution beyond, override
+		"""
 
 		self._dbconnect(config)
 		if not self._table_exists(self.SCHEMA_PACKAGE_NAME):
 			if self.readonly:
 				raise cache_errors.ReadOnlyRestriction("table %s doesn't exist" % \
 					self.SCHEMA_PACKAGE_NAME)
-			try:	self.con.execute(self.SCHEMA_PACKAGE_CREATE)
-			except  self._BaseError, e:
+			try:
+				self.con.execute(self.SCHEMA_PACKAGE_CREATE)
+			except self._BaseError, e:
 				raise cache_errors.InitializationError(self.__class__, e)
 
 		if not self._table_exists(self.SCHEMA_VALUES_NAME):
 			if self.readonly:
 				raise cache_errors.ReadOnlyRestriction("table %s doesn't exist" % \
 					self.SCHEMA_VALUES_NAME)
-			try:	self.con.execute(self.SCHEMA_VALUES_CREATE)
-			except	self._BaseError, e:
+			try:
+				self.con.execute(self.SCHEMA_VALUES_CREATE)
+			except self._BaseError, e:
 				raise cache_errors.InitializationError(self.__class__, e)
-
 
 	def _table_exists(self, tbl):
 		"""return true if a table exists
 		derived classes must override this"""
 		raise NotImplementedError
 
-
 	def _sfilter(self, s):
 		"""meta escaping, returns quoted string for use in sql statements"""
 		return "\"%s\"" % s.replace("\\","\\\\").replace("\"","\\\"")
 
-
 	def _getitem(self, cpv):
-		try:	self.con.execute("SELECT key, value FROM %s NATURAL JOIN %s "
+		try:
+			self.con.execute("SELECT key, value FROM %s NATURAL JOIN %s "
 			"WHERE label=%s AND cpv=%s" % (self.SCHEMA_PACKAGE_NAME, self.SCHEMA_VALUES_NAME,
 			self.label, self._sfilter(cpv)))
 		except self._BaseError, e:
@@ -105,15 +104,16 @@ class SQLDatabase(template.database):
 		if not rows:
 			raise KeyError(cpv)
 
-		vals = dict([(k,"") for k in self._known_keys])
+		vals = dict((k,"") for k in self._known_keys)
 		vals.update(dict(rows))
 		return vals
-
 
 	def _delitem(self, cpv):
 		"""delete a cpv cache entry
 		derived RDBM classes for this *must* either support cascaded deletes, or 
-		override this method"""
+		override this method
+		"""
+
 		try:
 			try:	
 				self.con.execute("DELETE FROM %s WHERE label=%s AND cpv=%s" % \
@@ -131,16 +131,15 @@ class SQLDatabase(template.database):
 			raise
 
 	def __del__(self):
-		# just to be safe.
-		if "db" in self.__dict__ and self.db != None:
+		if self.db is not None:
 			self.commit()
 			self.db.close()
 
 	def _setitem(self, cpv, values):
-
 		try:
 			# insert.
-			try:	pkgid = self._insert_cpv(cpv)
+			try:
+				pkgid = self._insert_cpv(cpv)
 			except self._BaseError, e:
 				raise cache_errors.CacheCorruption(cpv, e)
 
@@ -152,7 +151,8 @@ class SQLDatabase(template.database):
 					db_values.append({"key":key, "value":values[key]})
 
 			if db_values:
-				try:	self.con.executemany("INSERT INTO %s (pkgid, key, value) VALUES(\"%s\", %%(key)s, %%(value)s)" % \
+				try:
+					self.con.executemany("INSERT INTO %s (pkgid, key, value) VALUES(\"%s\", %%(key)s, %%(value)s)" % \
 					(self.SCHEMA_VALUES_NAME, str(pkgid)), db_values)
 				except self._BaseError, e:
 					raise cache_errors.CacheCorruption(cpv, e)
@@ -170,21 +170,26 @@ class SQLDatabase(template.database):
 		"""uses SCHEMA_INSERT_CPV_INTO_PACKAGE, which must be overloaded if the table definition
 		doesn't support auto-increment columns for pkgid.
 		returns the cpvs new pkgid
-		note this doesn't commit the transaction.  The caller is expected to."""
+		note this doesn't commit the transaction.  The caller is expected to.
+		"""
 		
 		cpv = self._sfilter(cpv)
 		if self._supports_replace:
 			query_str = self.SCHEMA_INSERT_CPV_INTO_PACKAGE.replace("INSERT","REPLACE",1)
 		else:
 			# just delete it.
-			try:	del self[cpv]
-			except	(cache_errors.CacheCorruption, KeyError):	pass
+			try:
+				del self[cpv]
+			except	(cache_errors.CacheCorruption, KeyError):
+				pass
 			query_str = self.SCHEMA_INSERT_CPV_INTO_PACKAGE
+
 		try:
 			self.con.execute(query_str % (self.label, cpv))
 		except self._BaseError:
 			self.db.rollback()
 			raise
+
 		self.con.execute("SELECT pkgid FROM %s WHERE label=%s AND cpv=%s" % \
 			(self.SCHEMA_PACKAGE_NAME, self.label, cpv))
 			
@@ -193,36 +198,39 @@ class SQLDatabase(template.database):
 				" %i matches upon the following select!" % len(rows))
 		return self.con.fetchone()[0]
 
-
 	def __contains__(self, cpv):
 		if not self.autocommits:
-			try:	self.commit()
+			try:
+				self.commit()
 			except self._BaseError, e:
 				raise cache_errors.GeneralCacheCorruption(e)
 
-		try:	self.con.execute("SELECT cpv FROM %s WHERE label=%s AND cpv=%s" % \
+		try:
+			self.con.execute("SELECT cpv FROM %s WHERE label=%s AND cpv=%s" % \
 				(self.SCHEMA_PACKAGE_NAME, self.label, self._sfilter(cpv)))
 		except self._BaseError, e:
 			raise cache_errors.GeneralCacheCorruption(e)
 		return self.con.rowcount > 0
 
-
 	def iterkeys(self):
 		if not self.autocommits:
-			try:	self.commit()
+			try:
+				self.commit()
 			except self._BaseError, e:
 				raise cache_errors.GeneralCacheCorruption(e)
 
-		try:	self.con.execute("SELECT cpv FROM %s WHERE label=%s" % 
+		try:
+			self.con.execute("SELECT cpv FROM %s WHERE label=%s" % 
 				(self.SCHEMA_PACKAGE_NAME, self.label))
 		except self._BaseError, e:
 			raise cache_errors.GeneralCacheCorruption(e)
-#		return [ row[0] for row in self.con.fetchall() ]
+
 		for x in self.con.fetchall():
 			yield x[0]
 
 	def iteritems(self):
-		try:	self.con.execute("SELECT cpv, key, value FROM %s NATURAL JOIN %s "
+		try:
+			self.con.execute("SELECT cpv, key, value FROM %s NATURAL JOIN %s "
 			"WHERE label=%s" % (self.SCHEMA_PACKAGE_NAME, self.SCHEMA_VALUES_NAME,
 			self.label))
 		except self._BaseError, e:
@@ -235,15 +243,16 @@ class SQLDatabase(template.database):
 				if oldcpv != None:
 					d = dict(l)
 					if "_eclasses_" in d:
-						d["_eclasses_"] = reconstruct_eclasses(oldcpv, d["_eclasses_"])
+						d["_eclasses_"] = template.reconstruct_eclasses(oldcpv, d["_eclasses_"])
 					yield cpv, d
 				l.clear()
 				oldcpv = x
 			l.append((y,v))
+
 		if oldcpv != None:
 			d = dict(l)
 			if "_eclasses_" in d:
-				d["_eclasses_"] = reconstruct_eclasses(oldcpv, d["_eclasses_"])
+				d["_eclasses_"] = template.reconstruct_eclasses(oldcpv, d["_eclasses_"])
 			yield cpv, d			
 
 	def commit(self):
@@ -264,10 +273,10 @@ class SQLDatabase(template.database):
 			query = ''
 
 		print "query = SELECT cpv from package_cache natural join values_cache WHERE label=%s %s" % (self.label, query)
-		try:	self.con.execute("SELECT cpv from package_cache natural join values_cache WHERE label=%s %s" % \
+		try:
+			self.con.execute("SELECT cpv from package_cache natural join values_cache WHERE label=%s %s" % \
 				(self.label, query))
 		except self._BaseError, e:
 			raise cache_errors.GeneralCacheCorruption(e)
 
 		return [ row[0] for row in self.con.fetchall() ]
-

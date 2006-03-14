a3c1f1f7b8ffb8eaf82a54f19fe4460a7f5af766
Brian Harring <ferringb@gmail.com>
2006-03-14 02:25:29 -0800

    generalized scrits from marienz

--- tmp-scripts/test-uninstall.sh
+++ tmp-scripts/test-uninstall.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-[ ! -d  /home/bharring/vdb-test/dev-util/bsdiff-4.3 ] && $(dirname "$0")/test-install.sh
-python -c'from portage.vdb import repository;
-v=repository("/home/bharring/vdb-test").trees[0];
+[ ! -d  "${HOME}/vdb-test/dev-util/bsdiff-4.3" ] && $(dirname "$0")/test-install.sh
+python -c"from portage.vdb import repository;
+v=repository('${HOME}/vdb-test').trees[0];
 v.frozen=False;
-p=v["dev-util/bsdiff-4.3"];
-u=v.uninstall(p,offset="/home/bharring/vdb-install");
-u.finish()'
+p=v['dev-util/bsdiff-4.3'];
+u=v.uninstall(p,offset='${HOME}/vdb-install');
+u.finish()"

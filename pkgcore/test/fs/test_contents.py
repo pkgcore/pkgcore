c7ba9090f387c3396119018666d61d50de6a3474
Brian Harring <ferringb@gmail.com>
2006-03-15 07:58:39 -0800

    test suite for pkgcore.fs.contents ; full coverage.

---
+++ pkgcore/test/fs/test_contents.py
@@ -0,0 +1,117 @@
+# Copyright: 2006 Brian Harring <ferringb@gmail.com>
+# License: GPL2
+# $Id:$
+
+from twisted.trial import unittest
+
+from pkgcore.fs import fs, contents
+from pkgcore.util.currying import post_curry
+
+import os
+
+
+class TestContentsSet(unittest.TestCase):
+
+	def __init__(self, *a, **kw):
+		unittest.TestCase.__init__(self, *a, **kw)
+		self.files = [fs.fsFile(x, strict=False) for x in ["/etc/blah", "/etc/foo", 
+			"/etc/dar", "/tmp/dar", "/tmp/blah/foo/long/ass/file/name/but/not/that/bad/really"]]
+		self.dirs = [fs.fsDir(x, strict=False) for x in ["/tmp", "/blah", "/tmp/dar", "/usr/","/usr/bin"]]
+		self.links = [fs.fsLink(x, os.path.dirname(x), strict=False) for x in 
+			["/tmp/foo", "/usr/X11R6/lib", "/nagga/noo"]]
+		self.devs = [fs.fsDev(x, strict=False) for x in 
+			[os.path.join("dev", y) for y in ("sda1", "hda", "hda2", "disks/ide1")]]
+		self.fifos = [fs.fsFifo(x, strict=False) for x in 
+			[os.path.join("tmp", y) for y in ("dar", "boo", "bah")]]
+		self.all = self.dirs + self.links + self.devs + self.fifos
+		
+	def test_init(self):
+		self.assertEquals(len(self.all), len(contents.contentsSet(self.all)))
+		self.assertRaises(TypeError, contents.contentsSet, self.all + [1])
+		contents.contentsSet(self.all, frozen=True)
+		contents.contentsSet(self.all, frozen=False)
+		# test to ensure no one screwed up the optional initials making it mandatory
+		self.assertEquals(len(contents.contentsSet(frozen=True)), 0)
+	
+	def test_add(self):
+		cs = contents.contentsSet(self.files + self.dirs)
+		map(cs.add, self.links)
+		for x in self.links:
+			self.assertIn(x, cs)
+		self.assertEqual(len(cs), len(self.files + self.dirs + self.links))
+		self.assertRaises(AttributeError, lambda:contents.contentsSet(frozen=True).add(self.devs[0]))
+		self.assertRaises(TypeError, cs.add, 1)
+		self.assertRaises(TypeError, cs.add, self.fifos)
+	
+	def test_remove(self):
+		self.assertRaises(KeyError, contents.contentsSet().remove, self.devs[0])
+		self.assertRaises(KeyError, contents.contentsSet().remove, 1)
+		cs = contents.contentsSet(self.all)
+		map(cs.remove, self.all)
+		cs = contents.contentsSet(self.all)
+		map(cs.remove, (x.location for x in self.all))
+		self.assertEqual(len(cs), 0)
+		self.assertRaises(KeyError, cs.remove, self.all[0])
+		self.assertRaises(AttributeError, lambda:contents.contentsSet(frozen=True).remove(self.devs[0]))
+	
+	def test_contains(self):
+		cs = contents.contentsSet()
+		for x in [y[0] for y in [self.files, self.dirs, self.links, self.devs, self.fifos]]:
+			self.assertFalse(x in cs)
+			self.assertFalse(x.location in cs)
+			cs.add(x)
+			self.assertTrue(x in cs)
+			self.assertTrue(x.location in cs)
+			cs.remove(x)
+	
+	def test_clear(self):
+		cs = contents.contentsSet(self.all)
+		self.assertTrue(len(cs))
+		cs.clear()
+		self.assertEqual(len(cs), 0)
+
+	def test_len(self):
+		self.assertEqual(len(contents.contentsSet(self.all)), len(self.all))
+
+	def iterobj(self, name, obj_class=None, forced_name=None):
+		s = set(getattr(self, name))
+		cs = contents.contentsSet(s)
+		s2 = set()
+		if forced_name is None:
+			forced_name = "iter"+name
+		for x in getattr(cs, forced_name)():
+			if obj_class is not None:
+				self.assertTrue(isinstance(x, obj_class))
+			s2.add(x)
+		self.assertEqual(s, s2)
+
+	def listobj(self, name, obj_class=None):
+		valid_list = getattr(self, name)
+		cs = contents.contentsSet(valid_list)
+		test_list = getattr(cs, name)()
+		if obj_class is not None:
+			for x in test_list:
+				self.assertTrue(isinstance(x, obj_class))
+		self.assertEqual(set(test_list), set(valid_list))
+
+	test_iterfiles = post_curry(iterobj, "files", fs.fsFile)
+	test_files = post_curry(listobj, "files", fs.fsFile)
+
+	test_iterdirss = post_curry(iterobj, "dirs", fs.fsDir)
+	test_dirs = post_curry(listobj, "dirs", fs.fsDir)
+
+	test_iterlinks = post_curry(iterobj, "links", fs.fsLink)
+	test_links = post_curry(listobj, "links", fs.fsLink)
+
+	test_iterdevs = post_curry(iterobj, "devs", fs.fsDev)
+	test_devs = post_curry(listobj, "devs", fs.fsDev)
+
+	test_iterfifos = post_curry(iterobj, "fifos", fs.fsFifo)
+	test_fifos = post_curry(listobj, "fifos", fs.fsFifo)
+	
+	test_iter = post_curry(iterobj, "all", forced_name="__iter__")
+
+	def test_check_instance(self):
+		for x in [y[0] for y in [self.files, self.dirs, self.links, self.devs, self.fifos]]:
+			self.assertEqual(x, contents.check_instance(x))
+		self.assertRaises(TypeError, contents.check_instance, 1)

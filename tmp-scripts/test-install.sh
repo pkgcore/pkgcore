6e1db41f3a499c755ef20020c25f0452b57e1ed7
Brian Harring <ferringb@gmail.com>
2006-03-20 10:07:46 -0800

    break down the install script a bit so it's easier for me to hook debugging cruft into it

--- tmp-scripts/test-install.sh
+++ tmp-scripts/test-install.sh
@@ -3,10 +3,13 @@ rm -rf ~/vdb-{install,test}
 mkdir ~/vdb-test
 python -c"import pkgcore.config;from pkgcore.vdb import repository;v=repository('${HOME}/vdb-test').trees[0];
 v.frozen=False;
-p=pkgcore.config.load_config().domain['livefs domain'].repos[0]['dev-util/bsdiff-4.3'].build().finalize();
-i=v.install(p,offset='${HOME}/vdb-install');
+p=pkgcore.config.load_config().domain['livefs domain'].repos[0]['dev-util/bsdiff-4.3'];
+f=p.build().finalize();
+i=v.install(f,offset='${HOME}/vdb-install');
 i.finish();
 assert p.cpvstr in v.versions;
 print v.categories;
 print v.packages;
-print v.versions;"
+print v.versions;
+
+"

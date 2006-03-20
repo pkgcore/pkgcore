4e89d07b40aa29b1bd9df40ceda7c35b725eada7
Brian Harring <ferringb@gmail.com>
2006-03-20 03:20:07 -0800

    add a few asserts and prints so I can verify the operations went as required for vdb

--- tmp-scripts/test-install.sh
+++ tmp-scripts/test-install.sh
@@ -3,6 +3,10 @@ rm -rf ~/vdb-{install,test}
 mkdir ~/vdb-test
 python -c"import pkgcore.config;from pkgcore.vdb import repository;v=repository('${HOME}/vdb-test').trees[0];
 v.frozen=False;
-i=v.install(pkgcore.config.load_config().domain['livefs domain'].repos[0]['dev-util/bsdiff-4.3'].build().finalize(),
-	offset='${HOME}/vdb-install');
-i.finish();"
+p=pkgcore.config.load_config().domain['livefs domain'].repos[0]['dev-util/bsdiff-4.3'].build().finalize();
+i=v.install(p,offset='${HOME}/vdb-install');
+i.finish();
+assert p.cpvstr in v.versions;
+print v.categories;
+print v.packages;
+print v.versions;"
--- tmp-scripts/test-uninstall.sh
+++ tmp-scripts/test-uninstall.sh
@@ -5,4 +5,8 @@ v=repository('${HOME}/vdb-test').trees[0];
 v.frozen=False;
 p=v['dev-util/bsdiff-4.3'];
 u=v.uninstall(p,offset='${HOME}/vdb-install');
-u.finish()"
+u.finish()
+assert p.cpvstr not in v.versions;
+print v.categories;
+print v.packages;
+print v.versions;"

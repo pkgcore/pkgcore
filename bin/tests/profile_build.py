d34ab7af1c5b8ffbff7a2b7de47b710628be0c48
Brian Harring <ferringb@gmail.com>
2006-03-20 20:02:15 -0800

    typo nexon pointed out, Profile -> profile

--- bin/tests/profile_build.py
+++ bin/tests/profile_build.py
@@ -2,4 +2,4 @@
 import sys
 sys.path.insert(0,__file__)
 import profile
-Profile.run("import build_installed_state_graph", "stats.pdb")
+profile.run("import build_installed_state_graph", "stats.pdb")

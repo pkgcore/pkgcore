4acc71248f56e303707b11b9fca3545bd8c4d5ce
Brian Harring <ferringb@gmail.com>
2006-03-23 03:02:02 -0800

    acccept an arg to override the defaults

--- tmp-scripts/test-install.sh
+++ tmp-scripts/test-install.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
-rm -rf ~/vdb-{install,test}
-mkdir ~/vdb-test
+if [ -z "$1" ]; then
+	PKG="dev-util/bsdiff-4.3"
+else
+	PKG="$1"
+fi
+if [ -d ~/vdb-install/"${PKG}" ]; then
+	rm -rf ~/vdb-{install,test}
+fi
+[ -d ~/vdb-install ] && mkdir ~/vdb-test
 python -c"import pkgcore.config;from pkgcore.vdb import repository;v=repository('${HOME}/vdb-test').trees[0];
 v.frozen=False;
-p=pkgcore.config.load_config().domain['livefs domain'].repos[0]['dev-util/bsdiff-4.3'];
+p=pkgcore.config.load_config().domain['livefs domain'].repos[0]['${PKG}'];
 f=p.build().finalize();
 i=v.install(f,offset='${HOME}/vdb-install');
 i.finish();
--- tmp-scripts/test-uninstall.sh
+++ tmp-scripts/test-uninstall.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
-[ ! -d  "${HOME}/vdb-test/dev-util/bsdiff-4.3" ] && $(dirname "$0")/test-install.sh
+if [ -z "$1" ]; then
+	PKG="dev-util/bsdiff-4.3"
+else
+	PKG="$1"
+fi
+[ ! -d  "${HOME}/vdb-test/$kg" ] && $(dirname "$0")/test-install.sh
 python -c"from pkgcore.vdb import repository;
 v=repository('${HOME}/vdb-test').trees[0];
 v.frozen=False;
-p=v['dev-util/bsdiff-4.3'];
+p=v['${PKG}'];
 u=v.uninstall(p,offset='${HOME}/vdb-install');
 u.finish()
 assert p.cpvstr not in v.versions;

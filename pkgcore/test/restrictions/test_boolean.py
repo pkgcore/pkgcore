669d17ea656d75249807bdf651b5d7db0b59c3e7
Brian Harring <ferringb@gmail.com>
2006-03-23 03:35:48 -0800

    updated tests to match current syntax, and fixed whitespace.

--- pkgcore/test/restrictions/test_boolean.py
+++ pkgcore/test/restrictions/test_boolean.py
@@ -8,94 +8,90 @@ from pkgcore.restrictions import boolean, restriction
 from pkgcore.util import mappings
 
 
-true = restriction.AlwaysBool('foo', True)
-false = restriction.AlwaysBool('foo', False)
+true = restriction.AlwaysBool(node_type='foo', negate=True)
+false = restriction.AlwaysBool(node_type='foo', negate=False)
 
 
 class AlwaysForcableBool(boolean.base):
 
-    def force_True(self, action, *args):
-        yield True
+	def force_True(self, action, *args):
+		yield True
 
-    match = force_False = force_True
+	match = force_False = force_True
 
 
 class BaseTest(unittest.TestCase):
 
-    def test_invalid_restrictions(self):
-        self.assertRaises(TypeError, boolean.base, 'foo', 42)
-        base = boolean.base('foo')
-        self.assertRaises(TypeError, base.add_restriction, 42)
-        self.assertRaises(TypeError, base.add_restriction)
+	def test_invalid_restrictions(self):
+		self.assertRaises(TypeError, boolean.base, 42, node_type='foo')
+		base = boolean.base(node_type='foo')
+		self.assertRaises(TypeError, base.add_restriction, 42)
+		self.assertRaises(TypeError, base.add_restriction)
 
-    def test_init_finalize(self):
-        final = boolean.base('foo', true, finalize=True)
-        # TODO perhaps this should be more specific?
-        self.assertRaises(Exception, final.add_restriction, false)
+	def test_init_finalize(self):
+		final = boolean.base(true, node_type='foo', finalize=True)
+		# TODO perhaps this should be more specific?
+		self.assertRaises(Exception, final.add_restriction, false)
 
-    def test_finalize(self):
-        base = boolean.base('foo', true)
-        base.add_restriction(false)
-        base.finalize()
-        # TODO perhaps this should be more specific?
-        self.assertRaises(Exception, base.add_restriction, true)
+	def test_finalize(self):
+		base = boolean.base(true, node_type='foo')
+		base.add_restriction(false)
+		base.finalize()
+		# TODO perhaps this should be more specific?
+		self.assertRaises(Exception, base.add_restriction, true)
 
-    def test_base(self):
-        base = boolean.base('foo', true, false)
-        self.assertEquals(len(base), 2)
-        self.assertEquals(list(base), [true, false])
-        self.assertRaises(NotImplementedError, base.match, false)
-        # TODO is the signature for these correct?
-        self.assertRaises(NotImplementedError, base.force_False, false)
-        self.assertRaises(NotImplementedError, base.force_True, false)
-        self.assertIdentical(base[1], false)
+	def test_base(self):
+		base = boolean.base(true, false, node_type='foo')
+		self.assertEquals(len(base), 2)
+		self.assertEquals(list(base), [true, false])
+		self.assertRaises(NotImplementedError, base.match, false)
+		# TODO is the signature for these correct?
+		self.assertRaises(NotImplementedError, base.force_False, false)
+		self.assertRaises(NotImplementedError, base.force_True, false)
+		self.assertIdentical(base[1], false)
 
-    # TODO total_len? what does it do?
+	# TODO total_len? what does it do?
 
 # TODO these tests are way too limited
 class AndRestrictionTest(unittest.TestCase):
 
-    def test_match(self):
-        self.failUnless(boolean.AndRestriction('foo', true, true).match(None))
-        self.failIf(
-            boolean.AndRestriction('foo', false, true, true).match(None))
-        self.failIf(
-            boolean.AndRestriction('foo', true, false, true).match(None))
+	def test_match(self):
+		self.failUnless(boolean.AndRestriction(true, true, node_type='foo').match(None))
+		self.failIf(
+			boolean.AndRestriction(false, true, true, node_type='foo').match(None))
+		self.failIf(
+			boolean.AndRestriction(true, false, true, node_type='foo').match(None))
 
-    def test_negate_match(self):
-        self.failUnless(
-            boolean.AndRestriction(
-                'foo', false, true, negate=True).match(None))
-        self.failUnless(
-            boolean.AndRestriction(
-                'foo', true, false, negate=True).match(None))
-        self.failUnless(
-            boolean.AndRestriction(
-                'foo', false, false, negate=True).match(None))
-        self.failIf(
-            boolean.AndRestriction(
-                'foo', true, true, negate=True).match(None))
+	def test_negate_match(self):
+		self.failUnless(
+			boolean.AndRestriction(false, true,
+				node_type='foo', negate=True).match(None))
+		self.failUnless(
+			boolean.AndRestriction(true, false, 
+				node_type='foo', negate=True).match(None))
+		self.failUnless(
+			boolean.AndRestriction(false, false, 
+				node_type='foo', negate=True).match(None))
+		self.failIf(
+			boolean.AndRestriction(true, true,
+				node_type='foo', negate=True).match(None))
 
 
 class OrRestrictionTest(unittest.TestCase):
 
-    def test_match(self):
-        self.failUnless(boolean.OrRestriction('foo', true, true).match(None))
-        self.failUnless(
-            boolean.OrRestriction('foo', false, true, false).match(None))
-        self.failUnless(
-            boolean.OrRestriction('foo', true, false, false).match(None))
-        self.failUnless(
-            boolean.OrRestriction('foo', false, false, true).match(None))
-        self.failIf(
-            boolean.OrRestriction('foo', false, false).match(None))
+	def test_match(self):
+		self.failUnless(boolean.OrRestriction(true, true, node_type='foo').match(None))
+		self.failUnless(
+			boolean.OrRestriction(false, true, false, node_type='foo').match(None))
+		self.failUnless(
+			boolean.OrRestriction(true, false, false, node_type='foo').match(None))
+		self.failUnless(
+			boolean.OrRestriction(false, false, true, node_type='foo').match(None))
+		self.failIf(
+			boolean.OrRestriction(false, false, node_type='foo').match(None))
 
-    def test_negate_match(self):
-        self.failIf(
-            boolean.OrRestriction('foo', false, true, negate=True).match(None))
-        self.failIf(
-            boolean.OrRestriction('foo', true, false, negate=True).match(None))
-        self.failIf(
-            boolean.OrRestriction('foo', true, true, negate=True).match(None))
-        self.failUnless(
-            boolean.OrRestriction('foo', false, false, negate=True).match(None))
+	def test_negate_match(self):
+		for x in ((true, false), (false, true), (true,true)):
+			self.failIf(boolean.OrRestriction(node_type='foo', negate=True, *x).match(None))
+		self.failUnless(
+			boolean.OrRestriction(false, false, node_type='foo', negate=True).match(None))
--- pkgcore/test/restrictions/test_restriction.py
+++ pkgcore/test/restrictions/test_restriction.py
@@ -9,23 +9,23 @@ from pkgcore.restrictions import restriction
 
 class BaseTest(unittest.TestCase):
 
-    def test_base(self):
-        base = restriction.base()
-        self.assertEquals(len(base), 1)
-        self.assertRaises(NotImplementedError, str, base)
-        self.assertRaises(NotImplementedError, repr, base)
-        self.assertRaises(NotImplementedError, hash, base)
-        self.assertRaises(NotImplementedError, base.match)
-        self.assertIdentical(None, base.intersect(base))
+	def test_base(self):
+		base = restriction.base()
+		self.assertEquals(len(base), 1)
+		self.assertRaises(NotImplementedError, str, base)
+		self.assertRaises(NotImplementedError, repr, base)
+		self.assertRaises(NotImplementedError, hash, base)
+		self.assertRaises(NotImplementedError, base.match)
+		self.assertIdentical(None, base.intersect(base))
 
 
 class AlwaysBoolTest(unittest.TestCase):
 
-    def test_true(self):
-        true = restriction.AlwaysBool('foo', True)
-        false = restriction.AlwaysBool('foo', False)
-        self.failUnless(true.match(false))
-        self.failIf(false.match(true))
-        self.assertEquals(str(true), "always 'True'")
-        self.assertEquals(str(false), "always 'False'")
-        self.assertNotEqual(hash(true), hash(false))
+	def test_true(self):
+		true = restriction.AlwaysBool('foo', True)
+		false = restriction.AlwaysBool('foo', False)
+		self.failUnless(true.match(false))
+		self.failIf(false.match(true))
+		self.assertEquals(str(true), "always 'True'")
+		self.assertEquals(str(false), "always 'False'")
+		self.assertNotEqual(hash(true), hash(false))

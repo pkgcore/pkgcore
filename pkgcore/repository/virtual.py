4164f739234b5daf7b67d86a4433c84943547a61
Brian Harring <ferringb@gmail.com>
2006-04-16 19:24:20 -0700

    pass in the virtuals pull func to virtuals.tree

--- pkgcore/repository/virtual.py
+++ pkgcore/repository/virtual.py
@@ -1,42 +1,24 @@
 # Copyright: 2005 Jason Stubbs <jstubbs@gentoo.org>
 # License: GPL2
 
-from pkgcore.config import load_config
-from pkgcore.repository import prototype, errors
-from pkgcore.package import metadata
-from pkgcore.package.cpv import CPV
-from pkgcore.package.atom import atom
-from pkgcore.ebuild.conditionals import DepSet
 from pkgcore.restrictions.packages import OrRestriction
-
-from pkgcore.vdb import ondisk
+from pkgcore.repository import prototype
+from pkgcore.package import metadata
 
 class tree(prototype.tree):
 
-	def __init__(self, parent):
+	def __init__(self, grab_virtuals_func):
 		super(tree,self).__init__()
-		if not isinstance(parent, ondisk.tree):
-			raise errors.InitializationError("parent must be a pkgcore.vdb.ondisk repo" + str(type(parent)))
-		self.parent = parent
+		if not callable(grab_virtuals_func):
+			raise TypeError("grab_virtuals_func must be a callable")
+		self._grab_virtuals = grab_virtuals_func
 		self.package_class = factory(self).new_package
 		self._virtuals = None
 
-	def _grab_virtuals(self):
-		self._virtuals = {}
-		for pkg in self.parent:
-			for virtual in pkg.provides.evaluate_depset(pkg.use):
-				self._virtuals.setdefault(virtual.package, {}).setdefault(pkg.fullver, []).append(pkg)
-
-		for pkg_dict in self._virtuals.itervalues():
-			for full_ver, rdep_atoms in pkg_dict.iteritems():
-				if len(rdep_atoms) == 1:
-					pkg_dict[full_ver] = atom("=%s" % rdep_atoms[0])
-				else:
-					pkg_dict[full_ver] = OrRestriction(finalize=True, *[atom("=%s" % x) for x in rdep_atoms])
-
 	def _fetch_metadata(self, pkg):
-		if self._virtuals is None:
-			self._grab_virtuals()
+		if self._grab_virtuals is not None:
+			self._virtuals = self._grab_virtuals()
+			self._grab_virtuals = None
 		return self._virtuals[pkg.package][pkg.fullver]
 
 	def _get_categories(self, *optionalCategory):
@@ -46,8 +28,9 @@ class tree(prototype.tree):
 		return ("virtual",)
 
 	def _get_packages(self, category):
-		if self._virtuals is None:
-			self._grab_virtuals()
+		if self._grab_virtuals is not None:
+			self._virtuals = self._grab_virtuals()
+			self._grab_virtuals = None
 
 		if category == "virtual":
 			return self._virtuals.keys()
@@ -67,6 +50,8 @@ class package(metadata.package):
 		if key == "rdepends":
 			val = self.data
 		elif key == "depends":
+			val = OrRestriction()
+		elif key == "provides":
 			val = OrRestriction()
 		elif key == "metapkg":
 			val = True
--- pkgcore/vdb/__init__.py
+++ pkgcore/vdb/__init__.py
@@ -1,10 +1,25 @@
 # Copyright: 2005 Brian Harring <ferringb@gmail.com>
 # License: GPL2
 
-from pkgcore.repository import multiplex
-from ondisk import tree as vdb_repository
-from virtual import tree as virtualrepository
+from pkgcore.restrictions.packages import OrRestriction
+from pkgcore.repository import multiplex, virtual
+from pkgcore.vdb.ondisk import tree as vdb_repository
+from pkgcore.util.currying import pre_curry
+
+def _grab_virtuals(parent_repo):
+	virtuals = {}
+	for pkg in parent_repo:
+		for virtual in pkg.provides.evaluate_depset(pkg.use):
+			virtuals.setdefault(virtual.package, {}).setdefault(pkg.fullver, []).append(pkg)
+	
+	for pkg_dict in virtuals.itervalues():
+		for full_ver, rdep_atoms in pkg_dict.iteritems():
+			if len(rdep_atoms) == 1:
+				pkg_dict[full_ver] = rdep_atoms[0].versioned_atom
+			else:
+				pkg_dict[full_ver] = OrRestriction(finalize=True, *[x.versioned_atom for x in rdep_atoms])
+	return virtuals
 
 def repository(*args, **kwargs):
 	r = vdb_repository(*args, **kwargs)
-	return multiplex.tree(r, virtualrepository(r))
+	return multiplex.tree(r, virtual.tree(pre_curry(_grab_virtuals, r)))

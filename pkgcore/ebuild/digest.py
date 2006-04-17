3e12cd434e8c863d2050f45ecd3dba0860fc00ad
Brian Harring <ferringb@gmail.com>
2006-04-17 05:22:35 -0700

    unittest for digest parsing (yay), and bug fix- digest was stomping previous chksums per file instead of adding to the existing chksums.

--- pkgcore/ebuild/digest.py
+++ pkgcore/ebuild/digest.py
@@ -13,8 +13,11 @@ def parse_digest(path, throw_errors=True):
 			if len(l) != 4:
 				if throw_errors:
 					raise ParseChksumError(path, "line count was not 4, was %i: '%s'" % (len(l), line))
+
 			#MD5 c08f3a71a51fff523d2cfa00f14fa939 diffball-0.6.2.tar.bz2 305567
-			d[l[2]] = {l[0].lower():l[1], "size":long(l[3])}
+			d.setdefault(l[2], {})[l[0].lower()] = l[1]
+			if "size" not in d[l[2]]:
+				d[l[2]]["size"] = long(l[3])
 		f.close()
 	except (OSError, IOError, TypeError), e:
 		raise ParseChksumError("failed parsing " + path, e)

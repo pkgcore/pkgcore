9aec4c2d0268a1739ecf7c09e6511fd9a49eadc8
Brian Harring <ferringb@gmail.com>
2006-04-19 08:31:40 -0700

    sightly renaming slotting problem to pigeonholing (since it's the more general issue there), introducing an obj to track the pigeonholing; since the # of holes may not be known, allows for contraints to be added (restriction.base derivatives) that are checked when introducing a new slotting/pkg in.

---
+++ pkgcore/graph/pigeonholes.py
@@ -0,0 +1,45 @@
+# Copyright: 2006 Brian Harring <ferringb@gmail.com>
+# License: GPL2
+
+from pkgcore.restrictions import restriction
+from pkgcore.util.compatibility import any, all
+
+# lil too getter/setter like for my tastes...
+
+class PigeonHoledSlots(object):
+
+	def __init__(self):
+		self.slot_dict = {}
+		
+	def fill_slotting(self, obj):
+		key = obj.key
+		for x in self.slot_dict.setdefault(key, []):
+			if isinstance(x, restriction.base):
+				if x.match(obj):
+					# no go.  blocker.
+					return x
+			else:
+				if x.slot == obj.slot:
+					return x
+		self.slot_dict[key].append(obj)
+		return None
+			
+	def add_limiter(self, atom):
+		if not isinstance(atom, restriction.base):
+			raise TypeError("atom must be a restriction.base derivative")
+		# debug.
+		if any(atom is x for x in self.slot_dict.get(atom.key, [])):
+			raise KeyError("%s is already in %s: %s" % (atom, atom.key, self.slot_dict[atom.key]))
+		self.slot_dict.setdefault(atom.key, []).append(atom)
+		
+	def remove_slotting(self, obj):
+		key = obj.key
+		# let the key error be thrown if they screwed up.
+		l = [x for x in self.slot_dict[key] if x is not obj]
+		if len(l) == len(self.slot_dict[key]):
+			raise KeyError("obj %s isn't slotted" % obj)
+		if l:
+			self.slot_dict[key] = l
+		else:
+			del self.slot_dict[key]
+
---
+++ pkgcore/test/graph/test_pigeonholes.py
@@ -0,0 +1,53 @@
+# Copyright: 2006 Brian Harring <ferringb@gmail.com>
+# License: GPL2
+
+from twisted.trial import unittest
+from pkgcore.graph.pigeonholes import PigeonHoledSlots
+from pkgcore.test.graph.test_choice_point import fake_package
+from pkgcore.restrictions import restriction
+
+class fake_blocker(restriction.base):
+	def __init__(self, key, blocks=[]):
+		if not isinstance(blocks, (list, tuple)):
+			blocks = [blocks]
+		self.key, self.blocks = key, blocks
+	
+	def __str__(self):
+		return "fake_atom(%s, %s)" % self.key, self.blocks
+	
+	def match(self, obj):
+		for x in self.blocks:
+			if x is obj:
+				return True
+		return False
+
+class SlotTesting(unittest.TestCase):
+
+	def test_add(self):
+		c = PigeonHoledSlots()
+		o = fake_package()
+		self.assertEqual(None, c.fill_slotting(o))
+		self.assertEqual(o, c.fill_slotting(fake_package()))
+		self.assertEqual(None, c.fill_slotting(fake_package(slot=1, key=1)))
+
+	def test_add_limiter(self):
+		c = PigeonHoledSlots()
+		p = fake_package()
+		o = fake_blocker(None, p)
+		c.add_limiter(o)
+		self.assertEqual(o, c.fill_slotting(p))
+		# note we're doing 'is' tests in fake_blocker
+		self.assertEqual(None, c.fill_slotting(fake_package()))
+
+	def test_remove_slotting(self):
+		c = PigeonHoledSlots()
+		p, p2 = fake_package(), fake_package(slot=2)
+		o = fake_blocker(None, p)
+		c.add_limiter(o)
+		c.remove_slotting(o)
+		self.assertRaises(KeyError, c.remove_slotting, o)
+		self.assertEqual(None, c.fill_slotting(p))
+		self.assertEqual(None, c.fill_slotting(p2))
+		c.remove_slotting(p)
+		c.remove_slotting(p2)
+		

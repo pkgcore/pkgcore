8cb4819ddd48b302dbf78bacb797c64a8e902076
Brian Harring <ferringb@gmail.com>
2006-05-07 02:59:56 -0700

    fleshed out a bit further...

--- pkgcore/graph/linearize.py
+++ pkgcore/graph/linearize.py
@@ -1,22 +1,25 @@
 import itertools
+from collections import dequeue
 from pkgcore.util.compatibility import any, all
 from pkgcore.util.iterables import caching_iter
 from pkgcore.graph.pigeonholes import PigeonHoledSlots
-from pkgcore.graph.resolver import resolver
+from pkgcore.graph.choice_point import choice_point
 from pkgcore.package.atom import atom
 from pkgcore.util.currying import pre_curry
 
-INSTALL = 1
-REMOVE = 0
-PREEXISTING = 2
+MERGED  = 0
+MERGE   = 1
+UNMERGE = 2
+REPLACE = 3
 
 class entry(object):
-	def __init__(self, atom, pkg, entry_type):
+	def __init__(self, atom, data, entry_type, preblocks):
 		self.type = entry_Type
 		self.deps = []
 		self.revdeps = set()
 		self.atom = atom
-		self.pkg = pkg
+		self.pkg = data
+		self.preblocks = preblocks
 
 	def register_parent(self, entry):
 		self.revdeps.add(entry)
@@ -28,8 +31,37 @@ class entry(object):
 		return bool(self.revdeps)
 
 
-class entrylist(list):
+class InconsistantState(Exception):
 	pass
+
+
+class InsolubleSolution(Exception):
+	pass
+
+
+class entrylist(list):
+	def generate_state(self):
+		state = PigeonHoledSlots()
+		for idx, step in enumerate(self):
+			if step.type in (MERGED, MERGE):
+				l = state.fill_slotting(step.data.pkg)
+			elif step.type == UNMERGE:
+				# sanity check.  ensure what we're unmerging is in the graph.
+				assert step.data.pkg in state
+				state.remove_slotting(step.data.pkg)
+				assert step.data.pkg not in state
+				l = None
+			elif step.type == REPLACE:
+				# assert the target is in the state
+				assert step.data[0] in state
+				assert step.data[1] not in state
+				state.remove_slotting(step.data[0])
+				l = state.fill_slotting(step.data[1])
+			else:
+				raise AssertionError("step %s idx %d is of unknown type" % (step, idx))
+			if l:
+				raise InconsistantState("entry %s, idx %d conflicts with state %s" % (step, idx, l))
+		return state
 
 
 class merge_plan(object):
@@ -41,13 +73,14 @@ class merge_plan(object):
 			dbs = [dbs]
 		self.db, self.vdb = dbs, vdb
 		self.cached_queries = {}
-		self.buildplan = entrylist()
+		self.plan = entrylist()
 		self.forced_atoms = set()
-		self.resolver = resolver()
-		#should probably proxy self to avoid cycles...
-		self.get_matches = pre_curry(self, pkg_selection_strategy)
+
+		self.pkg_selection_strategy = pkg_selection_strategy
 		self.tail_state = PigeonHoledSlots()
 		self.load_vdb_blockers = load_initial_vdb_state
+		self.insoluble = set()
+		self.atom_cache = {}
 
 	def _load_vdb_blockers(self):
 		print "(re)loading vdb rdepends blockers..."
@@ -66,18 +99,91 @@ class merge_plan(object):
 	
 	def add_atom(self, atom):
 		if atom not in self.forced_atoms:
-			self.forced_atoms.add(atom)
-			self.resolver.add_root_atom(atom)
-
-	def iterate_unresolved_atoms(self):
-		return self.resolver.iterate_unresolved_atoms()
-
-	def satisfy_atom(self, *a):
-		return self.resolver.satisfy_atom(*a)
-
-	def linearize(self, plan=None):
-		state = PigeonHoledSlots()
+			self._rec_add_atom(atom)
+			ret = self.forced_atoms.add(atom)
+			if ret is not True:
+				self.regen_state()
+			
+	def _rec_add_atom(self, atom):
+		"""returns false on no issues (inserted succesfully), else a list of the stack that screwed it up"""
+		if atom in self.insoluble:
+			return [atom]
+		l = self.tail_state.find_atom_matches(atom)
+		if l:
+			return False
+		# not in the plan thus far.
 		
+		choices = choice_point(atom, self.get_db_matches(atom))
+		# ignore what dropped out, at this juncture we don't care.
+		choices.reduce_atoms(self.insoluble)
+		if not choices:
+			# and was intractable because it has a hard dep on an unsolvable atom.
+			self.insoluble.add(atom)
+			return [atom]
+
+		satisfied = True
+		blocks = []
+		while choices:
+			satisfied=True
+			additions, blocks, nolonger_used = [], [], []
+			for datom in choices.pkg.depends:
+				if datom.blocks:
+					# don't register, just do a scan.  and this sucks because any insertions prior to this won't get
+					# hit by the blocker
+					l = self.tail_state.find_atom_matches(datom)
+					if l:
+						print "depends blocker messing with us- dumping to pdb for inspection"
+						import pdb;pdb.set_trace()
+						raise Exception("whee, damn depends blockers")
+				else:
+					ret = self._rec_add_atom(datom)
+					if ret:
+						# reduce.
+						nolonger_used = choices.reduce(datom)
+						satisfied = False
+						break
+					additions.append(datom)
+			if satisfied:
+				for ratom in choices.pkg.rdepends:
+					if ratom.blocks:
+						# level blockers after resolution of this node- blocker may block something that is required 
+						# only as depends for a node required for rdepends
+						blocks.append(ratom)
+					else:
+						ret = self._rec_add_atom(ratom)
+						if ret:
+							# reduce.
+							nolonger_used = choices.reduce(ratom)
+							satisfied = False
+							break
+					additions.append(ratom)
+
+			if not satisified:
+				# need to clean up blockers here... cleanup our additions in light of reductions from choices.reduce
+				print "dirty dirty little boy!  skipping cleaning"
+			else:
+				break
+		if choices:
+			# well, we got ourselvs a resolution.
+			self.plan.append(choices)
+			# level blockers.
+			live_blocks = {}
+			
+			
+	def regen_state(self):
+		# brute force.
+		self.tail_state = self.plan.generate_state()	
+
+	def get_db_matches(self, atom):
+		if atom in self.insoluble:
+			return []
+		matches = self.atom_cache.get(atom, None):
+		if matches is None:
+			matches = self.pkg_selection_strategy(self, atom)
+			if not isinstance(matches, caching_iter):
+				matches = caching_iter(matches)
+			self.atom_cache[atom] = matches
+		return matches
 
 	# selection strategies for atom matches
 	@staticmethod

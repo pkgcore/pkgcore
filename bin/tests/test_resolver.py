ca8d325b838ffe9c852866e3bdfdfa604a1b648a
Brian Harring <ferringb@gmail.com>
2006-05-09 01:49:08 -0700

    rename.

--- bin/tests/test_resolver.py
+++ bin/tests/test_resolver.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python
-# Copyright: 2006 Brian Harring <ferringb@gmail.com>
-# License: GPL2
 
-import itertools
-from pkgcore.graph import resolver
 from pkgcore.config import load_config
+from pkgcore.graph import linearize
 from pkgcore.package.atom import atom
 from pkgcore.util.lists import flatten
+
 
 def pop_paired_args(args, arg, msg):
 	rets = []
@@ -41,14 +39,19 @@ if __name__ == "__main__":
 	import sys
 	args = sys.argv[1:]
 
-	resolver.debug_whitelist.extend(pop_paired_args(args, ["--debug", "-d"], "debug filter to enable"))
-	set_targets = pop_paired_args(args, ["--set", "-s"], "pkg sets to enable")
-
 	trigger_pdb = pop_arg(args, "-p", "--pdb")
 	empty_vdb = pop_arg(args, "-e", "--empty")
-	
-	conf=load_config()
+	upgrade = pop_arg(args, "-u", "--upgrade")
+	if upgrade:
+		strategy = linearize.merge_plan.prefer_highest_version_strategy
+	else:
+		strategy = linearize.merge_plan.prefer_reuse_strategy
 
+	deep = bool(pop_arg(args, "-D", "--deep"))
+
+	conf = load_config()
+
+	set_targets = pop_paired_args(args, ["--set", "-s"], "pkg sets to enable")
 	if set_targets:
 		print "using pkgset(s): %s" % (", ".join("'%s'" % x.strip() for x in set_targets))
 	set_targets = flatten([map(atom, getattr(conf, "%s_pkgset" % l)[l]) for l in set_targets], atom)
@@ -61,31 +64,16 @@ if __name__ == "__main__":
 			atoms = [atom("sys-apps/portage")]
 	else:
 		atoms = [atom(x) for x in args] + set_targets
-	
+
 	domain = conf.domain["livefs domain"]
-	v,repo = domain.vdb[0], domain.repos[0]
-
-	r = resolver.resolver()
-	map(r.add_root_atom, reversed(atoms))
-
-	lasta = None
-	count = 0
-	for a in r.iterate_unresolved_atoms():
-		count += 1
-		if a.blocks:
+	vdb, repo = domain.vdb[0], domain.repos[0]
+	resolver = linearize.merge_plan(vdb, repo, pkg_selection_strategy=strategy, verify_vdb=deep)
+	print "calling resolve"
+	for x in atoms:
+		ret = resolver.add_atom(x)
+		if not ret:
+			print "ret was",ret
+			print "resolution failed"
 			import pdb;pdb.set_trace()
-			print "caught blocker"
-			
-		resolver.debug("    unresolved atom: %s" % a)
-		if a is lasta:
-			import pdb;pdb.set_trace()
-		if empty_vdb:
-			r.satisfy_atom(a, itertools.chain(sorted(repo.itermatch(a)), v.itermatch(a)))
-		else:
-			r.satisfy_atom(a, itertools.chain(v.itermatch(a), sorted(repo.itermatch(a))))
-		lasta = a
-		print "loop %i" % count
 
-	if trigger_pdb:
-		import pdb
-		pdb.set_trace()
+	print "successfull, 'parently"

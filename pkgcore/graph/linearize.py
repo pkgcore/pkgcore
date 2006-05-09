4cfbf630c9fd8fb752f1255a55b6c8b862d22f47
Brian Harring <ferringb@gmail.com>
2006-05-08 22:44:47 -0700

    more experimentation. rdep cycle breaking via forcing to vdb is in, as is mangling of blockers to disallow blocking against the provider of the blocker (ie, pkg x-1 can't level an rdep block against =x-1)

--- pkgcore/graph/linearize.py
+++ pkgcore/graph/linearize.py
@@ -5,7 +5,7 @@ from pkgcore.util.iterables import caching_iter
 from pkgcore.graph.pigeonholes import PigeonHoledSlots
 from pkgcore.graph.choice_point import choice_point
 from pkgcore.util.currying import pre_curry
-from pkgcore.restrictions import packages, values, boolean
+from pkgcore.restrictions import packages, values, boolean, restriction
 
 REMOVE  = 0
 ADD     = 1
@@ -15,6 +15,7 @@ FORWARD_BLOCK = 3
 class nodeps_repo(object):
 	def __init__(self, repo):
 		self.__repo = repo
+
 	def itermatch(self, *a, **kwds):
 		return (nodeps_pkg(x) for x in self.__repo.itermatch(*a, **kwds))
 	
@@ -23,11 +24,14 @@ class nodeps_repo(object):
 
 
 class nodeps_pkg(object):
-	def __init__(self, pkg):
+	def __init__(self, pkg, overrides={"depends":packages.AndRestriction(finalize=True), 
+		"rdepends":packages.AndRestriction(finalize=True)}):
 		self._pkg = pkg
+		self._overrides = overrides
+		
 	def __getattr__(self, attr):
-		if attr in ("depends", "rdepends"):
-			return packages.AndRestriction(finalize=True)
+		if attr in self._overrides:
+			return self._overrides[attr]
 		return getattr(self._pkg, attr)
 
 	def __cmp__(self, other):
@@ -91,7 +95,7 @@ class merge_plan(object):
 				return False
 		return True
 
-	def _rec_add_atom(self, atom, current_stack):
+	def _rec_add_atom(self, atom, current_stack, depth=0, limit_to_vdb=False):
 		"""returns false on no issues (inserted succesfully), else a list of the stack that screwed it up"""
 		if atom in self.insoluble:
 			return [atom]
@@ -99,9 +103,9 @@ class merge_plan(object):
 		if l:
 			return False
 		# not in the plan thus far.
-		matches = self.get_db_matches(atom)
+		matches = self.get_db_matches(atom, depth=depth, limit_to_vdb=limit_to_vdb)
 		if matches:
-			choices = choice_point(atom, self.get_db_matches(atom))
+			choices = choice_point(atom, matches)
 			# ignore what dropped out, at this juncture we don't care.
 			choices.reduce_atoms(self.insoluble)
 			if not choices:
@@ -114,7 +118,7 @@ class merge_plan(object):
 		current_stack.append(atom)
 
 		blocks = []
-		print "processing      ",atom
+		print "processing       %s%s" % (depth *2 * " ", atom)
 		while choices:
 			satisfied = True
 			additions, blocks, nolonger_used = [], [], []
@@ -132,8 +136,11 @@ class merge_plan(object):
 						# cycle.
 #						new_atom = packages.AndRestriction(datom, packages.Restriction("depends", 
 #							values.ContainmentMatch(datom, 
-						import pdb;pdb.set_trace()
-					failure = self._rec_add_atom(datom, current_stack)
+#						import pdb;pdb.set_trace()
+						# reduce our options.
+						failure = True
+					else:
+						failure = self._rec_add_atom(datom, current_stack, depth=depth+1)
 					if failure:
 						# reduce.
 						nolonger_used = choices.reduce_atoms(datom)
@@ -142,15 +149,17 @@ class merge_plan(object):
 					additions.append(datom)
 			if satisfied:
 				for ratom in choices.rdepends:
-					if ratom in current_stack:
-						# cycle.  whee.
-						pass
 					if ratom.blocks:
 						# level blockers after resolution of this node- blocker may block something that is required 
 						# only as depends for a node required for rdepends
 						blocks.append(ratom)
 					else:
-						failure = self._rec_add_atom(ratom, current_stack)
+						if ratom in current_stack:
+							# cycle.  whee.
+							print "ratom cycle",ratom,current_stack
+							failures = self._rec_add_atom(ratom, current_stack, depth=depth+1, limit_to_vdb=True)
+						else:
+							failure = self._rec_add_atom(ratom, current_stack, depth=depth+1)
 						if failure:
 							# reduce.
 							nolonger_used = choices.reduce(ratom)
@@ -165,7 +174,7 @@ class merge_plan(object):
 				break
 
 		if not choices:
-			print "found no solution for %s" % atom
+			print "found no solution for %s%s" % (depth *2 * " ", atom)
 			current_stack.pop()
 			return [atom] + failure
 
@@ -174,54 +183,87 @@ class merge_plan(object):
 		if l:
 			# this means in this branch of resolution, someone slipped something in already.
 			# cycle, basically.
-			print "was trying to insert atom %s pkg %s, but %s exists already" (atom, choices.current_pkg, l)
+			print "was trying to insert atom %s pkg %s, but %s exists already" % (atom, choices.current_pkg, l)
+			# hack.  see if what was insert is enough for us.
+			l2 = self.state.match_atom(atom)
+			if l2:
+				current_stack.pop()
+				return False
+			
 			import pdb;pdb.set_trace()
+
 		# level blockers.
 		for x in blocks:
-			l = self.state.add_blocker(x)
+			# hackity hack potential- say we did this-
+			# disallowing blockers from blocking what introduced them.
+			# iow, we can't block ourselves (can block other versions, but not our exact self)
+			# this might be suspect mind you...
+			# disabled, but something to think about.
+
+			l = self.state.add_blocker(self.generate_mangled_blocker(choices, x), key=x.key)
 			if l:
 				# blocker caught something. yay.
 				print "rdepend blocker %x hit %s for atom %s pkg %s" % (x, l, atom, choices_current.pkg)
 				import pdb;pdb.set_trace()
+
+		for x in choices.provides:
+			l = self.state.add_provider(choices, x)
+			if l:
+				print "provider conflicted... how?"
+				import pdb;pdb.set_trace()
 		current_stack.pop()
 		return False
 	
-	def get_db_matches(self, atom):
-		print "querying db for ",atom
+	def get_db_matches(self, atom, depth=0, limit_to_vdb=False):
+		if limit_to_vdb:
+			dbs = []
+		else:
+			dbs = self.db
+		print "querying db for  %s%s" % (depth*2*" ", atom)
 		if atom in self.insoluble:
 			return []
 		matches = self.atom_cache.get(atom, None)
-		if matches is None:
-			matches = self.pkg_selection_strategy(self, atom)
+		# hack.
+		if matches is None or limit_to_vdb:
+			matches = self.pkg_selection_strategy(self, self.vdb, dbs, atom)
 			if not isinstance(matches, caching_iter):
 				matches = caching_iter(matches)
 			self.atom_cache[atom] = matches
 		return matches
 
+	def generate_mangled_blocker(self, choices, blocker):
+		"""converts a blocker into a "cannot block ourself" block"""
+		new_atom =	packages.AndRestriction(
+				packages.PackageRestriction("actual_pkg", 
+					restriction.FakeType(choices.current_pkg.versioned_atom, values.value_type),
+					negate=True),
+				blocker, finalize=True)
+		return new_atom			 
+
 	# selection strategies for atom matches
 	@staticmethod
-	def prefer_highest_version_strategy(self, atom):
-		return caching_iter(itertools.chain(*[r.itermatch(atom) for r in self.db + [self.vdb]]), sorted)
+	def prefer_highest_version_strategy(self, vdb, dbs, atom):
+		return caching_iter(itertools.chain(*[r.itermatch(atom) for r in dbs + [vdb]]), sorted)
 
 	@staticmethod
-	def prefer_reuse_strategy(self, atom):
+	def prefer_reuse_strategy(self, vdb, dbs, atom):
 		return caching_iter(itertools.chain(
-			self.vdb.itermatch(atom), 
-			caching_iter(itertools.chain(*[r.itermatch(atom) for r in self.db]), sorted)
+			vdb.itermatch(atom), 
+			caching_iter(itertools.chain(*[r.itermatch(atom) for r in dbs]), sorted)
 		))
 
 	@staticmethod
-	def force_max_strategy(self, atom):
+	def force_max_strategy(self, vdb, dbs, atom):
 		try:
-			yield max(self.vdb.match(atom) + [pkg for repo in self.db for pkg in db.itermatch(atom)])
+			yield max(vdb.match(atom) + [pkg for repo in dbs for pkg in repo.itermatch(atom)])
 		except ValueError:
 			# max throws it if max([]) is called.
 			yield []
 
 	@staticmethod
-	def force_min_strategy(self, atom):
+	def force_min_strategy(self, vdb, dbs, atom):
 		try:
-			yield min(self.vdb.match(atom) + [pkg for repo in self.db for pkg in db.itermatch(atom)])
+			yield min(vdb.match(atom) + [pkg for repo in dbs for pkg in repo.itermatch(atom)])
 		except ValueError:
 			# max throws it if max([]) is called.
 			yield []
@@ -233,23 +275,31 @@ class plan_state(object):
 		self.plan = []
 	
 	def add_pkg(self, choices, action=ADD):
+		return self._add_pkg(choices, choices.current_pkg, action)
+	
+	def add_provider(self, choices, provider, action=ADD):
+		return self._add_pkg(choices, provider, action)
+	
+	def _add_pkg(self, choices, pkg, action):
 		"""returns False (no issues), else the conflicts"""
 		if action == REMOVE:
 			# level it even if it's not existant?
-			self.state.remove_slotting(choices.current_pkg)
+			self.state.remove_slotting(pkg)
 			self.plan.append((action, choices))
 		elif action == ADD:
-			l = self.state.fill_slotting(choices.current_pkg)
+			l = self.state.fill_slotting(pkg)
 			if l:
 				return l
 			self.plan.append((action, choices))
 		return False
 		
-	def add_blocker(self, blocker):
+	def add_blocker(self, blocker, key=None):
 		"""adds blocker, returning any packages blocked"""
-		l = self.state.add_limiter(blocker)
+		l = self.state.add_limiter(blocker, key=key)
 		self.plan.append((FORWARD_BLOCK, blocker))
 		return l
 
+
 	def match_atom(self, atom):
 		return self.state.find_atom_matches(atom)
+

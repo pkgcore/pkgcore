8297eb3ffb89937ab0543e5333e323c7d188777b
Brian Harring <ferringb@gmail.com>
2006-05-09 01:43:35 -0700

    still's fugly, but is behaving fair sight better then before (backtracking enabled also)

--- pkgcore/graph/linearize.py
+++ pkgcore/graph/linearize.py
@@ -7,10 +7,6 @@ from pkgcore.graph.choice_point import choice_point
 from pkgcore.util.currying import pre_curry
 from pkgcore.restrictions import packages, values, boolean, restriction
 
-REMOVE  = 0
-ADD     = 1
-REPLACE = 2
-FORWARD_BLOCK = 3
 
 class nodeps_repo(object):
 	def __init__(self, repo):
@@ -51,7 +47,8 @@ class merge_plan(object):
 	
 	def __init__(self, vdb, dbs, pkg_selection_strategy=None, load_initial_vdb_state=True, verify_vdb=False):
 		if pkg_selection_strategy is None:
-			pkg_selection_strategy = self.prefer_highest_version_strategy
+#			pkg_selection_strategy = self.prefer_highest_version_strategy
+			pkg_selection_strategy = self.prefer_reuse_strategy
 		if not isinstance(dbs, (list, tuple)):
 			dbs = [dbs]
 		self.db, self.vdb = dbs, vdb
@@ -115,7 +112,15 @@ class merge_plan(object):
 			self.insoluble.add(atom)
 			return [atom]
 
+		# experiment. ;)
+		# see if we can insert or not at this point (if we can't, no point in descending)
+		l = self.state.pkg_conflicts(choices.current_pkg)
+		if l:
+			# we can't.
+			return [atom]
+		
 		current_stack.append(atom)
+		saved_state = self.state.current_state()
 
 		blocks = []
 		print "processing       %s%s" % (depth *2 * " ", atom)
@@ -128,7 +133,8 @@ class merge_plan(object):
 					# hit by the blocker
 					l = self.state.match_atom(datom)
 					if l:
-						print "depends blocker messing with us- dumping to pdb for inspection"
+						print "depends blocker messing with us- dumping to pdb for inspection of atom %s, pkg %s, ret %s" % \
+							(atom, choices.current_pkg, l)
 						import pdb;pdb.set_trace()
 						raise Exception("whee, damn depends blockers")
 				else:
@@ -157,25 +163,28 @@ class merge_plan(object):
 						if ratom in current_stack:
 							# cycle.  whee.
 #							print "ratom cycle",ratom,current_stack
-							failures = self._rec_add_atom(ratom, current_stack, depth=depth+1, limit_to_vdb=True)
+							failure = self._rec_add_atom(ratom, current_stack, depth=depth+1, limit_to_vdb=True)
 						else:
 							failure = self._rec_add_atom(ratom, current_stack, depth=depth+1)
 						if failure:
 							# reduce.
-							nolonger_used = choices.reduce(ratom)
+							nolonger_used = choices.reduce_atoms(ratom)
 							satisfied = False
 							break
 					additions.append(ratom)
 
 			if not satisfied:
 				# need to clean up blockers here... cleanup our additions in light of reductions from choices.reduce
-				print "dirty dirty little boy!  skipping cleaning"
+#				print "dirty dirty little boy!  skipping cleaning",additions
+				print "reseting for     %s%s" % (depth*2*" ", atom)
+				self.state.reset_state(saved_state)
 			else:
 				break
 
 		if not choices:
 			print "found no solution for %s%s" % (depth *2 * " ", atom)
 			current_stack.pop()
+			self.state.reset_state(saved_state)
 			return [atom] + failure
 
 		# well, we got ourselvs a resolution.
@@ -183,14 +192,19 @@ class merge_plan(object):
 		if l:
 			# this means in this branch of resolution, someone slipped something in already.
 			# cycle, basically.
-			print "was trying to insert atom %s pkg %s, but %s exists already" % (atom, choices.current_pkg, l)
+			print "was trying to insert atom '%s' pkg '%s',\nbut '%s' exists already" % (atom, choices.current_pkg, l)
 			# hack.  see if what was insert is enough for us.
 			l2 = self.state.match_atom(atom)
 			if l2:
 				current_stack.pop()
 				return False
 			
-			import pdb;pdb.set_trace()
+#			import pdb;pdb.set_trace()
+			import time
+			time.sleep(3)
+			self.state.reset_state(saved_state)
+			current_stack.pop()
+			return [atom]
 
 		# level blockers.
 		for x in blocks:
@@ -219,12 +233,12 @@ class merge_plan(object):
 			dbs = []
 		else:
 			dbs = self.db
-		print "querying db for  %s%s" % (depth*2*" ", atom)
 		if atom in self.insoluble:
 			return []
 		matches = self.atom_cache.get(atom, None)
 		# hack.
 		if matches is None or limit_to_vdb:
+			print "querying db for  %s%s" % (depth*2*" ", atom)
 			matches = self.pkg_selection_strategy(self, self.vdb, dbs, atom)
 			if not isinstance(matches, caching_iter):
 				matches = caching_iter(matches)
@@ -269,6 +283,10 @@ class merge_plan(object):
 			# max throws it if max([]) is called.
 			yield []
 
+REMOVE  = 0
+ADD     = 1
+REPLACE = 2
+FORWARD_BLOCK = 3
 
 class plan_state(object):
 	def __init__(self):
@@ -286,21 +304,45 @@ class plan_state(object):
 		if action == REMOVE:
 			# level it even if it's not existant?
 			self.state.remove_slotting(pkg)
-			self.plan.append((action, choices))
+			self.plan.append((action, choices, pkg))
 		elif action == ADD:
 			l = self.state.fill_slotting(pkg)
 			if l:
 				return l
-			self.plan.append((action, choices))
+			self.plan.append((action, choices, pkg))
 		return False
 		
 	def add_blocker(self, blocker, key=None):
 		"""adds blocker, returning any packages blocked"""
 		l = self.state.add_limiter(blocker, key=key)
-		self.plan.append((FORWARD_BLOCK, blocker))
+		self.plan.append((FORWARD_BLOCK, blocker, key))
 		return l
 
 
 	def match_atom(self, atom):
 		return self.state.find_atom_matches(atom)
 
+	def pkg_conflicts(self, pkg):
+		return self.state.get_conflicting_slot(pkg)
+
+	def current_state(self):
+		#hack- this doesn't work when insertions are possible
+		return len(self.plan)
+	
+	def reset_state(self, state_pos):
+		assert state_pos <= len(self.plan)
+		if len(self.plan) == state_pos:
+			return
+		# should revert, rather then force a re-run
+		ps = plan_state()
+		for x in self.plan[:state_pos]:
+			if x[0] == FORWARD_BLOCK:
+				ps.add_blocker(x[1], key=x[2])
+			elif x[0] in (REMOVE, ADD):
+				ps._add_pkg(x[1], x[2], x[0])
+			else:
+				print "unknown %s encountered in rebuilding state" % str(x)
+				import pdb;pdb.set_trace()
+				pass
+		self.plan = ps.plan
+		self.state = ps.state

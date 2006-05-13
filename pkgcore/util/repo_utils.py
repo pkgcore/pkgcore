9b911897f11729f5c16754ae4198ad2006f9cbd9
Brian Harring <ferringb@gmail.com>
2006-05-13 07:35:01 -0700

    bunch 'o features.  ability to pull apart repository wrappers, simple funcs, but they do the job. add --virtuals-only and --disable-virtuals ; added sanity checks for mutually exclusive args.

---
+++ pkgcore/util/repo_utils.py
@@ -0,0 +1,29 @@
+# Copyright: 2006 Brian Harring <ferringb@gmail.com>
+# License: GPL2
+
+from pkgcore.repository import virtual
+
+def get_raw_repos(repo):
+	"""returns a list of raw repos found.
+	repo can be either a repo instance, or a list
+	"""
+	if isinstance(repo, (list, tuple)):
+		l = []
+		map(l.extend, (get_raw_repos(x) for x in repo))
+		return l
+	while getattr(repo, "raw_repo"):
+		repo = repo.raw_repo
+	if hasattr(repo, "trees"):
+		l = []
+		map(l.extend, (get_raw_repos(x) for x in repo.trees))
+		return l
+	return [repo]
+
+def get_virtual_repos(repo, sentinel=True):
+	"""select only virtual repos
+	repo can be either a list, or a repo to descend through.
+	if sentinel is False, will select all non virtual repos
+	"""
+	if not isinstance(repo, (tuple, list)):
+		repo = get_raw_repos(repo)
+	return [x for x in repo if isinstance(x, virtual.tree) == sentinel]

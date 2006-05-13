16ca9e9f56daf6b6bcf53ac59be43d6e25d2b201
Brian Harring <ferringb@gmail.com>
2006-05-13 16:59:23 -0700

    simple lil script to trigger regen. oh yeah, regen is 2x faster now. And... this supports a single arg, # of threads to run.  quad machine, speedup (comparsed to stable) is 6x, despite the damn GIL. Take that portage >:)

---
+++ bin/utilities/regen.py
@@ -0,0 +1,74 @@
+#!/usr/bin/python
+import threading, Queue
+import sys
+import time
+from pkgcore.config import load_config
+
+def regen_iter(iterable):
+	for x in iterable:
+		try:
+			x.keywords
+		except RuntimeError:
+			raise
+		except Exception, e:
+			print "caught exception %s for %s" % (e, x)
+
+def reclaim_threads(threads):
+	for x in threads:
+		try:
+			x.join()
+		except RuntimeError:
+			raise
+		except Exception, e:
+			print "caught exception %s reclaiming thread" % e
+
+
+if __name__ == "__main__":
+	if len(sys.argv) > 2:
+		print "need a single optional arg, # of threads to spawn"
+		sys.exit(1)
+	elif len(sys.argv) == 2:
+		try:
+			thread_count = int(sys.argv[1])
+			if thread_count < 1:
+				raise ValueError
+		except ValueError:
+			print "arg must be an integer, and greater then 0"
+			sys.exit(1)
+	else:
+		thread_count = 1
+	repo = load_config().repo["rsync repo"]
+	start_time = time.time()
+	if thread_count == 1:
+		regen_iter(repo)
+	else:
+		queue = Queue.Queue()
+		kill = threading.Event()
+		kill.clear()
+		def iter_queue(kill, qlist, timeout=0.25):
+			while not kill.isSet():
+				try:
+					yield qlist.get(timeout=timeout)
+				except Queue.Empty:
+					continue
+		regen_threads = [threading.Thread(target=regen_iter, args=(iter_queue(kill, queue),)) for x in xrange(thread_count)]
+		print "starting %d thread" % thread_count
+		try:
+			for x in regen_threads:
+				x.start()
+			print "started"
+			# now we feed the queue.
+			for pkg in repo:
+				queue.put(pkg)
+		except Exception:
+			kill.set()
+			reclaim_threads(regen_threads)
+			raise			
+
+		# by now, queue is fed.  reliable for our uses since the queue is only subtracted from.
+		while not queue.empty():
+			time.sleep(.5)
+		kill.set()
+		reclaim_threads(regen_threads)
+		assert queue.empty()
+	print "finished in %.2f seconds" % (time.time() - start_time)

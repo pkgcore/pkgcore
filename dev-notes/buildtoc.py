875ea4b0e9900e8614ffb6bc2394bbc8b37e2bfc
Brian Harring <ferringb@gmail.com>
2006-05-13 18:34:30 -0700

    added subdir cleaning/making, and made toc able to look in first level subdirs for a toc.

--- dev-notes/buildtoc.py
+++ dev-notes/buildtoc.py
@@ -2,7 +2,7 @@
 
 """Spit out a restructuredtext file linking to every .rst in cwd."""
 
-import os
+import os,stat
 
 
 def main():
@@ -16,7 +16,9 @@ def main():
 		if entry.lower().endswith('.rst'):
 			entry = entry[:-4]
 			print '- `%s <%s.html>`_' % (entry, entry)
-
+		elif stat.S_ISDIR(os.stat(entry).st_mode):
+			if os.path.exists(os.path.join(entry, "toc.rst")):
+				print '- `%s <%s.html>`_' % (entry, "%s/toc.rst" % entry)
 
 if __name__ == '__main__':
 	main()

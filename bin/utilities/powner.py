5e32e4382ff4469362eba86570d239a7836a29d4
Brian Harring <ferringb@gmail.com>
2006-05-16 09:55:42 -0700

    quicky script to do contents queries.

---
+++ bin/utilities/powner.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+
+import sys, time
+from pkgcore.restrictions import packages, values
+from pkgcore.config import load_config
+from pkgcore.fs.util import normpath
+
+if __name__ == "__main__":
+	if len(sys.argv) == 1:
+		print "need at least one arg, file to find the owner of"
+		print "Multiple args are further restrictions on a match- pkg must own all of the files"
+	repo = load_config().domain["livefs domain"].vdb[0]
+	restrict = packages.PackageRestriction("contents", values.ContainmentMatch(
+		*[normpath(x) for x in sys.argv[1:]]))
+	start_time = time.time()
+	count = 0 
+	print "query- %s" % restrict
+	for pkg in repo.itermatch(restrict):
+		print "pkg: %s" % (pkg)
+		count += 1
+	print "found %i matches in %.2f seconds" % (count, time.time() - start_time)

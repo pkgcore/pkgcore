1370eca79b9ef109be8a68d5fc57b9a395b1e55b
Brian Harring <ferringb@gmail.com>
2006-05-15 20:43:47 -0700

    code guidelines cleanup.

--- pkgcore/interfaces/data_source.py
+++ pkgcore/interfaces/data_source.py
@@ -8,24 +8,23 @@ class base(object):
 		raise NotImplementedError
 	get_path = get_data
 
+
 class local_source(base):
 	__slots__ = ("path",)
 
 	def __init__(self, path):
 		self.path = path
 
-
 	def get_path(self):
 		if os.path.exists(self.path):
 			return self.path
 		return None
 
-
 	def get_data(self):
-		if self.path == None:
+		if self.path is None:
 			return None
 		try:
-			f = open(fp, "r")
+			f = open(fp, "r", 32768)
 			d = f.read()
 			f.close()
 			return d

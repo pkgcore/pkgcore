86482ecc339a50b57048df57f6e8cbbf0b7f4957
Brian Harring <ferringb@gmail.com>
2006-05-21 05:04:13 -0700

    ahh damn, forgot you.

---
+++ pkgcore/config/domain.py
@@ -0,0 +1,8 @@
+# Copyright: 2006 Brian Harring <ferringb@gmail.com>
+# License: GPL2
+
+
+# yes this is basically empty.  will fill it out as the base is better identified.
+
+class domain(object):
+	pass

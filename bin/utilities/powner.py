faa6b694bb07396fef01cffa67f0c45c4d9de937
Brian Harring <ferringb@gmail.com>
2006-05-21 05:47:09 -0700

    add --all || -a, and convert over to return first match as a default

--- bin/utilities/powner.py
+++ bin/utilities/powner.py
@@ -5,20 +5,36 @@ from pkgcore.restrictions import packages, values
 from pkgcore.config import load_config
 from pkgcore.fs.util import normpath
 
+def grab_arg(arg, args):
+	val = False
+	try:
+		while True:
+			args.remove(arg)
+			val = True
+	except ValueError:
+		pass
+	return val
+	
 if __name__ == "__main__":
-	if len(sys.argv) == 1 or "--help" in sys.argv[1:] or "-h" in sys.argv[1:]:
+	a = sys.argv[1:]
+	if grab_arg("--help", a) or grab_arg("-h", a) or not a:
 		print "need at least one arg, file to find the owner of"
+		print "default matching mode is return after first match, however if [ --all || -a ] is specified"
+		print "all owners are return"
 		print "Multiple args are further restrictions on a match- pkg must own all of the files"
-		sys.exit(2)
+		sys.exit(1)
+	all = grab_arg("-a", a) or grab_arg("--all", a)
 	repo = load_config().domain["livefs domain"].vdb[0]
 	restrict = packages.PackageRestriction("contents", values.ContainmentMatch(
 		*[normpath(x) for x in sys.argv[1:]]))
 	start_time = time.time()
 	count = 0 
-	print "query- %s" % restrict
+	print "query- %s, returning all matches? %s" % (restrict, all)
 	for pkg in repo.itermatch(restrict):
 		print "pkg: %s" % (pkg)
 		count += 1
+		if not all:
+			break
 	print "found %i matches in %.2f seconds" % (count, time.time() - start_time)
 	if count:
 		sys.exit(0)

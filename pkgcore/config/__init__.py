be81e67bb9edea85381acbb2ab6f33c10a6ca1db
Brian Harring <ferringb@gmail.com>
2006-05-26 04:20:35 -0700

    enable make.conf translator if /etc/portage/config ain't found.

--- pkgcore/config/__init__.py
+++ pkgcore/config/__init__.py
@@ -1,9 +1,7 @@
 # Copyright: 2005 Brian Harring <ferringb@gmail.com>
 # License: GPL2
 
-
 import os
-
 from pkgcore.config import central, cparser, errors
 from pkgcore.const import DEFAULT_CONF_FILE, GLOBAL_CONF_FILE, CONF_DEFAULTS
 
@@ -17,14 +15,16 @@ def load_config(local_conf_file=DEFAULT_CONF_FILE,
 
 	returns the generated configuration object representing the system config.
 	"""
+	types_def = cparser.configTypesFromIni(open(types_file))
 	if os.path.isfile(global_conf_file) and os.path.isfile(local_conf_file):
 		c = central.ConfigManager(
-			[cparser.configTypesFromIni(open(types_file))],
+			[types_def],
 			[cparser.configFromIni(open(local_conf_file)),
 			cparser.configFromIni(open(global_conf_file)) ])
 	else:
 		# make.conf...
-		raise errors.BaseException("sorry, config file %r or %r doesn't exist,"
-			" and I don't like make.conf currently (I'm working out my "
-			" issues however)" % (local_conf_file, global_conf_file))
+		from pkgcore.ebuild.portage_conf import configFromMakeConf
+		c = central.ConfigManager([types_def], 
+			[configFromMakeConf()]
+			)
 	return c

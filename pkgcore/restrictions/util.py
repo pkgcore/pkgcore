5530870f828241c563c4b06d9ccd8f367f6c1a09
Brian Harring <ferringb@gmail.com>
2006-06-03 04:48:59 -0700

    use getattr instead, slower but it's saner about missing attrs

--- pkgcore/restrictions/util.py
+++ pkgcore/restrictions/util.py
@@ -1,14 +1,12 @@
 # Copyright: 2006 Brian Harring <ferringb@gmail.com>
 # License: GPL2
 
-from operator import attrgetter
 from pkgcore.util.lists import iter_flatten
 from pkgcore.util.containers import InvertedContains
 from pkgcore.restrictions import packages, boolean
 
-_restriction_type_getter = attrgetter("type")
 def _is_package_instance(inst):
-	return _restriction_type_getter(inst) == packages.package_type and not isinstance(inst, boolean.base)
+	return getattr(inst, "type", None) == packages.package_type and not isinstance(inst, boolean.base)
 
 def collect_package_restrictions(restrict, attrs=None):
 	"""walks a restriction, descending as neccessary and returning any PackageRestrictions that work

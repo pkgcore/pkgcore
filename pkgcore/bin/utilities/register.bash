d1095f747bc89029ba78cd67fe36c9c5c81114c9
Brian Harring <ferringb@gmail.com>
2006-07-07 05:24:37 -0700

    typo.

--- pkgcore/bin/utilities/register.bash
+++ pkgcore/bin/utilities/register.bash
@@ -2,7 +2,7 @@
 export PYTHONPATH="../../../"
 
 ./register_plugin.py -s fs_ops copyfile 1 pkgcore.fs.ops.default_copyfile
-/register_plugin.py -s fs_ops ensure_perms 1 pkgcore.fs.ops.default_ensure_perms
+./register_plugin.py -s fs_ops ensure_perms 1 pkgcore.fs.ops.default_ensure_perms
 ./register_plugin.py -s fs_ops mkdir 1 pkgcore.fs.ops.default_mkdir
 ./register_plugin.py -s format ebuild_built 0.0 pkgcore.ebuild.ebuild_built.generate_new_factory
 ./register_plugin.py -s format ebuild_src 0.0 pkgcore.ebuild.ebuild_src.generate_new_factory

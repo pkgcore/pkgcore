333e36797c8a70878b15b3f2ca007e85477c15ec
Brian Harring <ferringb@gmail.com>
2006-07-11 05:19:51 -0700

    add all_repos and all_vdbs attribute that is a multiplexed tree of repos/vdbs

--- pkgcore/config/domain.py
+++ pkgcore/config/domain.py
@@ -6,8 +6,18 @@ base class to derive from for domain objects
 
 Bit empty at the moment
 """
+from pkgcore.util.demandload import demandload
+demandload(globals(), "pkgcore.repository:multiplex")
 
 # yes this is basically empty.  will fill it out as the base is better identified.
 
 class domain(object):
-	pass
+	
+	def __getattr__(self, attr):
+		if attr == "all_repos":
+			a = self.repos = multiplex.tree(*self.repos)
+		elif attr == "all_vdbs":
+			a = self.all_vdbs = multiplex.tree(*self.vdb)
+		else:
+			raise AttributeError(attr)
+		return a

73a63c44306fd7a1149b42bfb72790addf43bf5e
Brian Harring <ferringb@gmail.com>
2006-07-13 20:39:03 -0700

    do the force_true on the configured pkg, not the raw pkg

--- pkgcore/repository/configured.py
+++ pkgcore/repository/configured.py
@@ -59,8 +59,8 @@ class tree(prototype.tree):
 		filtered_restrict = OrRestriction(disable_inst_caching=True,
 			*[AndRestriction(disable_inst_caching=True, *x) for x in filtered_solutions])
 
-		return (self.package_class(pkg) for pkg in self.raw_repo.itermatch(filtered_restrict, 
-			restrict_solutions=filtered_solutions, **kwds) if restrict.force_True(pkg))
+		return (pkg for pkg in (self.package_class(x) for x in self.raw_repo.itermatch(filtered_restrict, 
+			restrict_solutions=filtered_solutions, **kwds)) if restrict.force_True(pkg))
 
 	itermatch.__doc__ = prototype.tree.itermatch.__doc__.replace("@param", "@keyword").replace("@keyword restrict:", "@param restrict:")
 

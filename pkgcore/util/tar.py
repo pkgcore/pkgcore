d3e05d6e6c7527a57deeab6fdfd565d5d5596d6a
Brian Harring <ferringb@gmail.com>
2006-07-25 22:12:21 -0700

    monkey patch tarfile to reduce memory usage

---
+++ pkgcore/util/tar.py
@@ -0,0 +1,26 @@
+# Copyright: 2006 Brian Harring <ferringb@gmail.com>
+# License: GPL2
+
+# monkey patching of stdlib tarfile to reduce mem usage (33% reduction).
+
+import sys
+t=sys.modules.pop("tarfile", None)
+tarfile = __import__("tarfile")
+if t is not None:
+	sys.modules["tarfile"] = t
+else:
+	del sys.modules["tarfile"]
+del t
+# ok, we now have our own local copy to monkey patch
+
+class TarInfo(tarfile.TarInfo):
+	__slots__ = ("name", "mode", "uid", "gid", "size", "mtime", "chksum", "type",
+		"linkname", "uname", "gname", "devmajor", "devminor", "prefix", "offset",
+		"offset_data")
+
+tarfile.TarInfo = TarInfo
+# finished monkey patching.  now to lift things out of our tarfile module into this scope so from/import behaves properly.
+
+for x in tarfile.__all__:
+	locals()[x] = getattr(tarfile, x)
+del x

756597997a0f21c27460e4761f3fb21aa30717f9
Brian Harring <ferringb@gmail.com>
2006-08-08 03:30:01 -0700

    actually enable the speedup...

--- pkgcore/util/osutils/__init__.py
+++ pkgcore/util/osutils/__init__.py
@@ -1,7 +1,6 @@
 cpy_listdir = cpy_listdir_files = cpy_listdir_dirs = native_listdir = native_listdir_dirs = native_listdir_files = None
 
 try:
-	raise ImportError
 	from _readdir import cpy_listdir, cpy_listdir_files, cpy_listdir_dirs
 	listdir = cpy_listdir
 	listdir_dirs = cpy_listdir_dirs

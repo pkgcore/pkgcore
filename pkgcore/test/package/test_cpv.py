8a14b4f6a24de13bcdf33c74ce6b48c7e8c386b5
Brian Harring <ferringb@gmail.com>
2006-08-08 23:21:47 -0700

    add a few more tests...

--- pkgcore/test/package/test_cpv.py
+++ pkgcore/test/package/test_cpv.py
@@ -6,11 +6,23 @@ from pkgcore.package import cpv, errors
 
 
 class CpvTest(unittest.TestCase):
+	goodcpv = ['dev-util/diffball', 'dev-util/diffball-0.7.1',
+		'dev-util/diffball-cvs.2006']
+	for x in ('alpha', 'beta', 'p', 'pre', 'rc'):
+		goodcpv += ['dev-util/diffball-2.1_'+x, 'dev-util/diffball-2.1_%s12' % x]
+
 
 	def test_cpv(self):
 		for brokencpv in [
 			'dev/one-2.8^&{/ILikeCookies',
 			'two-2.5',
 			'three',
+			'dev-util/-diffball',
+			'dev-util/diffball-cvs.cvs',
+			'dev-util/diffball-2.2_alphaa',
+			'dev-util/diffball-2.2_alpha_2.2_alpha',
 			]:
 			self.assertRaises(errors.InvalidCPV, cpv.CPV, brokencpv)
+		
+		for goodcpv in self.goodcpv:
+			cpv.CPV(goodcpv)

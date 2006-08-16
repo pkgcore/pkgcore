d38ca572c2bde0a1eb6081d1c7a0925dcbd8adc2
Brian Harring <ferringb@gmail.com>
2006-08-16 01:41:59 -0700

    clean up the wrapper repository a bit.  still needs work, but saner then previous...

--- pkgcore/repository/wrapper.py
+++ pkgcore/repository/wrapper.py
@@ -9,9 +9,9 @@ simple repository wrapping to override the package instances returned
 # ~harring
 import prototype, errors
 
-class wrapperTree(prototype.tree):
+class tree(prototype.tree):
 	"""wrap an existing repository filtering results based upon passed in restrictions."""
-	def __init__(self, repo, package_class):
+	def __init__(self, repo, package_class=None):
 		"""
 		@param repo: L{pkgcore.repository.prototype.tree} instance to wrap
 		@param package_class: callable to yield the package instance
@@ -20,4 +20,21 @@ class wrapperTree(prototype.tree):
 		if not isinstance(self.raw_repo, prototype.tree):
 			raise errors.InitializationError("%s is not a repository tree derivative" % str(self.raw_repo))
 		self.package_class = package_class
-		self.raw_repo = raw_repo
+		self.raw_repo = repo
+
+	def itermatch(self, *args, **kwargs):
+		if self.package_class is None:
+			return self.raw_repo.itermatch(*args, **kwargs)
+		return (self.package_class(x) for x in self.raw_repo.itermatch(*args, **kwargs))
+
+	def __getattr__(self, attr):
+		return getattr(self.raw_repo, repo)
+
+	def __len__(self):
+		return len(self.raw_repo)
+	
+	def __iter__(self):
+		if self.package_class is None:
+			return iter(self.raw_repo)
+		return (self.package_class(x) for x in self.raw_repo)
+		

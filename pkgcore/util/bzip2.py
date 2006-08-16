65d57d638182d2fafbbd9cac42610ca36a494934
Brian Harring <ferringb@gmail.com>
2006-08-16 01:54:52 -0700

    docstrings

--- pkgcore/util/bzip2.py
+++ pkgcore/util/bzip2.py
@@ -1,6 +1,17 @@
 # Copyright: 2005 Brian Harring <ferringb@gmail.com>
 # License: GPL2
 
+"""
+bzip2 decompression/compression
+
+where possible, this module defers to cpython bz2 module- if it's not available,
+it results to executing bzip2 with tempfile arguements to do decompression 
+and compression.
+
+Should use this module unless its absolutely critical that bz2 module be used
+"""
+
+
 from pkgcore.util.demandload import demandload
 demandload(globals(), "tempfile pkgcore.spawn:find_binary,spawn_get_output")
 

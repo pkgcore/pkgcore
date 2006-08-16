432f412064a53605a7725fbbf37482dfa6838dab
Brian Harring <ferringb@gmail.com>
2006-08-16 02:46:08 -0700

    derive from contentsSet, prefer tar ordering for yielding.

--- pkgcore/binpkg/tar.py
+++ pkgcore/binpkg/tar.py
@@ -7,20 +7,33 @@ binpkg tar utilities
 
 from pkgcore.util.tar import TarFile
 from pkgcore.fs.fs import fsFile, fsDir, fsSymLink, fsDev, fsFifo
-from pkgcore.fs.contents import contentsSet
+from pkgcore.fs import contents
+from pkgcore.util.mappings import OrderedDict
+
+class TarContentsSet(contents.contentsSet):
+	
+	def __init__(self, initial=None, frozen=False):
+		contents.contentsSet.__init__(self)
+		self._dict = OrderedDict()
+		if initial is not None:
+			for x in initial:
+				self.add(x)
+		self.frozen = frozen
+
+
+def converter(member):
+	d = {"uid":member.uid, "gid":member.gid, "mtime":member.mtime, "mode":member.mode}
+	location = member.name
+	if member.isdir():
+		return fsDir(location, **d)
+	elif member.isreg():
+		d["size"] = long(member.size)
+		return fsFile(location, **d)
+	elif member.issym():
+		return fsSymLink(location, member.linkname, **d)
+	else:
+		print "skipping",member
 
 def generate_contents(path):
 	t = TarFile.bz2open(path, mode="r")
-	def converter(member):
-		d = {"uid":member.uid, "gid":member.gid, "mtime":member.mtime, "mode":member.mode}
-		location = member.name
-		if member.isdir():
-			return fsDir(location, **d)
-		elif member.isreg():
-			d["size"] = long(member.size)
-			return fsFile(location, **d)
-		elif member.issym():
-			return fsSymLink(location, member.linkname, **d)
-		else:
-			print "skipping",member
-	return contentsSet((converter(x) for x in t.getmembers()), frozen=True)
+	return TarContentsSet((converter(x) for x in t), frozen=True)

23fc03ecd91cb1ca5f6f4f29cbf01c8a2012a193
Brian Harring <ferringb@gmail.com>
2006-08-19 05:05:33 -0700

    force mutable on contentsSet.__init__, reset mutable attribute after setting initial

--- pkgcore/binpkg/tar.py
+++ pkgcore/binpkg/tar.py
@@ -20,7 +20,7 @@ class tar_data_source(data_source):
 class TarContentsSet(contents.contentsSet):
 	
 	def __init__(self, initial=None, mutable=False):
-		contents.contentsSet.__init__(self)
+		contents.contentsSet.__init__(self, mutable=True)
 		self._dict = OrderedDict()
 		if initial is not None:
 			for x in initial:

12b4d4f8e70a28a2721ad623ae8e335722378a97
Brian Harring <ferringb@gmail.com>
2006-08-20 12:46:06 -0700

    mangle the piss out of configured's itermatch so it properly forces expansion when required. fair bit slower, but will optimize it another day

--- pkgcore/repository/configured.py
+++ pkgcore/repository/configured.py
@@ -7,6 +7,7 @@ wrap a repository, binding configuration to pkgs returned from the repository
 
 from pkgcore.util.compatibility import any
 from pkgcore.restrictions.packages import PackageRestriction, OrRestriction, AndRestriction
+from pkgcore.restrictions import boolean
 from pkgcore.restrictions.util import collect_package_restrictions
 from pkgcore.repository import prototype
 from pkgcore.package.conditionals import PackageWrapper
@@ -40,26 +41,55 @@ class tree(prototype.tree):
 	def __getattr__(self, attr):
 		return getattr(self.raw_repo, attr)
 
-	def itermatch(self, restrict, restrict_solutions=None, **kwds):
+	def itermatch(self, restrict, **kwds):
 		if not any(True for r in collect_package_restrictions(restrict, self.attr_filters)):
-			return (self.package_class(pkg) for pkg in self.raw_repo.itermatch(restrict, restrict_solutions=restrict_solutions, **kwds))
+			return (self.package_class(pkg) for pkg in self.raw_repo.itermatch(restrict, **kwds))
 			
-		if restrict_solutions is None:
-			if hasattr(restrict, "iter_cnf_solutions"):
-				restrict_solutions = restrict.cnf_solutions()
+		def transform_solutions_array(array):
+			if not array:
+				return None
+			elif len(array) == 1:
+				if not array[0]:
+					return None
+				elif len(array[0]) == 1:
+					restrict = array[0][0]
+				else:
+					restrict = AndRestriction(disable_inst_caching=True, *array[0])
 			else:
-				restrict_solutions = (restrict,)
-		filtered_solutions = [
-			[a for a in x if not (isinstance(a, PackageRestriction) and a.attr in self.attr_filters)]
-			for x in restrict_solutions]
-
-		# disable inst_caching for this restriction.  it's a one time generation, and potentially
+				restrict = OrRestriction(disable_inst_caching=True,
+					*[AndRestriction(disable_inst_caching=True, *filter(None, x)) for x in array if x])
+			return restrict
+		
+		if hasattr(restrict, "cnf_solutions"):
+			restrict_solutions = []
+			for and_block in restrict.cnf_solutions():
+				l = []
+				for node in and_block:
+					if isinstance(node, PackageRestriction):
+						if node.attr in self.attr_filters:
+							continue
+					elif isinstance(node, boolean.base):
+						node = [
+							[a for a in filter(None, x) if a and not (isinstance(a, PackageRestriction) and
+								a.attr in self.attr_filters)]
+							for x in node.dnf_solutions(full_solution_expansion=True)]
+						
+						node = transform_solutions_array(node)
+						if node is None:
+							continue
+					l.append(node)
+				if l:
+					restrict_solutions.append(l)
+			filtered_restrict = transform_solutions_array(restrict_solutions)
+			del restrict_solutions
+		else:
+			filtered_restrict = restrict
+		
+		# disable inst_caching for these restrictions.  it's a one time generation, and potentially
 		# quite costly for hashing
-		filtered_restrict = OrRestriction(disable_inst_caching=True,
-			*[AndRestriction(disable_inst_caching=True, *x) for x in filtered_solutions if x])
 
 		return (pkg for pkg in (self.package_class(x) for x in self.raw_repo.itermatch(filtered_restrict, 
-			restrict_solutions=filtered_solutions, **kwds)) if restrict.force_True(pkg))
+			**kwds)) if restrict.force_True(pkg))
 
 	itermatch.__doc__ = prototype.tree.itermatch.__doc__.replace("@param", "@keyword").replace("@keyword restrict:", "@param restrict:")
 

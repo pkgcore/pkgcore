557856b3f424f1b92519d29e0ca7fe47fc874f8e
Brian Harring <ferringb@gmail.com>
2006-08-20 14:04:05 -0700

    add groupby operator to split iterable feeds into per pkg iterators

--- pkgcore/util/packages.py
+++ pkgcore/util/packages.py
@@ -1,8 +1,15 @@
 # Copyright: 2006 Brian Harring <ferringb@gmail.com>
 # License: GPL2
 
+import itertools, operator
+
 def get_raw_pkg(pkg):
 	p = pkg
 	while hasattr(p, "_raw_pkg"):
 		p = p._raw_pkg
 	return p
+
+groupby_key_getter = operator.attrgetter("key")
+def groupby_pkg(iterable):
+	for key, pkgs in itertools.groupby(iterable, groupby_key_getter):
+		yield pkgs

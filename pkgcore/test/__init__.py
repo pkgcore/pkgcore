a2af1c06b0586f9e5a59dee0e510453f424275fb
Brian Harring <ferringb@gmail.com>
2006-08-24 21:05:18 -0700

    rather then just disabling demandload (which isn't doable due to a few cycles), add a walker that finds demandloaded objs and triggers their lookup

--- pkgcore/test/__init__.py
+++
@@ -1,2 +0,0 @@
-import pkgcore.util.demandload
-pkgcore.util.demandload.demandload = pkgcore.util.demandload.disabled_demandload
---
+++ pkgcore/test/test_demandload_usage.py
@@ -0,0 +1,33 @@
+# Copyright: 2005 Brian Harring <ferringb@gmail.com>
+# License: GPL2
+
+from pkgcore.util import demandload
+import sys
+
+from twisted.trial import unittest
+
+class TestDemandLoadTargets(unittest.TestCase):
+	
+	matching_types = [type(getattr(demandload, x)) for x in ("_replacer_from", "_replacer", "_importer", "_delayed_compiler")]
+	
+	def test_demandload_targets(self):
+		# picks up only the namespace loaded for searching
+		remaining = sys.modules.items()
+		seen = set()
+		while remaining:
+			for name, mod in remaining:
+				self.check_space(name, mod)
+				seen.add(name)
+			remaining = [(k, v) for k,v in sys.modules.iteritems() if k not in seen]
+		
+	def check_space(self, name, mod):
+		if not name.startswith("pkgcore."):
+			return
+		for attr in dir(mod):
+			try:
+				obj = getattr(mod, attr)
+				# force __getattribute__ to fire
+				getattr(obj, "__class__", None)
+			except ImportError, ie:
+				# hit one.
+				self.fail("failed 'touching' demandloaded %s.%s" % (name, attr))

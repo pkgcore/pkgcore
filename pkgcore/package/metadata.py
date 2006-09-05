0e75a8097cdc08bc6772cf0164a6dca671cd34b5
Brian Harring <ferringb@gmail.com>
2006-09-05 04:06:36 -0700

    make metadata into a mixin func.

--- pkgcore/package/metadata.py
+++ pkgcore/package/metadata.py
@@ -14,59 +14,74 @@ demandload(globals(), "warnings")
 from pkgcore.package.cpv import CPV
 from pkgcore.package.atom import atom
 
-class package(CPV):
+def DeriveMetadataKls(original_kls):
+    if getattr(original_kls, "_derived_metadata_kls", False):
+        return original_kls
+    
+    class package(CPV):
+        _derived_metadata_kls = True
+        built = False
+        try:
+            __doc__ = "package class with metadata bound to it for attribute " \
+                "generation\n\n" + \
+                     "\n".join(x.lstrip()
+                          for x in original_kls.__doc__.split("\n")
+                          if "@ivar" in x or "@cvar" in x)
+            __doc__ += "\n@ivar repo: parent repository"
+        except AttributeError:
+            # wee, must be in -OO mode.
+            __doc__ = None
 
-    built = False
-    try:
-        __doc__ = "package class with metadata bound to it for attribute generation\n\n" + \
-            "\n".join(x.lstrip()
-                      for x in CPV.__doc__.split("\n")
-                      if "@ivar" in x or "@cvar" in x)
-        __doc__+="\n@ivar repo: parent repository"
-    except AttributeError:
-        # wee, must be in -OO mode.
-        __doc__ = None
-    immutable = True
-    package_is_real = True
+        immutable = True
+        package_is_real = True
 
-    _get_attr = dict(CPV._get_attr)
+        _get_attr = dict(original_kls._get_attr)
 
-    def __init__(self, parent_repository, cpv):
-        """
-        @param cpv: cpv string to parse.  Gentoo specific, should be abstracted out
-        @param parent_repository: parent repository this package belongs to
-        @type parent_repository: L{pkgcore.repository.prototype.tree} instance
-        """
-        CPV.__init__(self, cpv)
-        self.__dict__["_parent"] = parent_repository
+        def __init__(self, parent_repository, *a, **kwds):
+            """
+            wrapper for %s.__init__; see %s.__init__ for allowed args/kwds, 
+                they're passed directly to it
 
-    def _get_data(self):
-        """internal hook func to get the packages metadata, consumer of L{_get_attr}"""
-        if "data" in self.__dict__:
-            warnings.warn("odd, got a request for data yet it's in the dict")
-            return self.__dict__["data"]
+            @param parent_repository: parent repository this package belongs to
+            @type parent_repository: L{pkgcore.repository.prototype.tree} 
+                instance
+            """
+            original_kls.__init__(self, *a, **kwds)
+            self.__dict__["_parent"] = parent_repository
 
-        return self._fetch_metadata()
-    _get_attr["data"] = _get_data
+        def _get_data(self):
+            """
+            internal hook func to get the packages metadata, consumer
+            of L{_get_attr}
+            """
+            if "data" in self.__dict__:
+                warnings.warn("odd, got a request for data yet it's in the dict")
+                return self.__dict__["data"]
 
-    @property
-    def repo(self):
-        return self._parent._parent_repo
+            return self._fetch_metadata()
+        _get_attr["data"] = _get_data
 
-    @property
-    def slotted_atom(self):
-        return atom("%s:%s" % (self.key, self.slot))
+        @property
+        def repo(self):
+            return self._parent._parent_repo
 
-    def _fetch_metadata(self):
-        """
-        pull the metadata for this package.
-        must be overrode in derivative
-        """
-        raise NotImplementedError
+        @property
+        def slotted_atom(self):
+            return atom("%s:%s" % (self.key, self.slot))
 
-    def add_format_triggers(self, op_inst, format_op_inst, engine_inst):
-        pass
+        def _fetch_metadata(self):
+            """
+            pull the metadata for this package.
+            must be overrode in derivative
+            """
+            raise NotImplementedError
 
+        def add_format_triggers(self, op_inst, format_op_inst, engine_inst):
+            pass
+
+    return package
+    
+package = DeriveMetadataKls(CPV)
 
 class factory(object):
 

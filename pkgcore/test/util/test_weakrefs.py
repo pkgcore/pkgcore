8d922863f3a53e05c98051b7c4176c8192aa0a8e
Brian Harring <ferringb@gmail.com>
2006-09-09 01:00:12 -0700

    cpython WeakValCache tests

---
+++ pkgcore/test/util/test_weakrefs.py
@@ -0,0 +1,36 @@
+# Copyright: 2006 Brian Harring <ferringb@gmail.com>
+# License: GPL2
+
+from twisted.trial import unittest
+from pkgcore.util.weakrefs import WeakValCache
+from weakref import WeakValueDictionary
+
+class RefObj(object):
+    pass
+
+class TestWeakValCache(unittest.TestCase):
+    if WeakValueDictionary is WeakValCache:
+        skip = "WeakValCache is weakref.WeakValueDictionary; indicates " \
+            "pkgcore.util._caching isn't compiled"
+    
+    def setUp(self):
+        self.o = RefObj()
+        self.w = WeakValCache()
+    
+    def test_setitem(self):
+        s = "asdf"
+        self.w[s] = self.o
+        self.w["fds"] = self.o
+        self.w[s] = self.o
+    
+    def test_getitem(self):
+        s = "asdf"
+        self.w[s] = self.o
+        self.assertIdentical(self.w[s], self.o)
+
+    def test_expiring(self):
+        s = "asdf"
+        self.w[s] = self.o
+        self.assertTrue(self.w[s])
+        del self.o
+        self.assertRaises(KeyError, self.w.__getitem__, s)

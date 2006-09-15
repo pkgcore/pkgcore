8a4365e15c7ff7432f11c2b0dd49930bef68e4e6
Brian Harring <ferringb@gmail.com>
2006-09-15 10:02:10 -0700

    don't use 2.5 only syntax...

--- pkgcore/ebuild/digest.py
+++ pkgcore/ebuild/digest.py
@@ -11,29 +11,30 @@ from pkgcore.util.obj import make_SlottedDict_kls
 def parse_digest(path, throw_errors=True, kls_override=dict):
     d = kls_override()
     chf_keys = set(["size"])
-    f = None
     try:
-        f = open(path, "r", 32384)
-        for line in f:
-            l = line.split()
-            if not l:
-                continue
-            if len(l) != 4:
-                if throw_errors:
-                    raise errors.ParseChksumError(
-                        path, "line count was not 4, was %i: '%s'" % (
-                            len(l), line))
-                continue
-            chf = l[0].lower()
-            #MD5 c08f3a71a51fff523d2cfa00f14fa939 diffball-0.6.2.tar.bz2 305567
-            d2 = d.get(l[2])
-            if d2 is None:
-                d[l[2]] = {chf:l[1], "size":long(l[3])}
-            else:
-                d2[chf] = l[1]
-            chf_keys.add(chf)
-    except (OSError, IOError, TypeError), e:
-        raise errors.ParseChksumError("failed parsing %r" % path, e)
+        f = None
+        try:
+            f = open(path, "r", 32384)
+            for line in f:
+                l = line.split()
+                if not l:
+                    continue
+                if len(l) != 4:
+                    if throw_errors:
+                        raise errors.ParseChksumError(
+                            path, "line count was not 4, was %i: '%s'" % (
+                                len(l), line))
+                    continue
+                chf = l[0].lower()
+                #MD5 c08f3a71a51fff523d2cfa00f14fa939 diffball-0.6.2.tar.bz2 305567
+                d2 = d.get(l[2])
+                if d2 is None:
+                    d[l[2]] = {chf:l[1], "size":long(l[3])}
+                else:
+                    d2[chf] = l[1]
+                chf_keys.add(chf)
+        except (OSError, IOError, TypeError), e:
+            raise errors.ParseChksumError("failed parsing %r" % path, e)
     finally:
         if f is not None or not f.close:
             f.close()

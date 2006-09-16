bd709cabfa6fc85041aac64847e2b059f14ce0af
Brian Harring <ferringb@gmail.com>
2006-09-16 00:47:04 -0700

    break out SimpleTree from prototype tests for reuse

--- pkgcore/test/repository/test_prototype.py
+++ pkgcore/test/repository/test_prototype.py
@@ -8,25 +8,9 @@ from pkgcore.ebuild.atom import atom
 from pkgcore.ebuild.cpv import CPV
 from pkgcore.util.currying import pre_curry
 from pkgcore.util.mappings import OrderedDict
+from pkgcore.test.repository.util import SimpleTree
 
 rev_sorted = pre_curry(sorted, reverse=True)
-
-class SimpleTree(tree):
-    package_class = staticmethod(CPV)
-    def __init__(self, cpv_dict):
-        self.cpv_dict = cpv_dict
-        tree.__init__(self)
-
-    def _get_categories(self, *arg):
-        if arg:
-            return ()
-        return tuple(self.cpv_dict.iterkeys())
-
-    def _get_packages(self, category):
-        return tuple(self.cpv_dict[category].iterkeys())
-
-    def _get_versions(self, cp_key):
-        return tuple(self.cpv_dict[cp_key[0]][cp_key[1]])
 
 
 class TestPrototype(unittest.TestCase):

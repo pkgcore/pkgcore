f83051b629eb85580f788dc0e60bdbb13849efc1
Brian Harring <ferringb@gmail.com>
2006-09-16 00:10:06 -0700

    Manifest2/Manifest1 parsing.

--- pkgcore/ebuild/digest.py
+++ pkgcore/ebuild/digest.py
@@ -4,7 +4,7 @@
 """
 ebuild tree manifest/digest support
 """
-
+from itertools import izip
 from pkgcore.chksum import errors, gpg
 from pkgcore.util.obj import make_SlottedDict_kls
 
@@ -47,3 +47,96 @@ def parse_digest(path, throw_errors=True):
         d[k] = kls(v.iteritems())
     return d
 
+
+    
+def parse_manifest(path, throw_errors=True):
+    d = {}
+    dist, aux, ebuild, misc = {}, {}, {}, {}
+    types = (("DIST", dist), ("AUX", aux), ("EBUILD", ebuild), ("MISC", misc))
+    files = {}
+    # type format (see glep 44 for exact rules)
+    # TYPE filename size (CHF sum)+
+    # example 'type' entry, all one line
+    #MISC metadata.xml 219 RMD160 613195ece366b33606e71ff1753be048f2507841 SHA1 d162fb909241ef50b95a3539bdfcde95429bdf81 SHA256 cbd3a20e5c89a48a842f7132fe705bf39959f02c1025052efce8aad8a8baa8dc
+    # old style manifest
+    # CHF sum filename size
+    chf_types = set(["size"])
+    try:
+        f = None
+        try:
+            f = open(path, "r", 32384)
+            for data in gpg.skip_signatures(f):
+                l = data.split()
+                for t, d in types:
+                    if l[0] != t:
+                        continue
+                    if len(l) % 2 != 1:
+                        if throw_errors:
+                            raise errors.ParseChksumError(path,
+                                "manifest 2 entry doesn't have right "
+                                "number of tokens, %i: %r" % 
+                                (len(line), line))
+                    else:
+                        chf_types.update(l[3::2])
+                        # this is a trick to do pairwise collapsing;
+                        # [size, 1] becomes [(size, 1)]
+                        i = iter(l[3:])
+                        d[l[1]] = [("size", long(l[2]))] + \
+                            [(chf.lower(), long(sum, 16))
+                                for chf, sum in izip(i, i)]
+                    break
+                else:
+                    if len(l) != 4:
+                        if throw_errors:
+                            raise errors.ParseChksumError(path,
+                                "line count was not 4, was %i: %r" %
+                                (len(l, line)))
+                        continue
+                    chf_types.add(l[0])
+                    files.setdefault(l[2], []).append(
+                        [long(l[3]), l[0].lower(), long(l[1], 16)])
+            
+        except (OSError, IOError, TypeError), e:
+            raise errors.ParseChksumError("failed parsing %r" % path, e)
+    finally:
+        if f is not None or not f.close:
+            f.close()
+
+    # collapse files into 4 types, convert to lower mem dicts
+    # doesn't handle files sublists correctly yet
+    for fname, data in files.iteritems():
+        for t, d in types:
+            existing = d.get(fname, None)
+            if existing is None:
+                continue
+            break
+        else:
+            # work around portage_manifest sucking and not
+            # specifying all files in the manifest.
+            if fname.endswith(".ebuild"):
+                existing = ebuild.setdefault(fname, [])
+            else:
+                existing = misc.setdefault(fname, [])
+
+        for chksum in data:
+            if existing:
+                if existing[0][1] != chksum[0]:
+                    if throw_errors:
+                        raise errors.ParseChksumError(path,
+                            "size collision for file %s" % fname)
+                else:
+                    existing.append(chksum[1:])
+            else:
+                existing.append(("size", chksum[0]))
+                existing.append(chksum[1:])
+
+    del files
+
+    # finally convert it to slotted dict for memory savings.
+    kls = make_SlottedDict_kls(x.lower() for x in chf_types)        
+    ret = []
+    for t, d in types:
+        for k, v in d.items():
+            d[k] = kls(v)
+        ret.append(d)
+    return ret
--- pkgcore/test/ebuild/test_digest.py
+++ pkgcore/test/ebuild/test_digest.py
@@ -24,6 +24,7 @@ digest_chksum = (
     ("sha256", long("e163b95ee56819c0f3c58ef9278c30b9e49302c2f1a1917680ca894d33929f7e", 16))
 )
 
+
 files = ["Python-2.4.2.tar.bz2", "python-2.4-patches-1.tar.bz2"]
 
 class TestDigest(unittest.TestCase):
@@ -51,7 +52,84 @@ class TestDigest(unittest.TestCase):
         self.assertRaises(
             ParseChksumError,
             self.gen_digest, digest_contents+"\nMD5 asdfasdfasdfasdf")
-        self.assertEqual(
-            2,
+        self.assertEqual(2,
             len(self.gen_digest(
                     digest_contents+"\nMD5 asdfasdf", throw_errors=False)))
+
+
+# ripped straight from the glep
+pure_manifest2 = \
+"""AUX ldif-buffer-overflow-fix.diff 5007 RMD160 1354a6bd2687430b628b78aaf43f5c793d2f0704 SHA1 424e1dfca06488f605b9611160020227ecdd03ac
+AUX procmime.patch 977 RMD160 39a51a4d654759b15d1644a79fb6e8921130df3c SHA1 d76929f6dfc2179281f7ccee5789aab4e970ba9e
+EBUILD sylpheed-claws-1.0.5-r1.ebuild 3906 RMD160 cdd546c128db2dea7044437de01ec96e12b4f5bf SHA1 a84b49e76961d7a9100852b64c2bfbf9b053d45e
+EBUILD sylpheed-claws-1.9.100.ebuild 4444 RMD160 89326038bfc694dafd22f10400a08d3f930fb2bd SHA1 8895342f3f0cc6fcbdd0fdada2ad8e23ce539d23
+EBUILD sylpheed-claws-1.9.15.ebuild 4821 RMD160 ec0ff811b893084459fe5b17b8ba8d6b35a55687 SHA1 358278a43da244e1f4803ec4b04d6fa45c41ab4d
+MISC ChangeLog 25770 RMD160 0e69dd7425add1560d630dd3367342418e9be776 SHA1 1210160f7baf0319de3b1b58dc80d7680d316d28
+MISC metadata.xml 269 RMD160 39d775de55f9963f8946feaf088aa0324770bacb SHA1 4fd7b285049d0e587f89e86becf06c0fd77bae6d
+DIST sylpheed-claws-1.0.5.tar.bz2 3268626 RMD160 f2708b5d69bc9a5025812511fde04eca7782e367 SHA1 d351d7043eef7a875df18a8c4b9464be49e2164b
+DIST sylpheed-claws-1.9.100.tar.bz2 3480063 RMD160 72fbcbcc05d966f34897efcc1c96377420dc5544 SHA1 47465662b5470af5711493ce4eaad764c5bf02ca
+DIST sylpheed-claws-1.9.15.tar.bz2 3481018 RMD160 b01d1af2df55806a8a8275102b10e389e0d98e94 SHA1 a17fc64b8dcc5b56432e5beb5c826913cb3ad79e
+"""
+pure_manifest2_chksums = {}
+for x in pure_manifest2.split("\n"):
+    l = x.split()
+    if not l:
+        continue
+    i = iter(l[3:])
+    chksum = [("size", long(l[2]))]
+    chksum += [(k.lower(), long(v, 16)) for k,v in zip(i, i)]
+    chksum = tuple(chksum)
+    pure_manifest2_chksums.setdefault(l[0], {})[l[1]] = chksum
+    del chksum, l, i
+
+
+class TestManifest(unittest.TestCase):
+    
+    @staticmethod
+    def get_manifest(data):
+        fn = tempfile.mktemp()
+        open(fn, "w").write(data)
+        try:
+            return digest.parse_manifest(fn)
+        finally:
+            os.unlink(fn)
+    
+    def test_gpg_filtering(self, gpg_insert=True):
+        if gpg_insert:
+            data = digest_contents.split("\n")
+            s = "\n".join(data[0:2])
+            s += "\n%s\nasdf\n%s\n" % (gpg.sig_header, gpg.sig_footer)
+            s += "\n".join(data[2:])
+        else:
+            s = digest_contents
+        dist, aux, ebuild, misc = \
+            self.get_manifest(s)
+        self.assertFalse(dist)
+        self.assertFalse(ebuild)
+        self.assertFalse(aux)
+        self.assertEqual(sorted(misc.keys()),
+            sorted(["Python-2.4.2.tar.bz2", "python-2.4-patches-1.tar.bz2"]))
+
+        d2 = misc["Python-2.4.2.tar.bz2"]
+
+        self.assertEqual(
+            sorted(d2.keys()), sorted(x[0] for x in digest_chksum))
+        for chf, expectedsum in digest_chksum:
+            self.assertEqual(d2[chf], expectedsum)
+        self.assertTrue(isinstance(d2["size"], long))
+    
+    def test_manifest1(self):
+        self.test_gpg_filtering(False)
+
+    def test_manifest2(self):
+        dist, aux, ebuild, misc = \
+            self.get_manifest(pure_manifest2)
+        for dtype, d in (("DIST", dist), ("AUX", aux),
+            ("EBUILD", ebuild), ("MISC", misc)):
+            req_d = pure_manifest2_chksums[dtype]
+            self.assertEqual(sorted(req_d), sorted(d))
+            for k, v in req_d.iteritems():
+                i1 = sorted(v)
+                i2 = sorted(d[k].iteritems())
+                self.assertEqual(i1, i2, msg="%r != %r\nfor %s %s" %
+                    (i1, i2, dtype, k))

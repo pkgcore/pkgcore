85500fbb49abd193903441c3a944a52e9d64e007
Brian Harring <ferringb@gmail.com>
2006-09-16 23:59:40 -0700

    saner gpg handling; strips the pgp msg header

--- pkgcore/chksum/gpg.py
+++ pkgcore/chksum/gpg.py
@@ -1,20 +1,45 @@
 # Copyright: 2006 Brian Harring <ferringb@gmail.com>
 # License: GPL2
 
-sig_header = '-----BEGIN PGP'
+msg_header = '-----BEGIN PGP SIGNED MESSAGE-----'
+msg_header_len = len(msg_header)
+msg_hash = 'Hash:'
+msg_hash_len = len(msg_hash)
+sig_header = '-----BEGIN PGP SIGNATURE-----'
 sig_header_len = len(sig_header)
-sig_footer = '-----END PGP'
+sig_footer = '-----END PGP SIGNATURE-----'
 sig_footer_len = len(sig_footer)
 
 def skip_signatures(iterable):
+    i = iter(iterable)
+
+# format is-
+#"""
+#-----BEGIN PGP SIGNED MESSAGE-----
+#Hash: SHA1
+#
+#"""
+    # so... prune msg first, then 
+    line = i.next()
+    if line[:msg_header_len] == msg_header:
+        line = i.next()
+        while line[:msg_hash_len] == msg_hash:
+            line = i.next()
+        # skip blank line after msg.
+        line = i.next()
+    yield line
+
     # localize this to this scope so it's faster.
     sh, shl = sig_header, sig_header_len
     sf, sfl = sig_footer, sig_footer_len
-    i = iter(iterable)
+
     for line in i:
-        if line[:shl] == sh:
+        while line[:shl] == sh:
             line = i.next()
+            # swallow the footer.
             while line[:sfl] != sf:
                 line = i.next()
-            continue
+            # leave the next line on the stack
+            line = i.next()
+
         yield line
--- pkgcore/test/chksum/test_gpg.py
+++ pkgcore/test/chksum/test_gpg.py
@@ -7,11 +7,16 @@ from pkgcore.chksum import gpg
 class TestSkipSignatures(unittest.TestCase):
     
     def test_skipping(self):
-        for marker in ("SIGNED MESSAGE", "SIGNATURE"):
+        for header in ([],
+            ["-----BEGIN PGP SIGNED MESSAGE-----\n", "Hash: Sha1\n", ""]):
             d = ["asdf",
                 "fdsa",
-                "-----BEGIN PGP %s-----" % marker,
+                "-----BEGIN PGP SIGNATURE-----",
                 "this isn't a valid sig...",
                 "-----END PGP SIGNATURE-----",
                 "foon"]
-            self.assertEqual(list(gpg.skip_signatures(d)), [d[0], d[1], d[-1]])
+            d2 = header + d
+            parsed = list(gpg.skip_signatures(d2))
+            required = [d[0], d[1], d[-1]]
+            self.assertEqual(parsed, required, msg="%r != %r for header %r" % 
+                (parsed, required, header))

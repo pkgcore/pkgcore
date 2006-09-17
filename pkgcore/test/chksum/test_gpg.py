6798e42435b564cb791dcd96f3cca58bc9b9bd49
Brian Harring <ferringb@gmail.com>
2006-09-17 00:38:12 -0700

    add a check for N depth signed msgs

--- pkgcore/test/chksum/test_gpg.py
+++ pkgcore/test/chksum/test_gpg.py
@@ -6,7 +6,7 @@ from pkgcore.chksum import gpg
 
 class TestSkipSignatures(unittest.TestCase):
     
-    def test_skipping(self):
+    def test_simple_skipping(self):
         for header in ([],
             ["-----BEGIN PGP SIGNED MESSAGE-----\n", "Hash: Sha1\n", ""]):
             d = ["asdf",
@@ -20,3 +20,26 @@ class TestSkipSignatures(unittest.TestCase):
             required = [d[0], d[1], d[-1]]
             self.assertEqual(parsed, required, msg="%r != %r for header %r" % 
                 (parsed, required, header))
+
+    def test_signed_signed(self):
+        d = ["-----BEGIN PGP SIGNED MESSAGE-----\n",
+            "Hash: SHA1\n",
+            "\n",
+            "- -----BEGIN PGP SIGNED MESSAGE-----\n",
+            "Hash: SHA1\n",
+            "\n",
+            "blah\n",
+            "- -----BEGIN PGP SIGNATURE-----\n",
+            "Version: GnuPG v1.4.3 (GNU/Linux)\n",
+            "\n",
+            "iD8DBQFEViv+aGfFFLhbXWkRAo+lAJ93s57QA2lW5BE1FdmEc3uzijpJrwCfcE6j",
+            "3Nzn/8wExwZ5eUacC/HoSo8=",
+            "=oBur",
+            "- -----END PGP SIGNATURE-----\n",
+            "foon\n",
+            "-----BEGIN PGP SIGNATURE-----\n",
+            " not valid...\n",
+            "-----END PGP SIGNATURE-----\n",
+            "asdf\n"]
+        self.assertEqual(list(gpg.skip_signatures(d)),
+            ["blah\n", "foon\n", "asdf\n"])

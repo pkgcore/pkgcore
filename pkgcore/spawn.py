252ba5cb55146fcebb699ad0a847d2bdc2785fc8
Brian Harring <ferringb@gmail.com>
2006-09-27 01:25:58 -0700

    same treatment for spawn_bash

--- pkgcore/spawn.py
+++ pkgcore/spawn.py
@@ -47,13 +47,15 @@ def spawn_bash(mycommand, debug=False, opt_name=None, **keywords):
     """spawn the command via bash -c"""
 
     args = [BASH_BINARY]
+    if isinstance(mycommand, basestring):
+        mycommand = mycommand.split()
     if not opt_name:
-        opt_name = os.path.basename(mycommand.split()[0])
+        opt_name = os.path.basename(mycommand[0])
     if debug:
         # Print commands and their arguments as they are executed.
         args.append("-x")
     args.append("-c")
-    args.append(mycommand)
+    args.extend(mycommand)
     return spawn(args, opt_name=opt_name, **keywords)
 
 def spawn_sandbox(mycommand, opt_name=None, **keywords):

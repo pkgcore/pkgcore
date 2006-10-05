cbebfbe4af2ccdf00053ba65d88b53b39990d01f
Brian Harring <ferringb@gmail.com>
2006-10-05 03:57:21 -0700

    initial parsing tests

---
+++ pkgcore/test/sync/__init__.py
@@ -0,0 +1,12 @@
+# Copyright: 2006 Brian Harring <ferringb@gmail.com>
+# License: GPL2
+
+def make_bogus_syncer(raw_kls):
+    class bogus_syncer(raw_kls):
+        binary = "/tmp/crack-monkeys-rule-if-you-have-this-binary-you-are-insane"
+    return bogus_syncer
+
+def make_valid_syncer(raw_kls):
+    class valid_syncer(raw_kls):
+        binary = "/bin/sh"
+    return valid_syncer
---
+++ pkgcore/test/sync/test_bzr.py
@@ -0,0 +1,21 @@
+# Copyright: 2006 Brian Harring <ferringb@gmail.com>
+# License: GPL2
+
+from pkgcore.test.sync import make_bogus_syncer, make_valid_syncer
+from pkgcore.test import TestCase
+from pkgcore.sync import base, bzr
+
+bogus = make_bogus_syncer(bzr.bzr_syncer)
+valid = make_valid_syncer(bzr.bzr_syncer)
+
+class TestBzrParsing(TestCase):
+
+    def test_parse(self):
+        self.assertEqual(bzr.bzr_syncer.parse_uri("bzr+http://dar"),
+            "http://dar")
+        self.assertRaises(base.uri_exception, bzr.bzr_syncer.parse_uri,
+            "bzr://dar")
+        self.assertRaises(base.syncer_exception, bogus,
+            "/tmp/foon", "bzr+http://foon.com/dar")
+        o = valid("/tmp/foon", "bzr+http://dar")    
+        self.assertEqual(o.uri, "http://dar")

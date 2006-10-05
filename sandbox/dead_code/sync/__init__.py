2805c1bf0b1ff0a1792325368708f86355fecd33
Brian Harring <ferringb@gmail.com>
2006-10-05 04:39:15 -0700

    check the old sync code; still don't have snapshot reimplemented, but that code isn't usable (plus I'll be using tarsync and friends)

--- sandbox/dead_code/sync/__init__.py
+++
@@ -1,6 +0,0 @@
-# sync/__init__.py; sync module namespace initialization
-# Copyright 2004 Brian Harring <ferringb@gmail.com>
-# Distributed under the terms of the GNU General Public License v2
-
-from parseuri import parseSyncUri
-

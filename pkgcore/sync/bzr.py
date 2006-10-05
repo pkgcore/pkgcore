b3a232dae0836149d7ea0b8a6b8529d4c8bf437d
Brian Harring <ferringb@gmail.com>
2006-10-05 00:33:12 -0700

    bzr sync'er, fix up hg...

--- pkgcore/sync/bzr.py
+++ pkgcore/sync/bzr.py
@@ -10,9 +10,13 @@ class hg_syncer(base.syncer):
 
     @staticmethod
     def parse_uri(raw_uri):
-        if not raw_uri.startswith("hg://"):
-            raise base.uri_exception(raw_uri, "doesn't start with hg://")
-        return True
+        if not raw_uri.startswith("bzr+"):
+            raise base.uri_exception(raw_uri, "doesn't start with bzr+")
+        return uri[4:]
+
+    def __init__(self, basedir, uri):
+        uri = self.parse_uri
+        base.syncer.__init__(self, basedir, uri)
         
     def _sync(self, verbosity, output_fd):
         chdir = None
@@ -21,11 +25,11 @@ class hg_syncer(base.syncer):
         except (IOError, OSError), ie:
             if errno.ENOENT != ie.errno:
                 raise base.generic_exception(self.basedir, ie)
-            arg = ["clone"]
+            arg = "get"
         else:
             if not stat.S_ISDIR(st.st_mode):
                 raise base.generic_exception(self.basedir, "isn't a directory")
-            arg = ["pull", "-u"]
-        return 0 == self._spawn(["hg"] + arg + [self.uri, self.basedir],
+            arg = "pull"
+        return 0 == self._spawn(["bzr", arg, self.uri, self.basedir],
             fd_pipes={1:output_fd, 2:output_fd, 0:0}, chdir=chdir)
     

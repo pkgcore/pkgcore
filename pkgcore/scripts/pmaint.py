6dd6dc6d734379930a32f54f7236a4ffdf1513d3
Brian Harring <ferringb@gmail.com>
2006-10-27 21:12:09 -0700

    convert --sync to sync subcommand

--- pkgcore/scripts/pmaint.py
+++ pkgcore/scripts/pmaint.py
@@ -7,12 +7,12 @@ repository maintainence
 
 from pkgcore.util import commandline
 
-class OptionParser(commandline.OptionParser):
+commandline_commands = {}
+
+class SyncOptionParser(commandline.OptionParser):
 
     def __init__(self, **kwargs):
         commandline.OptionParser.__init__(self, description=__doc__, **kwargs)
-        self.add_option("--sync", action='store_true', default=False,
-            help="sync repositories")
         self.add_option("-r", "--repo", action='append',
             help="specify a specific repo to work on; defaults to all "
                 "applicable otherwise")
@@ -22,19 +22,30 @@ class OptionParser(commandline.OptionParser):
     def check_values(self, values, args):
         values, args = commandline.OptionParser.check_values(
             self, values, args)
-        if not values.sync:
-            self.error(
-                "need at least one directive; "
-                "--sync is the only supported command currently (see --help)")
-        if not values.repo:
+
+        if not args:
             values.repo = values.config.repo.keys()
+        else:
+            for x in args:
+                if x not in values.config.repo:
+                    self.error("repo %r doesn't exist:\nvalid repos %r" % 
+                        (x, values.config.repo.keys()))
+            values.repo = args
         return values, args
 
+def format_seq(seq, formatter=repr):
+    if not seq:
+        seq = None
+    elif len(seq) == 1:
+        seq = seq[0]
+    return formatter(seq)
 
-def do_sync(config, repos, out, err, force):
-    failed = []
+def sync_main(options, out, err):
+    """update a local repositories to match their remote parent"""
+    config = options.config
+    succeeded, failed = [], []
     seen = set()
-    for x in repos:
+    for x in options.repo:
         r = config.repo[x]
         if r in seen:
             continue
@@ -43,18 +54,18 @@ def do_sync(config, repos, out, err, force):
             continue
         out.write("*** syncing %r..." % x)
         if not r.sync():
-            failed.append(r, force=force)
-            out.write("*** failed syncing %s" % x)
+            failed.append(r, force=options.force)
+            out.write("*** failed syncing %r" % x)
             failed.append(x)
         else:
-            out.write("*** synced %s" % x)
-    return failed
-
-def main(options, out, err):
-    failed = do_sync(options.config, options.repo, out, err, options.force)
+            succeeded.append(x)
+            out.write("*** synced %r" % x)
+    if len(succeeded) + len(failed) > 1:
+        out.write("*** synced %r\n" % format_seq(sorted(succeeded)))
+        if failed:
+            err.write("!!! failed sync'ing %r\n" % format_seq(sorted(failed)))
     if failed:
-        err.write("!!! synced: %r\n!!! failed syncing: %r\n" % 
-            (sorted(set(repos).differenced(failed)),
-            sorted(failed)))
         return 1
     return 0
+
+commandline_commands['sync'] = (SyncOptionParser, sync_main)

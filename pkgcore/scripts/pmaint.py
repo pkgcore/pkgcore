66f395804612f5fd1030c21e2c0ed68ddea01374
Brian Harring <ferringb@gmail.com>
2006-10-29 00:12:20 -0700

    copy subcommand, quickpkg equiv functionality.

--- pkgcore/scripts/pmaint.py
+++ pkgcore/scripts/pmaint.py
@@ -5,7 +5,17 @@
 repository maintainence
 """
 
+import os
 from pkgcore.util import commandline
+from pkgcore.util.demandload import demandload
+
+demandload(globals(), "pkgcore.repository:multiplex "
+    "pkgcore.ebuild.atom:atom "
+    "pkgcore.package:errors "
+    "pkgcore.package:mutated "
+    "pkgcore.fs:contents,livefs "
+    "errno "
+    )
 
 commandline_commands = {}
 
@@ -14,6 +24,8 @@ def format_seq(seq, formatter=repr):
         seq = None
     elif len(seq) == 1:
         seq = seq[0]
+    else:
+        seq = tuple(seq)
     return formatter(seq)
 
 class SyncOptionParser(commandline.OptionParser):
@@ -62,9 +74,9 @@ def sync_main(options, out, err):
             succeeded.append(x)
             out.write("*** synced %r" % x)
     if len(succeeded) + len(failed) > 1:
-        out.write("*** synced %r\n" % format_seq(sorted(succeeded)))
+        out.write("*** synced %s\n" % format_seq(sorted(succeeded)))
         if failed:
-            err.write("!!! failed sync'ing %r\n" % format_seq(sorted(failed)))
+            err.write("!!! failed sync'ing %s\n" % format_seq(sorted(failed)))
     if failed:
         return 1
     return 0
@@ -72,17 +84,116 @@ def sync_main(options, out, err):
 commandline_commands['sync'] = (SyncOptionParser, sync_main)
 
 
-# end sync bits.
-
-class CopyPkgParser(commandline.OptionParser):
+class CopyParser(commandline.OptionParser):
     
     def __init__(self, **kwargs):
-        commandline.OptionParser.__init__(self, doc=
+        commandline.OptionParser.__init__(self, description=
             "copy built pkg(s) into a repository", **kwargs)
         self.add_option("-s", "--source-repo",
             help="copy from just the specified repository; else defaults "
                 "to finding any match")
+        self.add_option("-t", "--target-repo", default=None,
+            help="repository to copy packages into; if specified, "
+                "you don't need to specify the target repo as the last arg.  "
+                "Mainly useful for xargs invocations")
         self.add_option("--force", action='store_true', default=False,
             help="try and force the copy if the target repository is marked as "
                 "immutable")
+    
+    def check_values(self, values, args):
+        l = len(args)
+        if not values.target_repo and l < 2:
+            self.error("target_report wasn't specified- specify it either as "
+                "the last arguement, or via --target-repo")
+
+        if values.target_repo is not None:
+            target_repo = values.target_repo
+        else:
+            target_repo = args.pop(-1)
         
+        try:
+            values.target_repo = values.config.repo[target_repo]
+        except KeyError:
+            self.error("target repo %r was not found, known repos-\n%s" %
+                (target_repo, format_seq(values.config.repo.keys())))
+        
+        if values.source_repo:
+            try:
+                values.source_repo = values.config.repo[values.source_repo]
+            except KeyError:
+                self.error("source repo %r was not found, known repos-\n%s" %
+                    (values.source_repo, format_seq(values.config.repo.keys())))
+        else:
+            values.source_repo = multiplex.tree(*values.config.repos.values())
+        
+        values.candidates = []
+        for x in args:
+            try:
+                values.candidates.append(atom(x))
+            except errors.InvalidDependency, e:
+                self.error("arg %r isn't a valid atom: %s" %
+                    (x, e))
+        return values, []
+
+def copy_main(options, out, err):
+    trg_repo = options.target_repo
+    src_repo = options.source_repo
+
+    transfers = []
+    failures = False
+    kwds = {'force': options.force}
+    
+    for candidate in options.candidates:
+        matches = src_repo.match(candidate)
+        if not matches:
+            err.write("didn't find any matching pkgs for %r\n" % candidate)
+            failures = True
+            continue
+
+        for src in matches:
+            existing = trg_repo.match(src.versioned_atom)
+            args = []
+            pkg = src
+            if len(existing) > 1:
+                err.write(
+                    "skipping %r; tried to replace more then one pkg %r...\n" %
+                    (src, format_seq(existing)))
+                failures = True
+                continue
+            elif len(existing) == 1:
+                out.write("replacing %s with %s... " % (src, existing[0]))
+                op = trg_repo.replace
+                args = existing
+            else:
+                out.write("copying %s... " % src)
+                op = trg_repo.install
+
+            if src.repo.livefs:
+                out.write("forcing regen of contents due to src being livefs..")
+                new_contents = contents.contentsSet(mutable=True)
+                for fsobj in src.contents:
+                    try:
+                        new_contents.add(livefs.gen_obj(fsobj.location))
+                    except OSError, oe:
+                        if oe.errno != errno.ENOENT:
+                            err.write("failed accessing fs obj %r; %r\n"
+                                "aborting this copy\n" %
+                                (fsobj, oe))
+                            failure = True
+                            new_contents = None
+                            break
+                        err.write("warning: dropping fs obj %r since it "
+                            "doesn't exist\n" % fsobj)
+                if new_contents is None:
+                    continue
+                pkg = mutated.MutatedPkg(src, {'contents':new_contents})   
+
+            op = op(*(args + [pkg]), **kwds)
+            op.finish()
+                
+            out.write("completed\n")
+    if failures:
+        return 1
+    return 0
+
+commandline_commands['copy'] = (CopyParser, copy_main)

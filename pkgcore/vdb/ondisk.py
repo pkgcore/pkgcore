3ff8e61613571e70e7d2f4a29fe429cacd1f354b
Brian Harring <ferringb@gmail.com>
2006-10-29 00:09:01 -0700

    fix ebuild path

--- pkgcore/vdb/ondisk.py
+++ pkgcore/vdb/ondisk.py
@@ -135,7 +135,8 @@ class tree(prototype.tree):
             else:
                 data = bz2_data_source(fp+".bz2")
         elif key == "ebuild":
-            fp = os.path.join(os.path.basename(path.rstrip(os.path.sep)))
+            fp = os.path.join(os.path.dirname(path), 
+                os.path.basename(path.rstrip(os.path.sep))+".ebuild")
             data = data_source.local_source(fp)
         else:
             try:

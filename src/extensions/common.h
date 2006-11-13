77fb54e28bc4f81b2b0bc173c3d5197eac152629
Brian Harring <ferringb@gmail.com>
2006-11-12 22:25:17 -0800

    PKGCORE_FUNC_DESCR; used to generated binding crap for cfuncs.

--- src/extensions/common.h
+++ src/extensions/common.h
@@ -54,4 +54,53 @@ type##_get_##attr (type *self, void *closure)                           \
     {doc, (getter)type##_get_##attr ,           \
         (setter)type##_set_##attr , NULL}
 
+
+#define PKGCORE_FUNC_DESC(meth_name, class_name, func, methargs)        \
+                                                                        \
+static PyObject *                                                       \
+func##_get_descr(PyObject *self, PyObject *obj, PyObject *type)         \
+{                                                                       \
+    static PyMethodDef mdef = {meth_name, (PyCFunction)func, methargs,  \
+        NULL};                                                          \
+    return PyCFunction_New(&mdef, obj);                                 \
+}                                                                       \
+                                                                        \
+static PyTypeObject func##_type = {                                     \
+    PyObject_HEAD_INIT(NULL)                                            \
+    0,                                  /* ob_size        */            \
+    class_name,                         /* tp_name        */            \
+    sizeof(PyObject),                   /* tp_basicsize   */            \
+    0,                                  /* tp_itemsize    */            \
+    0,                                  /* tp_dealloc     */            \
+    0,                                  /* tp_print       */            \
+    0,                                  /* tp_getattr     */            \
+    0,                                  /* tp_setattr     */            \
+    0,                                  /* tp_compare     */            \
+    0,                                  /* tp_repr        */            \
+    0,                                  /* tp_as_number   */            \
+    0,                                  /* tp_as_sequence */            \
+    0,                                  /* tp_as_mapping  */            \
+    0,                                  /* tp_hash      */              \
+    0,                                  /* tp_call      */              \
+    0,                                  /* tp_str       */              \
+    0,                                  /* tp_getattro  */              \
+    0,                                  /* tp_setattro  */              \
+    0,                                  /* tp_as_buffer */              \
+    Py_TPFLAGS_DEFAULT,                 /* tp_flags     */              \
+    "cpython version of ##name##",      /* tp_doc       */              \
+    0,                                  /* tp_traverse  */              \
+    0,                                  /* tp_clear     */              \
+    0,                                  /* tp_richcompare    */         \
+    0,                                  /* tp_weaklistoffset */         \
+    0,                                  /* tp_iter      */              \
+    0,                                  /* tp_iternext  */              \
+    0,                                  /* tp_methods   */              \
+    0,                                  /* tp_members   */              \
+    0,                                  /* tp_getset    */              \
+    0,                                  /* tp_base      */              \
+    0,                                  /* tp_dict      */              \
+    &func##_get_descr,                   /* tp_descr_get */              \
+    0,                                  /* tp_descr_set */              \
+}; 
+
 #endif

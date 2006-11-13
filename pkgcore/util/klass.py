3654709e040a5545d265fcdb611e68118ba7d2a4
Brian Harring <ferringb@gmail.com>
2006-11-12 23:39:34 -0800

    generic_equality methods.

--- pkgcore/util/klass.py
+++ pkgcore/util/klass.py
@@ -1,6 +1,8 @@
 # Copyright: 2006 Brian Harring <ferringb@gmail.com>
 # License: GPL2
 
+from operator import attrgetter
+
 def native_GetAttrProxy(target):
     def reflected_getattr(self, attr):
         return getattr(getattr(self, target), attr)
@@ -25,3 +27,36 @@ except ImportError:
     GetAttrProxy = native_GetAttrProxy
     contains = native_contains
     get = native_get
+
+
+def generic_equality(*attrlist):
+
+    class generic__eq__(object):
+        __slots__ = ("attrlist", "__weakref__")
+    
+        def __init__(self, *attrlist):
+            self.attrlist = tuple(attrgetter(x) for x in attrlist)
+    
+        def __call__(self, inst1, inst2):
+            if inst1 is inst2:
+                return True
+            for f in self.attrlist:
+                if f(inst1) != f(inst2):
+                    return False
+            return True
+
+    class generic__ne__(object):
+        __slots__ = ("attrlist", "__weakref__")
+    
+        def __init__(self, *attrlist):
+            self.attrlist = tuple(attrgetter(x) for x in attrlist)
+    
+        def __call__(self, inst1, inst2):
+            if inst1 is inst2:
+                return False
+            for f in self.attrlist:
+                if f(inst1) == f(inst2):
+                    return False
+            return True
+
+    return generic__eq__(*attrlist), generic__ne__(attrlist)

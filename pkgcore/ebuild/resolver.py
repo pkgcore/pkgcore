6b354e394a64ff0c55f9317a76744e5d5b975561
Brian Harring <ferringb@gmail.com>
2007-01-15 07:15:16 -0800

    slight change to the rules for virtuals; not optimized, but works for the moment basically, grab livefs repos, pull the pkglist up front (yuck), push non-real packages (old style virtuals) to the front of the list, with everything else behind it.
    Thus, if old style virtuals are active in the vdb, they're honored- else it falls back to the available virtuals from non-livefs repos

--- pkgcore/ebuild/resolver.py
+++ pkgcore/ebuild/resolver.py
@@ -11,17 +11,31 @@ from pkgcore.repository import virtual
 from pkgcore.repository.misc import nodeps_repo
 from pkgcore.resolver import plan
 from pkgcore.util.demandload import demandload
+from itertools import chain
 
 demandload(globals(),
            "pkgcore.restrictions:packages,values "
            "pkgcore.pkgsets.glsa:KeyedAndRestriction ")
 
+def prefer_old_style_virtuals(pkg1, pkg2):
+    if pkg1.package_is_real:
+        if pkg2.package_is_real:
+            return cmp(pkg1, pkg2)
+        return -1
+    elif pkg2.package_is_real:
+        return 1
+    return cmp(pkg1, pkg2)
 
 def prefer_highest_ver(resolver, dbs, atom):
     try:
         if atom.category == "virtual":
             # force vdb inspection first.
-            return resolver.prefer_reuse_strategy(resolver, dbs, atom)
+            livefs = [x for x in dbs if x.livefs]
+            nonlivefs = [x for x in dbs if not x.livefs]
+            l = list(resolver.prefer_reuse_strategy(resolver, livefs, atom))
+            l.sort(prefer_old_style_virtuals)
+            return chain(l, resolver.prefer_highest_version_strategy(resolver, 
+                nonlivefs, atom))
     except AttributeError:
         # should do inspection instead...
         pass

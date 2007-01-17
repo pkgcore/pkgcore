12244f524625e2ef8bab219f166b83fe7f92641d
Brian Harring <ferringb@gmail.com>
2007-01-17 15:39:21 -0800

    shift around the output a bit so that it's easier readable for xterm

--- pkgcore/scripts/pmerge.py
+++ pkgcore/scripts/pmerge.py
@@ -472,8 +472,8 @@ def main(options, out, err):
 
     change_count = len(changes)
     for count, op in enumerate(changes):
-        status_str = "processing %s, %i/%i" % (get_raw_pkg(op.pkg), count + 1,
-            change_count)
+        status_str = "%i/%i, %s: processing" % (count + 1, change_count,
+            get_raw_pkg(op.pkg))
         out.write(status_str)
         out.title(status_str)
         if op.desc != "remove":

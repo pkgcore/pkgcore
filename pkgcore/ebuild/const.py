6e0124d34232df6eab0a184051f59d6ff8d12e38
Brian Harring <ferringb@gmail.com>
2007-01-21 16:41:54 -0800

    explicit force EAPI into all metadata keys; already handled elsewhere, but better safe then sorry.

--- pkgcore/ebuild/const.py
+++ pkgcore/ebuild/const.py
@@ -16,7 +16,7 @@ incrementals = (
 
 metadata_keys = (
     'DEPEND', 'RDEPEND', 'SLOT', 'SRC_URI', 'RESTRICT', 'HOMEPAGE', 'LICENSE',
-    'DESCRIPTION', 'KEYWORDS', 'INHERITED', 'IUSE', 'PDEPEND', 'PROVIDE',
+    'DESCRIPTION', 'KEYWORDS', 'INHERITED', 'IUSE', 'PDEPEND', 'PROVIDE', 'EAPI',
     '_mtime_', '_eclasses_')
 
 ACCEPT_LICENSE = ()

4708f417bfc39a8e8aadbe845c56c654dd3226f5
Brian Harring <ferringb@gmail.com>
2007-02-17 00:47:26 -0800

    docstrings for pkgcore.repository.misc.*

--- pkgcore/repository/misc.py
+++ pkgcore/repository/misc.py
@@ -6,15 +6,30 @@ from pkgcore.package.mutated import MutatedPkg
 from pkgcore.util.iterables import caching_iter
 from pkgcore.util.klass import GetAttrProxy
 
+__all__ = ("nodeps_repo", "caching_repo")
+
 class nodeps_repo(object):
+
+    """
+    repository wrapper that returns wrapped pkgs via
+    L{MutatedPkg} that have their depends/rdepends/post_rdepends wiped
+    """
+
     default_depends = packages.AndRestriction(finalize=True)
     default_rdepends = packages.AndRestriction(finalize=True)
+    default_post_rdepends = packages.AndRestriction(finalize=True)
+
     def __init__(self, repo):
+        """
+        @param repo: repository to wrap
+        """
         self.raw_repo = repo
 
     def itermatch(self, *a, **kwds):
-        return (MutatedPkg(x, overrides={"depends":self.default_depends,
-                                         "rdepends":self.default_rdepends})
+        return (MutatedPkg(x, 
+            overrides={"depends":self.default_depends,
+                "rdepends":self.default_rdepends,
+                "post_rdepends":self.default_post_rdepends})
                 for x in self.raw_repo.itermatch(*a, **kwds))
 
     def match(self, *a, **kwds):
@@ -28,7 +43,32 @@ class nodeps_repo(object):
 
 class caching_repo(object):
 
+    """
+    repository wrapper that overrides match, returning
+    L{caching_iter} instances; itermatch is slaved to match,
+    in other words iterating over the caching_iter.
+    
+    Main use for this is to cache results from query lookups;
+    if matches restrict arg is in the cache, the caller gets a shared
+    caching_iter sequence, which may already be fully loaded with pkg
+    instances.
+    
+    This can boost random lookup time pretty nicely, while helping to
+    hold instance in memory to avoid redoing work.
+    
+    Cost of this of course is that involved objects are forced to stay 
+    in memory till the cache is cleared.  General use, not usually what
+    you want- if you're making a lot of random queries that are duplicates
+    (resolver does this for example), caching helps.
+    """
+
     def __init__(self, db, strategy):
+        """
+        @param: db, an instance supporting the repository protocol to cache
+          queries from.
+        @param strategy: forced sorting strategy for results.  If you don't
+          need sorting, pass in iter.
+        """
         self.__db__ = db
         self.__strategy__ = strategy
         self.__cache__ = {}

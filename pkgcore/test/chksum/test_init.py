5e95993d27ea2decb5abb00d858ce7fcdfdbb9ad
Brian Harring <ferringb@gmail.com>
2007-02-19 08:11:30 -0800

    get_handlers? tests. init needs to be converted over to chksumming, but waiting on marienzs' comments prior.

---
+++ pkgcore/test/chksum/test_init.py
@@ -0,0 +1,45 @@
+# Copyright: 2007 Brian Harring <ferringb@gmail.com>
+# License: GPL2
+
+from pkgcore import chksum
+from pkgcore.test import TestCase
+
+
+class Test_funcs(TestCase):
+
+    # ensure we aren't mangling chksum state for other tests.
+    def tearDown(self):
+        chksum.__inited__ = False
+        chksum.chksum_types.clear()
+        chksum.init = self._saved_init
+    
+    def setUp(self):
+        chksum.__inited__ = False
+        chksum.chksum_types.clear()
+        self._saved_init = chksum.init
+        self._inited_count = 0
+        def f():
+            self._inited_count += 1
+            chksum.__inited__ = True
+        chksum.init = f
+
+    def test_get_handlers(self):
+        expected = {"x":1, "y":2}
+        chksum.chksum_types.update(expected)
+        self.assertEqual(expected, chksum.get_handlers())
+        self.assertEqual(self._inited_count, 1)
+        self.assertEqual(expected, chksum.get_handlers(None))
+        self.assertEqual({"x":1}, chksum.get_handlers(["x"]))
+        self.assertEqual(expected, chksum.get_handlers(["x", "y"]))
+        self.assertEqual(self._inited_count, 1)
+
+    def test_get_handler(self):
+        self.assertRaises(KeyError, chksum.get_handler, "x")
+        self.assertEqual(self._inited_count, 1)
+        chksum.chksum_types["x"] = 1
+        self.assertRaises(KeyError, chksum.get_handler, "y")
+        chksum.chksum_types["y"] = 2
+        self.assertEqual(1, chksum.get_handler("x"))
+        self.assertEqual(2, chksum.get_handler("y"))
+        self.assertEqual(self._inited_count, 1)
+        

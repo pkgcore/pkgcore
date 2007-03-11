c1a0bad929751ce8669ffa1037bf0de0060309c6
Brian Harring <ferringb@gmail.com>
2007-03-11 06:56:39 -0700

    fix fallback import of ElementTree

--- pkgcore/util/xml/__init__.py
+++ pkgcore/util/xml/__init__.py
@@ -35,7 +35,7 @@ if not gotit:
         pass
 
 if not gotit:
-    from pkgcore.util.xml import bundled_elementtree as etree
+    from pkgcore.util.xml.bundled_elementtree import ElementTree as etree
 del gotit
 
 def escape(string):

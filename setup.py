a3c0d695ce4f71199d2c5c2595a20c8b7370ceeb
Brian Harring <ferringb@gmail.com>
2008-10-07 16:06:53 -0700

    more distutils nonsense

--- setup.py
+++ setup.py
@@ -383,9 +383,11 @@ core.setup(
     license='GPL-2',
     packages=packages,
     package_data={
-        'pkgcore': [
-            'bin/ebuild-env/*',
-            'bin/ebuild-env/eapi/*',
+        'pkgcore': 
+            ['bin/ebuild-env/%s' % x for x in 
+                ['ebuild-daemon.lib', 'ebuild-daemon.sh', 'ebuild-default-functions.sh', 'ebuild.sh',
+                'filter-env', 'isolated-functions.sh', 'portageq_emulation']] +
+            ['bin/ebuild-env/eapi/*',
             'bin/ebuild-helpers/*',
             ],
         },

706d04640007a66e2b74358ecf87263825bed41e
Brian Harring <ferringb@gmail.com>
2010-04-21 20:11:48 -0700

    pull in a limiter on the test_py3k bits to compensate for <snakeoil-0.3.6.3

--- pkgcore/test/test_py3k_eq_hash_inheritance.py
+++ pkgcore/test/test_py3k_eq_hash_inheritance.py
@@ -1,8 +1,13 @@
 # Copyright: 2010 Brian Harring <ferringb@gmail.com>
 # License: GPL2/BSD
 
-
-from snakeoil.test import test_py3k_eq_hash_inheritance as module
+try:
+    from snakeoil.test import test_py3k_eq_hash_inheritance as module
+except ImportError:
+    # might seem ugly, but basically if we're running <snakeoil-0.3.6.4
+    # which lacks this test, silence it
+    class module(object):
+        Test = object
 
 class Test(module.Test):
 

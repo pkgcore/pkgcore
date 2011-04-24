4bd49b6d05cb98922c893d7d6a9c6ea1e1374a47
Brian Harring <ferringb@gmail.com>
2011-04-24 14:48:59 -0700

    add 0.6 bits

--- pkgcore/const.py
+++ pkgcore/const.py
@@ -1,4 +1,4 @@
-# Copyright: 2005-2009 Brian Harring <ferringb@gmail.com>
+# Copyright: 2005-2011 Brian Harring <ferringb@gmail.com>
 # License: GPL2/BSD
 
 
@@ -31,4 +31,4 @@ WORLD_FILE           = '/var/lib/portage/world'
 LIBFAKEROOT_PATH     = "/usr/lib/libfakeroot.so"
 FAKED_PATH           = "/usr/bin/faked"
 
-VERSION              = '0.5.11.8'
+VERSION              = '0.6'

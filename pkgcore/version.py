e356b0d5b9054f521f13d6aec905d2ac34999bea
Brian Harring <ferringb@gmail.com>
2011-08-07 23:29:43 -0700

    delay the import, and fix snakeoil/pkgcore typo

--- pkgcore/version.py
+++ pkgcore/version.py
@@ -1,4 +1,4 @@
-# Copyright: 2010 Brian Harring <ferringb@gmail.com>
+# Copyright: 2010-2011 Brian Harring <ferringb@gmail.com>
 # License: BSD/GPL2
 
 
@@ -7,12 +7,11 @@
 __all__ = ("get_version",)
 
 from pkgcore import const
-from snakeoil.version import get_git_version
 
 _ver = None
 
 def get_version():
-    """:return: a string describing the snakeoil version."""
+    """:return: a string describing the pkgcore version."""
     global _ver
     if _ver is not None:
         return _ver
@@ -20,6 +19,8 @@ def get_version():
     try:
         from pkgcore._verinfo import version_info
     except ImportError:
+        # intentionally lazy imported.
+        from snakeoil.version import get_git_version
         version_info = get_git_version(__file__)
 
     _ver = 'pkgcore %s\n(%s)' % (const.VERSION, version_info)

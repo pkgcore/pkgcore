85feff61e5985f747e87a58ee72d0d31ae54df0d
Brian Harring <ferringb@gmail.com>
2011-12-15 04:11:38 -0800

    Fix hang from ctrl-c when running in parallel

--- pkgcore/operations/regen.py
+++ pkgcore/operations/regen.py
@@ -4,14 +4,17 @@
 import time
 import Queue
 import threading
+import thread
 
 from snakeoil import compatibility
 
-def regen_iter(iterable, observer, regen_func):
+def regen_iter(iterable, observer, regen_func, is_thread=False):
     for x in iterable:
         try:
             regen_func(x)
-        except compatibility.IGNORED_EXCEPTIONS:
+        except compatibility.IGNORED_EXCEPTIONS, e:
+            if isinstance(e, KeyboardInterrupt):
+                return
             raise
         except Exception, e:
             observer.error("caught exception %s while processing %s" % (e, x))
@@ -44,10 +47,14 @@ def regen_repository(repo, observer, threads=1, pkg_attr='keywords', **options):
                 yield x
         regen_iter(passthru(repo), observer, _get_repo_helper())
     else:
-        queue = Queue.Queue(threads * 2)
+        # note we allow an infinite queue since .put below
+        # is blocking, and won't return till it succeeds (regardless of signal)
+        # as such, we do it this way to ensure the put succeeds, then the keyboardinterrupt
+        # can be seen.
+        queue = Queue.Queue()
         kill = threading.Event()
         kill.clear()
-        def iter_queue(kill, qlist, timeout=0.25):
+        def iter_queue(kill, qlist, timeout=0.5):
             while not kill.isSet():
                 try:
                     yield qlist.get(timeout=timeout)
@@ -56,25 +63,24 @@ def regen_repository(repo, observer, threads=1, pkg_attr='keywords', **options):
         regen_threads = [
             threading.Thread(
                 target=regen_iter, args=(iter_queue(kill, queue), observer,
-                    _get_repo_helper()))
+                    _get_repo_helper(), True))
             for x in xrange(threads)]
         try:
+            failed = True
             for x in regen_threads:
                 x.start()
             # now we feed the queue.
             for pkg in repo:
                 queue.put(pkg)
-        except Exception:
+            failed = False
+        finally:
+            if not failed:
+                while not queue.empty():
+                    time.sleep(0.5)
+
             kill.set()
             reclaim_threads(regen_threads, observer)
-            raise
 
-        # by now, queue is fed. reliable for our uses since the queue
-        # is only subtracted from.
-        while not queue.empty():
-            time.sleep(.5)
-        kill.set()
-        reclaim_threads(regen_threads, observer)
         assert queue.empty()
 
     for helper in helpers:

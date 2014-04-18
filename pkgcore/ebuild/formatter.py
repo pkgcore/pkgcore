a9e8bdc3a5171801d5b695c2a6df58397ad6c165
Tim Harder <radhermit@gmail.com>
2014-04-18 00:56:10 -0700

    put most common case (unstable keyword) first for pkg symbol output

--- pkgcore/ebuild/formatter.py
+++ pkgcore/ebuild/formatter.py
@@ -379,11 +379,11 @@ class PortageFormatter(CountingFormatter):
             logger.warning("unformattable op type: desc(%r), %r", (op.desc, op))
 
         if self.verbose:
-            if not op.pkg.keywords:
-                op_chars[6] = [out.fg('red'), out.bold, '*', out.reset]
-            elif self.unstable_arch in op.pkg.keywords and \
+            if self.unstable_arch in op.pkg.keywords and \
                     self.unstable_arch not in self.domain_settings['ACCEPT_KEYWORDS']:
                 op_chars[6] = [out.fg('yellow'), out.bold, '~', out.reset]
+            elif not op.pkg.keywords:
+                op_chars[6] = [out.fg('red'), out.bold, '*', out.reset]
             else:
                 for masked_atom in op.pkg.repo.default_visibility_limiters:
                     if masked_atom.match(op.pkg.versioned_atom):

023707813d99d3d473c5b25771bd0c08a038ffce
Tim Harder <radhermit@gmail.com>
2014-12-02 10:12:09 -0800

    properly filter injected phase functions from env
    Previously they weren't getting filtered with the new dynamic EAPI
    function filter method since the required functions are in common.lib
    which wasn't sourced to avoid pulling all its other functions into the
    env dump.
    
    Now we source common.lib and then unset all its functions before
    requesting the list of EAPI specific functions.

--- pkgcore/ebuild/eapi-bash/generate_eapi_func_list.bash
+++ pkgcore/ebuild/eapi-bash/generate_eapi_func_list.bash
@@ -7,14 +7,33 @@ export PKGCORE_BIN_PATH=$(dirname "$0")
 # so any code that tried accessing it thinks it succeeded
 export PKGCORE_PYTHON_BINARY=/bin/true
 
+# workaround to add required function from isolated-functions.lib without
+# having to source it
+__is_function() {
+	declare -F "$1" &> /dev/null
+} &> /dev/null
+
+# pull in common.lib for various functions used in EAPI libs, currently
+# __inject_common_phase_funcs and __inject_phase_funcs in particular
+source "${PKGCORE_BIN_PATH}/eapi/common.lib" \
+	|| { echo "failed loading eapi/common.lib" >&2; exit 1; }
+
+# grab function list from common.lib, we'll need to unset them further on
+__content=$(builtin declare -F)
+declare() { echo "$2"; }
+__common_funcs=$(eval "${__content}")
+unset -f declare
+
 source "${PKGCORE_BIN_PATH}/eapi/${EAPI}.lib" \
 	|| { echo "failed loading eapi/${EAPI}.lib" >&2; exit 1; }
 
-# Grab the vars /before/ adding our custom declare function, else it'll
-# show up in the list of functions.
+# remove functions pulled in by common.lib, we're only interested in the ones
+# set specifically in the EAPI libs
+unset -f __is_function ${__common_funcs}
+
+# grab function list *before* adding our custom declare function, otherwise
+# it'll show up in the list of functions
 __content=$(builtin declare -F)
-declare() {
-	echo "$2"
-}
+declare() { echo "$2"; }
 eval "${__content}" || { echo "generate EAPI func list eval failed" >&2; exit 1; }
-unset declare
+unset -f declare

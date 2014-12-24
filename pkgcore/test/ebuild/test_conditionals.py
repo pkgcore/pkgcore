40def5513a2b0a98c51fcd9d27923493f6cb5359
Tim Harder <radhermit@gmail.com>
2014-12-23 18:03:56 -0800

    Revert "try to fix conditional use dep default handling for pcheck"
    This was fixed on the pcheck side instead for now. Use dep default
    handling probably needs to be improved during atom parsing though.

--- pkgcore/test/ebuild/test_conditionals.py
+++ pkgcore/test/ebuild/test_conditionals.py
@@ -312,8 +312,9 @@ class native_DepSetEvaluateTest(base):
                 " ".join(flag_set)),
             ("a/b[c,x0]", "a/b[c?,%s]" % (",".join(x + "?" for x in flag_set)), "c",
                 " ".join(flag_set[1:])),
-            ("a/b[c]", "a/b[c?,%s]" % (",".join(x + "?" for x in flag_set)), "c",
-                " ".join(flag_set)),
+            ("a/b[c,%s]" % (','.join(flag_set),),
+                "a/b[c?,%s]" % (",".join(x + "?" for x in flag_set)), "c",
+                ""),
             ):
 
             result = vals[0]

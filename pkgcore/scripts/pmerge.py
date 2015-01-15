ae07b5b7809d23c608d4943ecd63eb6f2f33dfe3
Tim Harder <radhermit@gentoo.org>
2015-01-15 17:45:17 -0500

    pmerge: try to use quoting in desc that won't get stripped by sphinx

--- pkgcore/scripts/pmerge.py
+++ pkgcore/scripts/pmerge.py
@@ -9,7 +9,7 @@ system. It provides an interface to install, update, and uninstall ebuilds from
 source or binary packages.
 
 With regards to portage compatibility, pmerge provides much of the same
-functionality that `emerge` does. In general, it should be possible to use both
+functionality that `emerge' does. In general, it should be possible to use both
 pmerge and emerge on the same system in a sane fashion. For example, pmerge can
 be used to install packages and then emerge should be able to upgrade or
 uninstall them, or vice versa. Also, binary packages created using pmerge
@@ -18,7 +18,7 @@ issue that arises when trying to use both package managers is probably a bug
 and should be reported.
 
 In terms of option naming, pmerge tries to remain somewhat compatible to
-portage so running `pmerge -1av` should work the same as `emerge -1av` when
+portage so running `pmerge -1av' should work the same as `emerge -1av' when
 using portage. However, pmerge doesn't implement nearly the same amount of
 options that portage provides so many of the more obscure ones are missing. In
 addition, pmerge defaults to a portage compatible output format that closely

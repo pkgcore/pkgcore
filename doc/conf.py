5a409a33eb2442ec725e76eb9ef687193655d34c
Tim Harder <radhermit@gmail.com>
2015-04-09 02:20:55 -0400

    doc: munge sys.path to allow use of the repo module

--- doc/conf.py
+++ doc/conf.py
@@ -15,12 +15,12 @@ from importlib import import_module
 import os
 import sys
 
-from pkgcore.const import VERSION as pkgcore_version
-
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-#sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(1, os.path.abspath('..'))
+
+from pkgcore.const import VERSION as pkgcore_version
 
 # -- General configuration -----------------------------------------------------
 

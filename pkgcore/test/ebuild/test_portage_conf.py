3d739c4cc0ee541caf80f193a636539ef259666a
Tim Harder <radhermit@gmail.com>
2015-06-28 14:44:22 -0400

    test: add dir loading, overrides, and incrementals for load_make_conf

--- pkgcore/test/ebuild/test_portage_conf.py
+++ pkgcore/test/ebuild/test_portage_conf.py
@@ -1,6 +1,7 @@
 # Copyright: 2015 Tim Harder
 # License: GPL2/BSD
 
+import shutil
 from tempfile import NamedTemporaryFile
 
 from snakeoil.osutils import pjoin
@@ -23,16 +24,37 @@ class TestPortageConfig(TempDirMixin, TestCase):
 
         # nonexistent file
         d = {}
+        # by default files are required
         self.assertRaises(
             errors.ParsingError, load_make_conf,
-            d, pjoin(self.dir, 'make.conf'))
+            d, pjoin(self.dir, 'make.globals'))
         # should return empty dict when not required
         load_make_conf(d, pjoin(self.dir, 'make.conf'), required=False)
         self.assertEqual({}, d)
 
-        # TODO: loading dirs
+        # overrides and incrementals
+        with NamedTemporaryFile() as f:
+            f.write('DISTDIR=foo\nACCEPT_LICENSE=foo\n')
+            f.flush()
+            d = {}
+            load_make_conf(d, pjoin(const.CONFIG_PATH, 'make.globals'))
+            load_make_conf(d, f.name, allow_sourcing=True, incrementals=True)
+            self.assertEqual('foo', d['DISTDIR'])
+            self.assertEqual(
+                ' '.join([default_make_conf['ACCEPT_LICENSE'], 'foo']),
+                d['ACCEPT_LICENSE'])
 
-        # TODO: loading additional files and with incrementals and overrides
+        # load files from dir
+        with NamedTemporaryFile(prefix='aaa', dir=self.dir) as f:
+            with NamedTemporaryFile(prefix='zzz', dir=self.dir) as g:
+                shutil.copyfile(pjoin(const.CONFIG_PATH, 'make.globals'), f.name)
+                g.write('DISTDIR=foo\n')
+                g.flush()
+                d = {}
+                load_make_conf(d, self.dir)
+                self.assertEqual(
+                    default_make_conf['ACCEPT_LICENSE'], d['ACCEPT_LICENSE'])
+                self.assertEqual('foo', d['DISTDIR'])
 
     def test_load_repos_conf(self):
         # default file

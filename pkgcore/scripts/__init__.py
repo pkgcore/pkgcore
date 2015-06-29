1fe5d3b1a1715dde1d79752c93fb9f59cf75a852
Tim Harder <radhermit@gmail.com>
2015-06-29 02:12:42 -0400

    scripts: update base documentation for script handling

--- pkgcore/scripts/__init__.py
+++ pkgcore/scripts/__init__.py
@@ -3,14 +3,14 @@
 
 """Commandline scripts.
 
-Modules in here are accessible through the pwrapper script. They
-should have an C{OptionParser} attribute that is a
-:obj:`snakeoil.commandline.OptionParser` subclass and a C{main}
-attribute that is a function usable with
-:obj:`snakeoil.commandline.main`.
+Modules in here are accessible through the pwrapper script. They should have a
+C{main} attribute that is a function usable with
+:obj:`pkgcore.util.commandline.main` and use
+:obj:`pkgcore.util.commandline.mk_argparser` (a wrapper around
+C{ArgumentParser}) to handle argument parsing.
 
-The goal of this is avoiding boilerplate and making sure the scripts
-have a similar look and feel. If your script needs to do something
-:obj:`snakeoil.commandline` does not support please improve it instead
-of bypassing it.
+The goal of this is avoiding boilerplate and making sure the scripts have a
+similar look and feel. If your script needs to do something
+:obj:`pkgcore.util.commandline` does not support please improve it instead of
+bypassing it.
 """

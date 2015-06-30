48e9e04f617d98b38d50f5e0cca93b89ae665b73
Tim Harder <radhermit@gmail.com>
2015-06-30 00:25:10 -0400

    test: fix cvs sync parsing tests

--- pkgcore/test/sync/test_cvs.py
+++ pkgcore/test/sync/test_cvs.py
@@ -12,22 +12,23 @@ valid = make_valid_syncer(cvs.cvs_syncer)
 class TestcvsParsing(TestCase):
 
     def test_parse(self):
-        self.assertRaises(base.syncer_exception, bogus,
+        self.assertRaises(
+            base.syncer_exception, bogus,
             "/tmp/foon", "cvs+hopefully_nonexistent_binary://foon.com/dar")
         o = valid("/tmp/foon", "cvs://dar:module")
         self.assertEqual(o.uri, ":anoncvs:dar")
         self.assertEqual(o.module, "module")
         self.assertEqual(o.rsh, None)
-        self.assertEqual(o.env, {"CVSROOT":":anoncvs:dar"})
+        self.assertEqual(o.env["CVSROOT"], ":anoncvs:dar")
 
         o = valid("/tmp/foon", "cvs+pserver://dar:module")
         self.assertEqual(o.uri, ":pserver:dar")
         self.assertEqual(o.module, "module")
         self.assertEqual(o.rsh, None)
-        self.assertEqual(o.env, {"CVSROOT":":pserver:dar"})
+        self.assertEqual(o.env["CVSROOT"], ":pserver:dar")
 
         o = valid("/tmp/foon", "cvs+/bin/sh://dar:module")
         self.assertEqual(o.rsh, "/bin/sh")
         self.assertEqual(o.uri, ":ext:dar")
-        self.assertEqual(sorted(o.env.iteritems()),
-            sorted({"CVSROOT":":ext:dar", "CVS_RSH":"/bin/sh"}.iteritems()))
+        self.assertEqual(o.env["CVSROOT"], ":ext:dar")
+        self.assertEqual(o.env["CVS_RSH"], "/bin/sh")

690fd14ef65c8534b8c3002d6f752b5381f7775f
Tim Harder <radhermit@gmail.com>
2015-07-26 16:35:35 -0400

    move build_scripts functionality to pkgdist
    So pkgcheck can easily use it too.

--- wrapper.py
+++ wrapper.py
@@ -8,13 +8,26 @@ from importlib import import_module
 import os
 import sys
 
+
+def project():
+    toplevel = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+    toplevel_depth = len(toplevel.split('/'))
+
+    for root, dirs, files in os.walk(toplevel):
+        if len(root.split('/')) > toplevel_depth + 1:
+            continue
+        if '__init__.py' in files and not \
+                os.path.abspath(root).startswith(os.path.dirname(os.path.realpath(__file__))):
+            return os.path.basename(root)
+    raise ValueError('No project module found')
+
+
 if __name__ == '__main__':
     # we're in a git repo or tarball so add the base dir to the system path
-    repo_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-    sys.path.insert(1, repo_path)
+    sys.path.insert(1, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
     try:
-        scripts = import_module('%s.scripts' % os.path.basename(repo_path))
+        scripts = import_module('%s.scripts' % os.path.basename(project()))
     except ImportError as e:
         raise
 

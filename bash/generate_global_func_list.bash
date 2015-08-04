023c2e6db1824b844d88d91e20d54f1409723107
Tim Harder <radhermit@gmail.com>
2015-08-04 02:14:48 -0400

    bash: keep PATH when generating global function list
    Otherwise file generation in the setup.py script during install goes
    into an infinite loop probably due to filter-env being called in
    ebuild.lib's global scope.

--- bash/generate_global_func_list.bash
+++ bash/generate_global_func_list.bash
@@ -6,7 +6,7 @@
 export PKGCORE_BIN_PATH=$(dirname "$0")
 
 if [[ -z ${PKGCORE_CLEAN_ENV} ]]; then
-	exec env -i PKGCORE_PYTHON_PATH=${PKGCORE_PYTHON_PATH} PKGCORE_CLEAN_ENV=1 "$0"
+	exec env -i PKGCORE_PYTHON_PATH=${PKGCORE_PYTHON_PATH} PATH=${PATH} PKGCORE_CLEAN_ENV=1 "$0"
 fi
 
 export LC_ALL=C # avoid any potential issues of unicode sorting for whacked func names

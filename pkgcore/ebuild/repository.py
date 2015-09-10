5481054855fdf89ba8dce35c91d32fa9dea5ff9a
Tim Harder <radhermit@gmail.com>
2015-09-10 16:45:11 -0400

    ebuild/repository: respect package.mask dirs in supported profiles
    Previously only portage-1 profiles would have worked for using dirs for
    package.mask, now we allow portage-1 and portage-2.

--- pkgcore/ebuild/repository.py
+++ pkgcore/ebuild/repository.py
@@ -486,7 +486,7 @@ class _UnconfiguredTree(prototype.tree):
         path = pjoin(self.base, 'profiles', 'package.mask')
         pos, neg = [], []
         try:
-            if not self.config.profile_formats.intersection(['pms', 'portage-2']):
+            if self.config.profile_formats.intersection(['portage-1', 'portage-2']):
                 paths = sorted_scan(path)
             else:
                 paths = [path]

daa84758e64b08ef499a98a2e8e94409b47f8e0b
Tim Harder <radhermit@gmail.com>
2016-01-16 05:03:51 -0500

    shell: reorganize shell support
    Bash and Zsh code needed to be split out a bit in order for
    them to work as expected in both cases.

--- shell/pkgcore.sh
+++ shell/pkgcore.sh
@@ -4,51 +4,14 @@
 # Note that most functions currently use non-POSIX features so bash or zsh are
 # basically required.
 
-# get an attribute for a given package
-_pkgattr() {
-	local pkg_attr=$1 pkg_atom=$2 repo=$3 p
-	local -a pkg
+PKGSHELL=$(ps -p $$ -ocomm=)
+if [[ ${PKGSHELL} != "bash" && ${PKGSHELL} != "zsh" ]]; then
+	echo "$(basename ${0}): unsupported shell: ${PKGSHELL}" >&2
+	exit 1
+fi
+source "$(dirname ${0})/${PKGSHELL}/pkgcore.${PKGSHELL}"
 
-	if [[ -z ${pkg_atom} ]]; then
-		echo "Enter a valid package name." >&2
-		return 1
-	fi
-
-	if [[ -n ${repo} ]]; then
-		IFS=$'\n' pkg=( $(pquery -r "${repo}" --raw --unfiltered --cpv --one-attr "${pkg_attr}" -n -- "${pkg_atom}" 2>/dev/null) )
-	else
-		IFS=$'\n' pkg=( $(pquery --ebuild-repos --raw --unfiltered --cpv --one-attr "${pkg_attr}" -n -- "${pkg_atom}" 2>/dev/null) )
-	fi
-	if [[ $? != 0 ]]; then
-		echo "Invalid package atom: '${pkg_atom}'" >&2
-		return 1
-	fi
-
-	if [[ -z ${pkg[@]} ]]; then
-		echo "No matches found." >&2
-		return 1
-	elif [[ ${#pkg[@]} > 1 ]]; then
-		echo "Multiple matches found:" >&2
-		for p in ${pkg[@]}; do
-			echo ${p%:*} >&2
-		done
-		return 1
-	fi
-	echo ${pkg#*:}
-}
-
-# cross-shell compatible PATH searching
-_which() {
-	local shell=$(basename ${SHELL})
-	if [[ ${shell} == "bash" ]]; then
-		type -P "$1" >/dev/null
-	elif [[ ${shell} == "zsh" ]]; then
-		whence -p "$1" >/dev/null
-	else
-		which "$1" >/dev/null
-	fi
-	return $?
-}
+export PATH="$(dirname ${0})"/bin:${PATH}
 
 # change to a package directory
 #
@@ -73,26 +36,4 @@ pcd() {
 	done
 
 	pushd "${pkgpath}" >/dev/null
-}
-
-# open a package's homepage in a browser
-#
-# usage: esite pkg [repo]
-# example: esite sys-devel/gcc gentoo
-#
-# If a package has more than one homepage listed the first is selected.
-#
-# Note that this requires xdg-utils to be installed for xdg-open.
-esite() {
-	local homepage=$(_pkgattr homepage "$@")
-	# only select the first homepage in a list
-	homepage=${homepage%% *}
-	[[ -z ${homepage} ]] && return 1
-
-	if ! _which xdg-open; then
-		echo "xdg-open missing, install xdg-utils"
-		return 1
-	fi
-
-	(xdg-open "${homepage}" &>/dev/null &)
 }
---
+++ shell/zsh/pkgcore.zsh
@@ -0,0 +1,40 @@
+#!/usr/bin/env zsh
+# Common library of useful shell functions leveraging pkgcore functionality.
+
+# get an attribute for a given package
+_pkgattr() {
+	local pkg_attr=$1 pkg_atom=$2 repo=$3 p
+	local -a pkg
+
+	if [[ -z ${pkg_atom} ]]; then
+		echo "Enter a valid package name." >&2
+		return 1
+	fi
+
+	if [[ -n ${repo} ]]; then
+		pkg=("${(@f)$(pquery -r "${repo}" --raw --unfiltered --cpv --one-attr "${pkg_attr}" -n -- "${pkg_atom}" 2>/dev/null)}")
+	else
+		pkg=("${(@f)$(pquery --ebuild-repos --raw --unfiltered --cpv --one-attr "${pkg_attr}" -n -- "${pkg_atom}" 2>/dev/null)}")
+	fi
+	if [[ $? != 0 ]]; then
+		echo "Invalid package atom: '${pkg_atom}'" >&2
+		return 1
+	fi
+
+	if [[ -z ${pkg[@]} ]]; then
+		echo "No matches found." >&2
+		return 1
+	elif [[ ${#pkg[@]} > 1 ]]; then
+		echo "Multiple matches found:" >&2
+		for p in ${pkg[@]}; do
+			echo ${p%%:*} >&2
+		done
+		return 1
+	fi
+	echo ${pkg#*:}
+}
+
+# cross-shell compatible PATH searching
+_which() {
+	whence -p "$1" >/dev/null
+}

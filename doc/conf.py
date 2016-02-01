5f6d53b3c76d6d45f78c4071abedaa531160ad1e
Tim Harder <radhermit@gmail.com>
2016-01-31 20:02:20 -0500

    doc: ignore generated files
    Drops a few of the doc not included any toctree warnings.

--- doc/conf.py
+++ doc/conf.py
@@ -82,7 +82,7 @@ release = 'trunk'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
-exclude_patterns = ['_build']
+exclude_patterns = ['_build', 'generated']
 
 # The reST default role (used for this markup: `text`) to use for all documents.
 #default_role = None

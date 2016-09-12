6ac861a87a514dc9a2123601f8067eb3828f4840
Tim Harder <radhermit@gmail.com>
2016-09-12 17:53:09 -0400

    toss official py33 support
    Use py34 or py35.

--- setup.py
+++ setup.py
@@ -223,7 +223,6 @@ setup(
         'License :: OSI Approved :: BSD License',
         'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
     ],

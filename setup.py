f3b410b42e1ab418d811f2b6ef2f42badf72212c
Tim Harder <radhermit@gmail.com>
2017-01-30 03:08:06 -0500

    setup.py: bump required version of snakeoil
    Note that 0.7.2 is not released yet so use git if using pkgcore from
    git.

--- setup.py
+++ setup.py
@@ -208,7 +208,7 @@ setup(
     author='Brian Harring, Tim Harder',
     author_email='pkgcore-dev@googlegroups.com',
     packages=find_packages(),
-    install_requires=['snakeoil>=0.7.0'],
+    install_requires=['snakeoil>=0.7.2'],
     scripts=os.listdir('bin'),
     data_files=list(chain(
         pkgdist.data_mapping(EBD_INSTALL_OFFSET, 'ebd'),

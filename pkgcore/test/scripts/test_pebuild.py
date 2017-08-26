dac9815fb7a110e9e129e1cabbf431dd2e1d8189
Tim Harder <radhermit@gmail.com>
2017-08-26 04:05:32 -0400

    test.scripts.test_pebuild: fix broken pebuild test due to adding arg validation
    Also, add a few more tests.

--- pkgcore/test/scripts/test_pebuild.py
+++ pkgcore/test/scripts/test_pebuild.py
@@ -1,18 +1,47 @@
 # Copyright: 2006 Marien Zwart <marienz@gentoo.org>
 # License: BSD/GPL2
 
-from snakeoil import compatibility
-
+from pkgcore.config import basics, ConfigHint, configurable
 from pkgcore.scripts import pebuild
+from pkgcore.test.misc import FakePkg, FakeRepo
 from pkgcore.test.scripts.helpers import ArgParseMixin
+
+from snakeoil import compatibility
 from snakeoil.test import TestCase
+
+
+class FakeDomain(object):
+
+    pkgcore_config_type = ConfigHint({'repo': 'ref:repo'}, typename='domain')
+
+    def __init__(self, repo):
+        object.__init__(self)
+        self.ebuild_repos_raw = repo
+
+
+@configurable(typename='repo')
+def fake_repo():
+    pkgs = [
+        FakePkg('app-arch/bzip2-1.0.1-r1', slot='0'),
+        FakePkg('app-arch/bzip2-1.0.5-r2', slot='0'),
+        FakePkg('sys-apps/coreutils-8.25', slot='0'),
+        FakePkg('x11-libs/gtk+-2.24', slot='2'),
+        FakePkg('x11-libs/gtk+-3.22', slot='3'),
+    ]
+    repo = FakeRepo(repo_id='gentoo', pkgs=pkgs)
+    return repo
+
+
+domain_config = basics.HardCodedConfigSection({
+        'class': FakeDomain,
+        'repo': basics.HardCodedConfigSection({'class': fake_repo}),
+        'default': True,
+        })
 
 
 class CommandlineTest(TestCase, ArgParseMixin):
 
     _argparser = pebuild.argparser
-
-    suppress_domain = True
 
     def test_parser(self):
         if compatibility.is_py3k:
@@ -21,4 +50,16 @@ class CommandlineTest(TestCase, ArgParseMixin):
         else:
             self.assertError('too few arguments')
             self.assertError('too few arguments', 'dev-util/diffball')
-        self.assertEqual(self.parse('foo/bar', 'baz', 'spork').phase, ['baz', 'spork'])
+
+        self.assertError("no matches: 'foo/bar'", 'foo/bar', 'baz', 'spork', domain=domain_config)
+
+        # select highest version of a package with multiple versions
+        config = self.parse('app-arch/bzip2', 'baz', 'spork', domain=domain_config)
+        self.assertEqual(config.pkg, FakePkg('app-arch/bzip2-1.0.5-r2'))
+
+        # packages with multiple slots require a specific slot selection
+        self.assertError("please refine your restriction to one match", 'x11-libs/gtk+', 'baz', 'spork', domain=domain_config)
+
+        # working initialization
+        config = self.parse('sys-apps/coreutils', 'bar', 'baz', domain=domain_config)
+        self.assertEqual(config.phase, ['bar', 'baz'])

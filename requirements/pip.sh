286066788f1401b4705d1e45907d80ea87fd273a
Tim Harder <radhermit@gmail.com>
2017-09-28 05:46:44 -0400

    tox: update to use pip.sh shim to have release -> git fallback for deps

---
+++ requirements/pip.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#
+# `pip install` shim that installs packages passed to it as well as trying to
+# install released build/install deps and then falling back to using git.
+
+DIR=${BASH_SOURCE[0]%/*}
+PACKAGES=( "$@" )
+
+# Try installing the latest build/runtime deps once, if they don't exist
+# install directly from the git.
+INSTALLED="${VIRTUAL_ENV}"/.installed_deps
+if [[ ! -f ${INSTALLED} ]]; then
+	touch "${INSTALLED}"
+
+	pip install -r "${DIR}"/build.txt 2>/dev/null
+	ret=$?
+
+	if [[ ${ret} -eq 0 ]]; then
+		pip install -r "${DIR}"/install.txt 2>/dev/null
+		ret=$?
+	fi
+
+	if [[ ${ret} -ne 0 ]]; then
+		pip install -r "${DIR}"/dev.txt
+		ret=$?
+	fi
+fi
+
+# install packages passed to us via tox
+for package in "${PACKAGES[@]}"; do
+	pip install ${package}
+done

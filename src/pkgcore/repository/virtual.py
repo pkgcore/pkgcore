cc903b17cf3e4b307ad12dd13ef8e3848f3b8c7d
Tim Harder <radhermit@gmail.com>
2017-12-14 07:09:46 -0500

    repository.virtual: InjectedPkg: define the remaining major attrs
    That are found in the base pkg class.

--- src/pkgcore/repository/virtual.py
+++ src/pkgcore/repository/virtual.py
@@ -62,21 +62,19 @@ class tree(prototype.tree):
 
 class InjectedPkg(pkg_base.wrapper):
 
-    __slots__ = ("repo", "repo_id", "built", "depends", "rdepends", "post_rdepends")
+    __slots__ = ("repo", "repo_id", "built", "depends", "rdepends", "post_rdepends",
+                 "versioned_atom", "unversioned_atom")
     default_depends = default_rdepends = default_post_rdepends = DepSet()
     package_is_real = False
+    is_supported = True
 
     def __init__(self, raw_pkg, repo):
         pkg_base.wrapper.__init__(self, raw_pkg)
         object.__setattr__(self, "repo", repo)
         object.__setattr__(self, "repo_id", repo.repo_id)
-        if self.repo.livefs:
-            built = True
-        else:
-            built = False
-        object.__setattr__(self, "built", built)
-
-        # underlying pkg deps are empty
+        object.__setattr__(self, "built", repo.livefs)
+        object.__setattr__(self, "versioned_atom", str(self._raw_pkg))
+        object.__setattr__(self, "unversioned_atom", self._raw_pkg.key)
         object.__setattr__(self, "depends", self.default_depends)
         object.__setattr__(self, "rdepends", self.default_rdepends)
         object.__setattr__(self, "post_rdepends", self.default_post_rdepends)

25dcba869b17314c4fc30252981624e8ec0fb517
Tim Harder <radhermit@gmail.com>
2018-10-01 00:58:52 -0400

    tests: fix py3 eq hash inheritance check

---
+++ tests/test_eq_hash_inheritance.py
@@ -0,0 +1,10 @@
+# Copyright: 2010 Brian Harring <ferringb@gmail.com>
+# License: GPL2/BSD
+
+from snakeoil.test.eq_hash_inheritance import Test
+
+
+class Test_Eq_Hash(Test):
+
+    target_namespace = "pkgcore"
+    ignore_all_import_failures = True

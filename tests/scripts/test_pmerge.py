105e68b92f6714710befae0509587368f3a699f8
Tim Harder <radhermit@gmail.com>
2018-11-06 01:28:51 -0600

    tests: update pmerge target parsing tests to use pytest

--- tests/scripts/test_pmerge.py
+++ tests/scripts/test_pmerge.py
@@ -1,70 +1,74 @@
 # Copyright: 2006 Marien Zwart <marienz@gentoo.org>
 # License: BSD/GPL2
 
-from pkgcore.config import basics
-from pkgcore.ebuild import formatter
-from pkgcore.repository import util
+import pytest
+
+from pkgcore.repository.util import SimpleTree
 from pkgcore.scripts import pmerge
 from pkgcore.util.parserestrict import parse_match
-from snakeoil.test import TestCase
-
-default_formatter = basics.HardCodedConfigSection({
-    'class': formatter.basic_factory,
-    'default': True,
-})
 
 
-class TargetParsingTest(TestCase):
+# TODO: make repo objs into configurable fixtures
+class TestTargetParsing(object):
 
-    def test_parse_target(self):
-        repo = util.SimpleTree({'spork': {'foon': ('1', '1.0.1', '2')}})
-        installed_repos = util.SimpleTree({'foo': {'bar': ('1')}})
+    def test_base_targets(self):
+        repo = SimpleTree({'spork': {'foon': ('1', '1.0.1', '2')}})
+        installed_repos = SimpleTree({'foo': {'bar': ('1')}})
         for cat in ('', 'spork/'):
             a = pmerge.parse_target(parse_match(f'={cat}foon-1'), repo, installed_repos)
-            self.assertEqual(len(a), 1)
-            self.assertEqual(a[0].key, 'spork/foon')
-            self.assertEqual(
-                [x.fullver for x in repo.itermatch(a[0])],
-                ['1'])
+            assert len(a) == 1
+            assert a[0].key == 'spork/foon'
+            assert [x.fullver for x in repo.itermatch(a[0])] == ['1']
             a = pmerge.parse_target(parse_match(f'{cat}foon'), repo, installed_repos)
-            self.assertEqual(len(a), 1)
-            self.assertEqual(a[0].key, 'spork/foon')
-            self.assertEqual(
-                sorted(x.fullver for x in repo.itermatch(a[0])),
-                sorted(['1', '1.0.1', '2']))
+            assert len(a) == 1
+            assert a[0].key == 'spork/foon'
+            assert (
+                sorted(x.fullver for x in repo.itermatch(a[0])) ==
+                sorted(['1', '1.0.1', '2'])
+            )
 
-        repo = util.SimpleTree({
+    def test_no_matches(self):
+        repo = SimpleTree({
             'spork': {'foon': ('1',)},
             'spork2': {'foon': ('2',)}})
-        self.assertRaises(
-            pmerge.NoMatches,
-            pmerge.parse_target, parse_match("foo"), repo, installed_repos)
-        self.assertRaises(
-            pmerge.AmbiguousQuery,
-            pmerge.parse_target, parse_match("foon"), repo, installed_repos)
-        # test unicode conversion.
-        a = pmerge.parse_target(parse_match('=spork/foon-1'), repo, installed_repos)
-        self.assertEqual(len(a), 1)
-        self.assertEqual(a[0].key, 'spork/foon')
-        self.assertTrue(isinstance(a[0].key, str))
-        # test globbing
-        a = pmerge.parse_target(parse_match('*/foon'), repo, installed_repos)
-        self.assertEqual(len(a), 2)
+        installed_repos = SimpleTree({'foo': {'bar': ('1')}})
+        with pytest.raises(pmerge.NoMatches):
+            pmerge.parse_target(parse_match("foo"), repo, installed_repos)
 
+    def test_ambiguous(self):
+        repo = SimpleTree({
+            'spork': {'foon': ('1',)},
+            'spork2': {'foon': ('2',)}})
+        installed_repos = SimpleTree({'foo': {'bar': ('1')}})
+        with pytest.raises(pmerge.AmbiguousQuery):
+            pmerge.parse_target(parse_match("foon"), repo, installed_repos)
+
+    def test_globbing(self):
+        repo = SimpleTree({
+            'spork': {'foon': ('1',)},
+            'spork2': {'foon': ('2',)}})
+        installed_repos = SimpleTree({'foo': {'bar': ('1')}})
+        a = pmerge.parse_target(parse_match('*/foon'), repo, installed_repos)
+        assert len(a) == 2
+
+    def test_collisions_repo(self):
         # test pkg name collisions between real and virtual pkgs in a repo, but not installed
         # repos, the real pkg will be selected over the virtual
-        installed_repos = util.SimpleTree({'foo': {'baz': ('1')}})
-        repo = util.SimpleTree({'foo': {'bar': ('1',)}, 'virtual': {'bar': ('1',)}})
+        installed_repos = SimpleTree({'foo': {'baz': ('1')}})
+        repo = SimpleTree({'foo': {'bar': ('1',)}, 'virtual': {'bar': ('1',)}})
         a = pmerge.parse_target(parse_match("bar"), repo, installed_repos)
-        self.assertEqual(len(a), 1)
-        self.assertEqual(a[0].key, 'foo/bar')
-        self.assertTrue(isinstance(a[0].key, str))
+        assert len(a) == 1
+        assert a[0].key == 'foo/bar'
+        assert isinstance(a[0].key, str)
 
+    def test_collisions_livefs(self):
         # test pkg name collisions between real and virtual pkgs on livefs
         # repos, the real pkg will be selected over the virtual
-        installed_repos = util.SimpleTree({'foo': {'bar': ('1')}, 'virtual': {'bar': ('0')}})
-        repo = util.SimpleTree({'foo': {'bar': ('1',)}, 'virtual': {'bar': ('1',)}})
+        installed_repos = SimpleTree({'foo': {'bar': ('1')}, 'virtual': {'bar': ('0')}})
+        repo = SimpleTree({'foo': {'bar': ('1',)}, 'virtual': {'bar': ('1',)}})
         a = pmerge.parse_target(parse_match("bar"), repo, installed_repos)
-        self.assertEqual(len(a), 1)
-        self.assertEqual(a[0].key, 'foo/bar')
-        self.assertTrue(isinstance(a[0].key, str))
+        assert len(a) == 1
+        assert a[0].key == 'foo/bar'
+        assert isinstance(a[0].key, str)
+
+    # TODO: add slotted (extra restrictions) collision test

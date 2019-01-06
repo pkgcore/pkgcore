a2daf80b6c9cae973438f6dae58bf61313b4a768
Tim Harder <radhermit@gmail.com>
2019-01-05 20:42:13 -0600

    package.errors: MetadataException: strip newlines from bash output

--- src/pkgcore/package/errors.py
+++ src/pkgcore/package/errors.py
@@ -20,7 +20,7 @@ class MetadataException(PackageError):
     def __init__(self, pkg, attr, error, verbose=None):
         super().__init__(f"metadata exception: pkg {pkg}, attr {attr}\nerror: {error}")
         self.pkg, self.attr, self.error = pkg, attr, error
-        self._verbose = verbose.strip()
+        self._verbose = verbose.strip('\n')
 
     def msg(self, verbosity=0):
         """Extract error message from verbose output depending on verbosity level."""
@@ -28,7 +28,7 @@ class MetadataException(PackageError):
         if self._verbose:
             if verbosity > 0:
                 # add full bash output in verbose mode
-                s += ":\n" + self._verbose.strip('\n')
+                s += f":\n{self._verbose}"
             else:
                 # extract context and die message from bash output
                 bash_error = [x.lstrip('* ') for x in self._verbose.split('\n')]

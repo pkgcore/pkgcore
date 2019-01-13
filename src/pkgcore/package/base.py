67ac4cdf344cc516238221d478f32dc9994fb684
Tim Harder <radhermit@gmail.com>
2019-01-13 03:04:18 -0600

    package.base: dynamic_getattr_dict: pass down verbose bash error when rewrapping MetadataExceptions

--- src/pkgcore/package/base.py
+++ src/pkgcore/package/base.py
@@ -93,6 +93,6 @@ def dynamic_getattr_dict(self, attr):
     except MetadataException as e:
         if e.attr == attr:
             raise
-        raise MetadataException(self, attr, e.error) from e
+        raise MetadataException(self, attr, e.error, e.verbose) from e
     except Exception as e:
         raise MetadataException(self, attr, str(e)) from e

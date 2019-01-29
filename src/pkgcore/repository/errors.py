9697cd22a138fa5f8cbe1e466b1e0964f11c8b71
Tim Harder <radhermit@gmail.com>
2019-01-28 23:12:28 -0600

    repository.errors: fix __str__ for error subclasses

--- src/pkgcore/repository/errors.py
+++ src/pkgcore/repository/errors.py
@@ -13,19 +13,22 @@ from pkgcore.exceptions import PkgcoreException, PkgcoreCliException
 class RepoError(PkgcoreException):
     """General repository error."""
 
+    def __init__(self, msg):
+        self.msg = msg
+
 
 class InitializationError(RepoError, PkgcoreCliException):
     """General repository initialization failure."""
 
     def __str__(self):
-        return f"repo init failed: {self.err}"
+        return f"repo init failed: {self.msg}"
 
 
 class InvalidRepo(RepoError, PkgcoreCliException):
     """Repository is not a repo or is otherwise invalid."""
 
     def __str__(self):
-        return f"invalid repo: {self.err}"
+        return f"invalid repo: {self.msg}"
 
 
 class UnsupportedRepo(RepoError, PkgcoreCliException):

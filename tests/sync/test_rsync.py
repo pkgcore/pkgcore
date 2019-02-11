185e6fd7ef725324111e5a8fcb88bb9ea207f3d5
Tim Harder <radhermit@gmail.com>
2019-02-11 03:07:21 -0600

    tests/sync/test_rsync: add initial real rsync syncer test

--- tests/sync/test_rsync.py
+++ tests/sync/test_rsync.py
@@ -1,9 +1,13 @@
 # Copyright: 2006 Brian Harring <ferringb@gmail.com>
 # License: GPL2/BSD
 
+import datetime
+import os
+
 import pytest
 
 from pkgcore.sync import base, rsync
+from pkgcore.sync.tar import tar_syncer
 from tests.sync.syncer import make_bogus_syncer, make_valid_syncer
 
 bogus = make_bogus_syncer(rsync.rsync_syncer)
@@ -23,3 +27,26 @@ class TestRsyncSyncer(object):
         o = valid("/tmp/foon", "rsync+/bin/sh://dar/module")
         assert o.uri == "rsync://dar/module/"
         assert o.rsh == "/bin/sh"
+
+
+@pytest.mark_network
+class TestRsyncSyncerReal(object):
+
+    def test_sync(self, tmp_path):
+        # perform a tarball sync for initial week-old base
+        path = tmp_path / 'repo'
+        now = datetime.datetime.now()
+        week_old = now - datetime.timedelta(days=7)
+        date_str = week_old.strftime("%Y%m%d")
+        syncer = tar_syncer(
+            str(path), f"http://distfiles.gentoo.org/snapshots/portage-{date_str}.tar.xz")
+        assert syncer.sync()
+        timestamp = os.path.join(path, 'metadata', 'timestamp.chk')
+        assert os.path.exists(timestamp)
+        stat = os.stat(timestamp)
+
+        # run rsync over the unpacked repo tarball to update to the latest tree
+        syncer = rsync.rsync_syncer(
+            str(path), "rsync://rsync.gentoo.org/gentoo-portage")
+        assert syncer.sync()
+        assert stat != os.stat(timestamp)

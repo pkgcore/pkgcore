e73536f26286b6226ec4e44e3ba34f7774cd81b2
Tim Harder <radhermit@gmail.com>
2019-02-28 21:27:23 -0600

    ebd/eapi/7: ban libopts in src_install specifically

--- ebd/eapi/7/phase.bash
+++ ebd/eapi/7/phase.bash
@@ -1,5 +1,3 @@
-PKGCORE_BANNED_FUNCS=( libopts )
-
 __econf_options_eapi7() {
 	if [[ $1 == *"--with-sysroot"* ]]; then
 		echo --with-sysroot="${ESYSROOT:-/}"
--- ebd/eapi/7/src_install.bash
+++ ebd/eapi/7/src_install.bash
@@ -1,1 +1,3 @@
+PKGCORE_BANNED_FUNCS=( libopts )
+
 dostrip() { __ebd_ipc_cmd ${FUNCNAME} "" "$@"; }

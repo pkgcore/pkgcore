02b7410afc9c21177a960283e3f418d2d7cb3d50
Tim Harder <radhermit@gmail.com>
2019-02-28 20:53:09 -0600

    ebd/eapi/0: limit *into and *opts commands to src_install scope
    They only affect do*, new*, and similar commands that are themselves
    limited to src_install.

--- ebd/eapi/0/phase.bash
+++ ebd/eapi/0/phase.bash
@@ -109,68 +109,6 @@ einstall() {
 has_version() { return $(__ebd_ipc_cmd ${FUNCNAME} "" "$@"); }
 best_version() { __ebd_ipc_cmd ${FUNCNAME} "" "$@"; }
 
-## Install destination commands
-
-into() {
-	${PKGCORE_PREFIX_SUPPORT} || local ED=${D}
-	if [[ $1 == "/" ]]; then
-		export DESTTREE=""
-	else
-		export DESTTREE=$1
-	fi
-}
-
-insinto() {
-	${PKGCORE_PREFIX_SUPPORT} || local ED=${D}
-	if [[ $1 == "/" ]]; then
-		export INSDESTTREE=""
-	else
-		export INSDESTTREE=$1
-	fi
-}
-
-exeinto() {
-	${PKGCORE_PREFIX_SUPPORT} || local ED=${D}
-	if [[ $1 == "/" ]]; then
-		export PKGCORE_EXEDESTTREE=""
-	else
-		export PKGCORE_EXEDESTTREE=$1
-	fi
-}
-
-docinto() {
-	${PKGCORE_PREFIX_SUPPORT} || local ED=${D}
-	if [[ $1 == "/" ]]; then
-		export PKGCORE_DOCDESTTREE=""
-	else
-		export PKGCORE_DOCDESTTREE=$1
-	fi
-}
-
-## Install options commands
-
-insopts() {
-	{ has -s "$@" || has --strip "$@"; } && \
-		ewarn "insopts shouldn't be given -s; stripping should be left to the manager."
-	export INSOPTIONS=$@
-}
-
-diropts() {
-	export DIROPTIONS=$@
-}
-
-exeopts() {
-	{ has -s "$@" || has --strip "$@"; } && \
-		ewarn "exeopts shouldn't be given -s; stripping should be left to the manager."
-	export EXEOPTIONS=$@
-}
-
-libopts() {
-	{ has -s "$@" || has --strip "$@"; } && \
-		ewarn "libopts shouldn't be given -s; stripping should be left to the manager."
-	export LIBOPTIONS=$@
-}
-
 ## USE flag commands
 
 use() {

ddcbf39f77d9470572e88ddcec1f3ded95499d75
Tim Harder <radhermit@gmail.com>
2019-03-01 02:33:58 -0600

    ebd/eapi/6: limit einstalldocs to src_install scope

---
+++ ebd/eapi/6/src_install.bash
@@ -0,0 +1,32 @@
+einstalldocs() {
+	local docs PKGCORE_DOCDESTTREE=
+	if ! docs=$(declare -p DOCS 2> /dev/null); then
+		local -a DOCS
+		for docs in README* ChangeLog AUTHORS NEWS TODO CHANGES \
+				THANKS BUGS FAQ CREDITS CHANGELOG; do
+			[[ -s ${docs} ]] && DOCS+=( ${docs} )
+		done
+		if [[ ${#DOCS[@]} -gt 0 ]]; then
+			dodoc "${DOCS[@]}" || return $?
+		fi
+	elif [[ ${docs} == "declare -a "* ]]; then
+		if [[ ${#DOCS[@]} -gt 0 ]]; then
+			dodoc -r "${DOCS[@]}" || return $?
+		fi
+	elif [[ -n ${DOCS} ]]; then
+		dodoc -r ${DOCS} || return $?
+	fi
+
+	PKGCORE_DOCDESTTREE=html
+	if ! docs=$(declare -p HTML_DOCS 2> /dev/null); then
+		:
+	elif [[ ${docs} == "declare -a "* ]]; then
+		if [[ ${#HTML_DOCS[@]} -gt 0 ]]; then
+			dodoc -r "${HTML_DOCS[@]}" || return $?
+		fi
+	elif [[ -n ${HTML_DOCS} ]]; then
+		dodoc -r ${HTML_DOCS} || return $?
+	fi
+
+	return 0
+}

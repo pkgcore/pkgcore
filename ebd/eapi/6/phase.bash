6c87331106e98cfcfdc917bfaa03beb8e3a57945
Tim Harder <radhermit@gmail.com>
2019-03-01 02:28:19 -0600

    ebd/eapi/6: einstalldocs: don't call dodoc per file

--- ebd/eapi/6/phase.bash
+++ ebd/eapi/6/phase.bash
@@ -16,12 +16,14 @@ get_libdir() { __get_libdir lib; }
 einstalldocs() {
 	local docs PKGCORE_DOCDESTTREE=
 	if ! docs=$(declare -p DOCS 2> /dev/null); then
+		local -a DOCS
 		for docs in README* ChangeLog AUTHORS NEWS TODO CHANGES \
 				THANKS BUGS FAQ CREDITS CHANGELOG; do
-			if [[ -s ${docs} ]]; then
-				dodoc "${docs}" || return $?
-			fi
+			[[ -s ${docs} ]] && DOCS+=( ${docs} )
 		done
+		if [[ ${#DOCS[@]} -gt 0 ]]; then
+			dodoc "${DOCS[@]}" || return $?
+		fi
 	elif [[ ${docs} == "declare -a "* ]]; then
 		if [[ ${#DOCS[@]} -gt 0 ]]; then
 			dodoc -r "${DOCS[@]}" || return $?

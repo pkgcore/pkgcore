ab8f92654afb233b7fb606eb9194a539742823dc
Tim Harder <radhermit@gmail.com>
2019-09-16 10:51:23 -0600

    tests: drop demandload usage tests
    We're using the new demandimport lazy loading mechanism now.

--- tests/module/test_source_hygene.py
+++ tests/module/test_source_hygene.py
@@ -1,10 +1,4 @@
-from snakeoil.test.demandload import DemandLoadTargets
 from snakeoil.test.modules import ExportedModules
-
-
-class TestDemandLoadUsage(DemandLoadTargets):
-    target_namespace = "pkgcore"
-    ignore_all_import_failures = True
 
 
 class Test_modules(ExportedModules):

1fc59e07ab68af035b681821294fca9a8d8f2f05
Tim Harder <radhermit@gmail.com>
2020-10-21 21:14:18 -0600

    ebuild.ebd_ipc: drop userpriv eapply()/unpack() support
    Using it causes breakage when unpacking inside dirs with root perms, e.g.
    recent firefox-bin ebuilds that unpack directly to subdirs under ${D}.

--- src/pkgcore/ebuild/ebd_ipc.py
+++ src/pkgcore/ebuild/ebd_ipc.py
@@ -963,9 +963,6 @@ class Eapply(IpcCommand):
             output_func = self.observer.info
 
         spawn_kwargs = {'collect_fds': (1, 2)}
-        if self.op.userpriv:
-            spawn_kwargs['uid'] = os_data.portage_uid
-            spawn_kwargs['gid'] = os_data.portage_gid
 
         for path, patches in args:
             prefix = ''
@@ -1070,18 +1067,13 @@ class Unpack(IpcCommand):
             yield archive, ext, path
 
     def run(self, args):
-        spawn_kwargs = {}
-        if self.op.userpriv:
-            spawn_kwargs['uid'] = os_data.portage_uid
-            spawn_kwargs['gid'] = os_data.portage_gid
-
         for filename, ext, source in args.targets:
             self.observer.write(f'>>> Unpacking {filename} to {self.cwd}', autoline=True)
             self.observer.flush()
             dest = pjoin(self.cwd, filename[:-len(ext)])
             try:
                 target = ArComp(source, ext=ext)
-                target.unpack(dest=dest, **spawn_kwargs)
+                target.unpack(dest=dest)
             except ArCompError as e:
                 raise IpcCommandError(str(e), code=e.code)
 

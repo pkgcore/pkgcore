fa294462c6898555433c1314ffc61631d8a50f29
Tim Harder <radhermit@gmail.com>
2020-11-02 01:16:44 -0700

    setup.py: explicitly note py39 support
    And add py39 support for tox testing.

--- setup.py
+++ setup.py
@@ -291,6 +291,7 @@ setup(**dict(pkgdist_setup,
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
         ],
     )
 )

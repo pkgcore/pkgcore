008a3e604ef8c5f493272dc82e92e866c1d8cc23
Tim Harder <radhermit@gmail.com>
2021-03-21 23:44:09 -0600

    util.packages: drop unnecessary groupby_key_getter

--- src/pkgcore/util/packages.py
+++ src/pkgcore/util/packages.py
@@ -1,7 +1,7 @@
 __all__ = ("get_raw_pkg", "groupby_pkg")
 
 import itertools
-import operator
+from operator import attrgetter
 
 
 def get_raw_pkg(pkg):
@@ -10,7 +10,7 @@ def get_raw_pkg(pkg):
         p = p._raw_pkg
     return p
 
-groupby_key_getter = operator.attrgetter("key")
+
 def groupby_pkg(iterable):
-    for key, pkgs in itertools.groupby(iterable, groupby_key_getter):
+    for key, pkgs in itertools.groupby(iterable, attrgetter('key')):
         yield pkgs

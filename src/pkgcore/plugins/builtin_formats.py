c4235069db9557733519a60ad4ef7164728aef99
Tim Harder <radhermit@gmail.com>
2021-03-28 19:59:56 -0600

    plugins: drop unused formats plugin

--- src/pkgcore/plugins/builtin_formats.py
+++
@@ -1,4 +0,0 @@
-pkgcore_plugins = {
-    'format.ebuild_src': ['pkgcore.ebuild.ebuild_src.generate_new_factory'],
-    'format.ebuild_built': ['pkgcore.ebuild.ebuild_built.generate_new_factory'],
-}

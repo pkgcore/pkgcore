0d5c320132cd2bf0f458bee8148ea8e3088a3b93
Tim Harder <radhermit@gmail.com>
2021-05-19 01:49:43 -0600

    setup.py: note py3.10 support

--- setup.py
+++ setup.py
@@ -263,5 +263,6 @@ setup(**dict(
         'License :: OSI Approved :: BSD License',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
     ],
 ))

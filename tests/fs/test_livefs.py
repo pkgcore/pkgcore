7575722c024bf5929a0953497f995ea194ee3a79
Arthur Zamarin <arthurzam@gentoo.org>
2022-08-03 23:46:54 +0300

    fs/test_livefs.py: modernize tests to pytest
    Signed-off-by: Arthur Zamarin <arthurzam@gentoo.org>

--- tests/fs/test_livefs.py
+++ tests/fs/test_livefs.py
@@ -1,171 +1,140 @@
 import os
+from pathlib import Path
+
+import pytest
 
 from pkgcore.fs import fs, livefs
 from pkgcore.fs.contents import contentsSet
-from snakeoil.osutils import pjoin
-from snakeoil.test import TestCase
-from snakeoil.test.mixins import TempDirMixin
 
 
-class FsObjsTest(TempDirMixin, TestCase):
+class TestFsObjs:
 
     def check_attrs(self, obj, path, offset=None):
         if offset is None:
-            st = os.lstat(path)
+            st = path.lstat()
         else:
-            st = os.lstat(offset + '/' + path)
+            st = (offset / path).lstat()
         if offset is not None:
-            self.assertTrue(
-                path.startswith("/"),
-                msg=f"path must be absolute, got {path!r}")
-        self.assertEqual(obj.mode & 0o7777, st.st_mode & 0o7777)
-        self.assertEqual(obj.uid, st.st_uid)
-        self.assertEqual(obj.gid, st.st_gid)
+            assert offset.is_absolute(), f"path must be absolute, got {path!r}"
+        assert (obj.mode & 0o7777) == (st.st_mode & 0o7777)
+        assert obj.uid == st.st_uid
+        assert obj.gid == st.st_gid
         if fs.isreg(obj):
             if offset is None:
-                self.assertEqual(obj.data.path, path)
+                assert obj.data.path == str(path)
             else:
-                self.assertEqual(obj.data.path,
-                    offset + path)
+                assert obj.data.path == str(offset / path)
 
     def test_data_source(self):
         o = livefs.gen_obj("/tmp/etc/passwd", real_location="/etc/passwd")
-        self.assertTrue(o.location, "/tmp/etc/passwd")
-        self.assertTrue(o.data.path, "/etc/passwd")
+        assert o.location, "/tmp/etc/passwd"
+        assert o.data.path, "/etc/passwd"
         with open("/etc/passwd", "rb") as f:
-            self.assertTrue(o.data.bytes_fileobj().read(), f.read())
+            assert o.data.bytes_fileobj().read(), f.read()
 
-    def test_gen_obj_reg(self):
-        path = os.path.join(self.dir, "reg_obj")
-        open(path, "w").close()
-        o = livefs.gen_obj(path)
-        self.assertTrue(fs.isreg(o))
+    def test_gen_obj_reg(self, tmp_path):
+        (path := tmp_path / "reg_obj").touch()
+        o = livefs.gen_obj(str(path))
+        assert fs.isreg(o)
         self.check_attrs(o, path)
-        o2 = livefs.gen_obj(path, inode=None)
+        o2 = livefs.gen_obj(str(path), inode=None)
         self.check_attrs(o, path)
-        self.assertNotEqual(o.inode, o2.inode)
+        assert o.inode != o2.inode
 
-    def test_gen_obj_dir(self):
-        o = livefs.gen_obj(self.dir)
-        self.assertTrue(fs.isdir(o))
-        self.check_attrs(o, self.dir)
+    def test_gen_obj_dir(self, tmp_path):
+        o = livefs.gen_obj(str(tmp_path))
+        assert fs.isdir(o)
+        self.check_attrs(o, tmp_path)
 
-    def test_gen_obj_sym(self):
-        path = os.path.join(self.dir, "test_sym")
-        os.mkdir(path)
-        src = os.path.join(path, "s")
-        link = os.path.join(path, "t")
-        open(src, "w").close()
-        os.symlink(src, link)
-        obj = livefs.gen_obj(link)
-        self.assertInstance(obj, fs.fsSymlink)
+    def test_gen_obj_sym(self, tmp_path):
+        (src := tmp_path / "s").touch()
+        (link := tmp_path / "t").symlink_to(src)
+        obj = livefs.gen_obj(str(link))
+        assert isinstance(obj, fs.fsSymlink)
         self.check_attrs(obj, link)
-        self.assertEqual(os.readlink(link), obj.target)
+        assert os.readlink(link) == obj.target
 
-    def test_gen_obj_fifo(self):
-        path = os.path.join(self.dir, "fifo")
-        os.mkfifo(path)
-        o = livefs.gen_obj(path)
+    def test_gen_obj_fifo(self, tmp_path):
+        os.mkfifo(path := tmp_path / "fifo")
+        o = livefs.gen_obj(str(path))
         self.check_attrs(o, path)
 
-    def test_iterscan(self):
-        path = os.path.join(self.dir, "iscan")
-        os.mkdir(path)
-        files = [os.path.normpath(os.path.join(path, x)) for x in [
-                "tmp", "blah", "dar"]]
-        # cheap version of a touch.
+    def test_iterscan(self, tmp_path):
+        (path := tmp_path / "iscan").mkdir()
+        files = [path / x for x in ("tmp", "blah", "dar")]
         for x in files:
-            open(x, "w").close()
-        dirs = [os.path.normpath(os.path.join(path, x)) for x in [
-                "a", "b", "c"]]
+            x.touch()
+        dirs = [path / x for x in ("a", "b", "c")]
         for x in dirs:
-            os.mkdir(x)
+            x.mkdir()
         dirs.append(path)
-        for obj in livefs.iter_scan(path):
-            self.assertInstance(obj, fs.fsBase)
+        for obj in livefs.iter_scan(str(path)):
+            assert isinstance(obj, fs.fsBase)
             if fs.isreg(obj):
-                self.assertTrue(obj.location in files)
+                assert Path(obj.location) in files
             elif fs.isdir(obj):
-                self.assertTrue(obj.location in dirs)
+                assert Path(obj.location) in dirs
             else:
-                raise Exception(f"unknown object popped up in testing dir, {obj!r}")
-            self.check_attrs(obj, obj.location)
+                pytest.fail(f"unknown object popped up in testing dir, {obj!r}")
+            self.check_attrs(obj, Path(obj.location))
+
         # do offset verification now.
-        offset = os.path.join(self.dir, "iscan")
-        for obj in livefs.iter_scan(path, offset=offset):
-            self.check_attrs(obj, obj.location, offset=offset)
+        offset = path
+        for obj in livefs.iter_scan(str(path), offset=str(offset)):
+            self.check_attrs(obj, Path(obj.location).relative_to('/'), offset=offset)
 
         seen = []
-        for obj in livefs.iter_scan(files[0]):
-            self.check_attrs(obj, obj.location)
+        for obj in livefs.iter_scan(str(files[0])):
+            self.check_attrs(obj, Path(obj.location))
             seen.append(obj.location)
-        self.assertEqual((files[0],), tuple(sorted(seen)))
+        assert [str(files[0])] == sorted(seen)
 
-    def test_sorted_scan(self):
-        path = os.path.join(self.dir, "sorted_scan")
-        os.mkdir(path)
-        files = [os.path.normpath(os.path.join(path, x)) for x in
-                 ["tmp", "blah", "dar"]]
-        # cheap version of a touch.
-        for x in files:
-            open(x, "w").close()
-        dirs = [os.path.normpath(os.path.join(path, x)) for x in [
-                "a", "b", "c"]]
-        for x in dirs:
-            os.mkdir(x)
+    def test_sorted_scan(self, tmp_path):
+        for x in ("tmp", "blah", "dar"):
+            (tmp_path / x).touch()
+        for x in ("a", "b", "c"):
+            (tmp_path / x).mkdir()
 
         # regular directory scanning
-        sorted_files = livefs.sorted_scan(path)
-        self.assertEqual(
-            list([pjoin(path, x) for x in ['blah', 'dar', 'tmp']]),
-            sorted_files)
+        sorted_files = livefs.sorted_scan(str(tmp_path))
+        assert sorted_files == [str(tmp_path / x) for x in ('blah', 'dar', 'tmp')]
 
         # nonexistent paths
-        nonexistent_path = os.path.join(self.dir, 'foobar')
-        sorted_files = livefs.sorted_scan(nonexistent_path)
-        self.assertEqual(sorted_files, [])
-        sorted_files = livefs.sorted_scan(nonexistent_path, nonexistent=True)
-        self.assertEqual(sorted_files, [nonexistent_path])
+        nonexistent_path = str(tmp_path / 'foobar')
+        assert livefs.sorted_scan(nonexistent_path) == []
+        assert livefs.sorted_scan(nonexistent_path, nonexistent=True) == [nonexistent_path]
 
-    def test_sorted_scan_hidden(self):
-        path = os.path.join(self.dir, "sorted_scan")
-        os.mkdir(path)
-        files = [os.path.normpath(os.path.join(path, x)) for x in
-                 [".tmp", "blah",]]
-        # cheap version of a touch.
-        for x in files:
-            open(x, "w").close()
-        sorted_files = livefs.sorted_scan(path)
-        assert list([pjoin(path, x) for x in ['.tmp', 'blah']]) == sorted_files
-        sorted_files = livefs.sorted_scan(path, hidden=False)
-        assert list([pjoin(path, x) for x in ['blah']]) == sorted_files
+    def test_sorted_scan_hidden(self, tmp_path):
+        for x in (".tmp", "blah"):
+            (tmp_path / x).touch()
 
-    def test_sorted_scan_backup(self):
-        path = os.path.join(self.dir, "sorted_scan")
-        os.mkdir(path)
-        files = [os.path.normpath(os.path.join(path, x)) for x in ["blah", "blah~"]]
-        # cheap version of a touch.
-        for x in files:
-            open(x, "w").close()
-        sorted_files = livefs.sorted_scan(path)
-        assert list([pjoin(path, x) for x in ['blah', 'blah~']]) == sorted_files
-        sorted_files = livefs.sorted_scan(path, backup=False)
-        assert list([pjoin(path, x) for x in ['blah']]) == sorted_files
+        sorted_files = livefs.sorted_scan(str(tmp_path))
+        assert [str(tmp_path / x) for x in ('.tmp', 'blah')] == sorted_files
+        sorted_files = livefs.sorted_scan(str(tmp_path), hidden=False)
+        assert [str(tmp_path / x) for x in ('blah', )] == sorted_files
 
-    def test_relative_sym(self):
-        f = os.path.join(self.dir, "relative-symlink-test")
-        os.symlink("../sym1/blah", f)
-        o = livefs.gen_obj(f)
-        self.assertTrue(o.target == "../sym1/blah")
+    def test_sorted_scan_backup(self, tmp_path):
+        for x in ("blah", "blah~"):
+            (tmp_path / x).touch()
 
-    def test_intersect(self):
-        open(pjoin(self.dir, 'reg'), 'w').close()
+        sorted_files = livefs.sorted_scan(str(tmp_path))
+        assert [str(tmp_path / x) for x in ("blah", "blah~")] == sorted_files
+        sorted_files = livefs.sorted_scan(str(tmp_path), backup=False)
+        assert [str(tmp_path / x) for x in ('blah', )] == sorted_files
+
+    def test_relative_sym(self, tmp_path):
+        (path := tmp_path / "relative-symlink-test").symlink_to("../sym1/blah")
+        o = livefs.gen_obj(str(path))
+        assert o.target == "../sym1/blah"
+
+    def test_intersect(self, tmp_path):
+        (tmp_path / 'reg').touch()
         cset = contentsSet([fs.fsFile('reg', strict=False)])
-        cset = cset.insert_offset(self.dir)
-        self.assertEqual(contentsSet(livefs.intersect(cset)), cset)
+        cset = cset.insert_offset(str(tmp_path))
+        assert contentsSet(livefs.intersect(cset)) == cset
         cset = contentsSet([fs.fsFile('reg/foon', strict=False),
             fs.fsFile('reg/dar', strict=False),
-            fs.fsDir('reg/dir', strict=False)]).insert_offset(self.dir)
-        self.assertEqual(list(livefs.intersect(cset)), [])
+            fs.fsDir('reg/dir', strict=False)]).insert_offset(str(tmp_path))
+        assert not list(livefs.intersect(cset))
         cset = contentsSet([fs.fsDir('reg', strict=False)])
-        self.assertEqual(list(livefs.intersect(cset)), [])
+        assert not list(livefs.intersect(cset))

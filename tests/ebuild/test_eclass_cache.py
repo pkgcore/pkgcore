d0a86104be6818825f3048c600f5ab14d0e6a844
Arthur Zamarin <arthurzam@gentoo.org>
2022-08-21 22:27:58 +0300

    ebuild/test_eclass_cache.py: modernize tests to pytest
    Signed-off-by: Arthur Zamarin <arthurzam@gentoo.org>

--- tests/ebuild/test_eclass_cache.py
+++ tests/ebuild/test_eclass_cache.py
@@ -1,11 +1,11 @@
 import os
 
+import pytest
+
 from pkgcore.ebuild import eclass_cache
 from snakeoil import data_source
 from snakeoil.chksum import LazilyHashedPath
 from snakeoil.osutils import pjoin
-from snakeoil.test import TestCase
-from snakeoil.test.mixins import TempDirMixin
 
 
 class FakeEclassCache(eclass_cache.base):
@@ -17,77 +17,72 @@ class FakeEclassCache(eclass_cache.base):
             "eclass2":LazilyHashedPath(path, mtime=200)}
 
 
-class TestBase(TestCase):
+class TestBase:
 
-    def setUp(self):
-        self.dir = '/nonexistent/path/'
-        self.ec = FakeEclassCache(self.dir)
-        self.ec_locs = {x: self.dir for x in ("eclass1", "eclass2")}
+    @pytest.fixture(autouse=True)
+    def _setup(self):
+        path = '/nonexistent/path/'
+        self.ec = FakeEclassCache(path)
+        self.ec_locs = {"eclass1": path, "eclass2": path}
 
-    def test_rebuild_eclass_entry(self):
-        def assertRebuildResults(result, *raw_data):
-            i = iter(raw_data)
-            data = [(ec, [('mtime', mtime)]) for ec, mtime in zip(i, i)]
-            got = self.ec.rebuild_cache_entry(data)
-            self.assertTrue(bool(got) == bool(result),
-                msg=f"Expected {result!r} from {raw_data!r}, got {got!r}")
-
-        assertRebuildResults(False, 'eclass3', 100)
-        assertRebuildResults(True, 'eclass1', 100)
-        assertRebuildResults(False, 'eclass1', 200)
+    @pytest.mark.parametrize(('result', 'ec', 'mtime'), (
+        (False, 'eclass3', 100),
+        (True,  'eclass1', 100),
+        (False, 'eclass1', 200),
+    ))
+    def test_rebuild_eclass_entry(self, result, ec, mtime):
+        data = [(ec, [('mtime', mtime)])]
+        got = self.ec.rebuild_cache_entry(data)
+        assert bool(got) == result
 
     def test_get_eclass_data(self):
         keys = list(self.ec.eclasses.keys())
         data = self.ec.get_eclass_data([])
-        self.assertIdentical(data, self.ec.get_eclass_data([]))
+        assert data is self.ec.get_eclass_data([])
         data = self.ec.get_eclass_data(keys)
-        self.assertIdentical(data, self.ec.get_eclass_data(keys))
-        self.assertEqual(sorted(keys), sorted(data))
+        assert data is self.ec.get_eclass_data(keys)
+        assert set(keys) == set(data)
         data = self.ec.get_eclass_data(["eclass1"])
-        self.assertEqual(data, {'eclass1':self.ec.eclasses['eclass1']})
+        assert data == {'eclass1': self.ec.eclasses['eclass1']}
 
 
-class TestEclassCache(TempDirMixin, TestBase):
+class TestEclassCache(TestBase):
 
-    def setUp(self):
-        TempDirMixin.setUp(self)
+    @pytest.fixture(autouse=True)
+    def _setup(self, tmp_path):
         for x, mtime in (("eclass1", 100), ("eclass2", 200)):
-            open(pjoin(self.dir, f"{x}.eclass"), "w").close()
-            os.utime(pjoin(self.dir, f"{x}.eclass"), (mtime, mtime))
+            (path := tmp_path / f"{x}.eclass").touch()
+            os.utime(path, (mtime, mtime))
         # insert a crap file to ensure it doesn't grab it.
-        open(pjoin(self.dir, 'foon-eclass'), 'w').close()
-        self.ec = eclass_cache.cache(self.dir)
-        self.ec_locs = {x: self.dir for x in ("eclass1", "eclass2")}
+        (path := tmp_path / "foon-eclass").touch()
+        self.ec = eclass_cache.cache(str(tmp_path))
+        self.ec_locs = {"eclass1": str(tmp_path), "eclass2": str(tmp_path)}
 
     def test_get_eclass(self):
         for x in ("eclass1", "eclass2"):
             handle = self.ec.get_eclass(x)
-            self.assertInstance(handle, data_source.local_source)
-            self.assertEqual(pjoin(self.ec_locs[x], f"{x}.eclass"),
-                handle.path)
+            assert isinstance(handle, data_source.local_source)
+            assert pjoin(self.ec_locs[x], f"{x}.eclass") == handle.path
 
         # note an eclass, thus shouldn't grab it.
-        self.assertEqual(None, self.ec.get_eclass("foon"))
-        self.assertEqual(None, self.ec.get_eclass("foon-eclass"))
+        assert self.ec.get_eclass("foon") is None
+        assert self.ec.get_eclass("foon-eclass") is None
 
 
 class TestStackedCaches(TestEclassCache):
 
-    def setUp(self):
-        TempDirMixin.setUp(self)
-        self.loc1 = pjoin(self.dir, "stack1")
-        self.loc2 = pjoin(self.dir, "stack2")
+    @pytest.fixture(autouse=True)
+    def _setup(self, tmp_path):
+        (loc1 := tmp_path / "stack1").mkdir()
+        (loc1 / 'eclass1.eclass').touch()
+        os.utime(loc1 / 'eclass1.eclass', (100, 100))
+        ec1 = eclass_cache.cache(str(loc1))
 
-        os.mkdir(self.loc1)
-        open(pjoin(self.loc1, 'eclass1.eclass'), 'w').close()
-        os.utime(pjoin(self.loc1, 'eclass1.eclass'), (100, 100))
-        self.ec1 = eclass_cache.cache(self.loc1)
-
-        os.mkdir(self.loc2)
-        open(pjoin(self.loc2, 'eclass2.eclass'), 'w').close()
-        os.utime(pjoin(self.loc2, 'eclass2.eclass'), (100, 100))
-        self.ec2 = eclass_cache.cache(self.loc2)
-        self.ec = eclass_cache.StackedCaches([self.ec1, self.ec2])
-        self.ec_locs = {"eclass1":self.loc1, "eclass2":self.loc2}
+        (loc2 := tmp_path / "stack2").mkdir()
+        (loc2 / 'eclass2.eclass').touch()
+        os.utime(loc2 / 'eclass2.eclass', (100, 100))
+        ec2 = eclass_cache.cache(str(loc2))
+        self.ec = eclass_cache.StackedCaches([ec1, ec2])
+        self.ec_locs = {"eclass1": str(loc1), "eclass2": str(loc2)}
         # make a shadowed file to verify it's not seen
-        open(pjoin(self.loc2, 'eclass1.eclass'), 'w').close()
+        (loc2 / 'eclass1.eclass').touch()

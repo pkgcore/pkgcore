52c15d2fc8ccb10b07c9172da69f9de459ee91b7
David Seifert <soap@gentoo.org>
2022-12-25 20:37:16 +0100

    Make EAPI 8 `--disable-static` logic libtool-specific
    * The intention has always been to only target `configure` scripts that use
      libtool, not just any script with a `--disable-static*` option.
    
    * libtool has been using the same `configure` format for at least
      the past 15 years (going back to libtool 1.5.22):
    
      1. shared and static libraries enabled (the main use case):
           --enable-shared[=PKGS]  build shared libraries [default=yes]
           --enable-static[=PKGS]  build static libraries [default=yes]
    
      2. shared libraries enabled and static libraries disabled:
           --enable-static[=PKGS]  build static libraries [default=no]
           --enable-shared[=PKGS]  build shared libraries [default=yes]
    
      3. shared libraries disabled and static libraries enabled:
           --enable-shared[=PKGS]  build shared libraries [default=no]
           --enable-static[=PKGS]  build static libraries [default=yes]
    
    Bug: https://bugs.gentoo.org/814380
    Signed-off-by: David Seifert <soap@gentoo.org>
    Closes: https://github.com/pkgcore/pkgcore/pull/386
    Signed-off-by: Arthur Zamarin <arthurzam@gentoo.org>

--- data/lib/pkgcore/ebd/eapi/8/src_configure.bash
+++ data/lib/pkgcore/ebd/eapi/8/src_configure.bash
@@ -2,7 +2,7 @@ __econf_options_eapi8() {
 	if [[ $1 == *"--datarootdir"* ]]; then
 		echo "--datarootdir=${EPREFIX}/usr/share"
 	fi
-	if [[ $1 == *"--disable-static"* || $1 == *"--enable-static"* ]]; then
+	if [[ $1 == *--enable-shared[^A-Za-z0-9+_.-]* && $1 == *--enable-static[^A-Za-z0-9+_.-]* ]]; then
 		echo "--disable-static"
 	fi
 }

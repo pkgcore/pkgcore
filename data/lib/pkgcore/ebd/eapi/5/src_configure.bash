2ead8aa520f77735e801f35fee30d6d493e7b075
Ulrich Müller <ulm@gentoo.org>
2023-04-11 18:54:26 +0200

    econf matches configure --help output better
    Check for proper end of string for all option names beginning with
    "with-", "disable-" or "enable-". This mainly affects --with-sysroot,
    where false positives have been observed.
    
    This follows this retroactive change in PMS:
    https://gitweb.gentoo.org/proj/pms.git/commit/?id=0e311ca4ac75be6ebea2a0b3c1b46f4daac75190
    which was approved by the Gentoo Council in its 2023-04-09 meeting.
    
    Signed-off-by: Ulrich Müller <ulm@gentoo.org>

--- data/lib/pkgcore/ebd/eapi/5/src_configure.bash
+++ data/lib/pkgcore/ebd/eapi/5/src_configure.bash
@@ -1,5 +1,5 @@
 __econf_options_eapi5() {
-	if [[ $1 == *"--disable-silent-rules"* ]]; then
+	if [[ $1 == *--disable-silent-rules[^A-Za-z0-9+_.-]* ]]; then
 		echo --disable-silent-rules
 	fi
 }
--- data/lib/pkgcore/ebd/eapi/7/src_configure.bash
+++ data/lib/pkgcore/ebd/eapi/7/src_configure.bash
@@ -1,5 +1,5 @@
 __econf_options_eapi7() {
-	if [[ $1 == *"--with-sysroot"* ]]; then
+	if [[ $1 == *--with-sysroot[^A-Za-z0-9+_.-]* ]]; then
 		echo --with-sysroot="${ESYSROOT:-/}"
 	fi
 }

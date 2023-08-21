8d1a2095e9e3d5892d5eef39725d8bd2bcf74cbe
Arthur Zamarin <arthurzam@gentoo.org>
2023-08-21 21:23:37 +0300

    operations.format.fetch_base: fix for when no proxy
    Follows: b1053683ecb8e17ed69e5797c32b83dd48452f59
    Resolves: https://github.com/pkgcore/pkgdev/issues/151
    Signed-off-by: Arthur Zamarin <arthurzam@gentoo.org>

--- src/pkgcore/operations/format.py
+++ src/pkgcore/operations/format.py
@@ -45,8 +45,8 @@ class fetch_base:
             resumecmd,
             attempts=attempts,
             PATH=os.environ["PATH"],
-            http_proxy=os.environ["http_proxy"],
-            https_proxy=os.environ["https_proxy"],
+            http_proxy=os.environ.get("http_proxy", ""),
+            https_proxy=os.environ.get("https_proxy", ""),
         )
 
     def fetch_all(self, observer):

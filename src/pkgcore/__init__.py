c4f51cb2d625641ae760c9b74a2154af8c3d569e
Arthur Zamarin <arthurzam@gentoo.org>
2026-06-20 00:51:51 +0300

    start work on 0.12.35
    Signed-off-by: Arthur Zamarin <arthurzam@gentoo.org>

--- src/pkgcore/__init__.py
+++ src/pkgcore/__init__.py
@@ -1,4 +1,4 @@
 __title__ = "pkgcore"
-__version__ = "0.12.34"
-__version_info__ = (0, 12, 34)
+__version__ = "0.12.35.dev0"
+__version_info__ = (0, 12, 35, "dev0")
 __python_mininum_version__ = (3, 12, 0)

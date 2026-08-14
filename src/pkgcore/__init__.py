7e853367323beac5a49c7c87f731ecb888a52cb0
Arthur Zamarin <arthurzam@gentoo.org>
2026-08-14 16:54:54 +0300

    new release 0.12.39
    Signed-off-by: Arthur Zamarin <arthurzam@gentoo.org>

--- src/pkgcore/__init__.py
+++ src/pkgcore/__init__.py
@@ -1,4 +1,4 @@
 __title__ = "pkgcore"
-__version__ = "0.12.39.dev0"
-__version_info__ = (0, 12, 39, "dev0")
+__version__ = "0.12.39"
+__version_info__ = (0, 12, 39)
 __python_mininum_version__ = (3, 12, 0)
